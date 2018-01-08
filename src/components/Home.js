154aaed56bdc78b654879596f838435ee3b8392c
Loëck Vézien <nainpuissant@gmail.com>
2018-01-08 17:55:29 +0100

    Bôtiful publicKey

--- src/components/Home.js
+++ src/components/Home.js
@@ -2,8 +2,32 @@ import React, { Component } from 'react'
 import ledgerco from 'ledgerco'
 
 class App extends Component {
+  state = {
+    publicKey: null,
+  }
+
+  componentWillMount() {
+    this.getWalletPublicKey()
+  }
+
+  getWalletPublicKey() {
+    ledgerco.comm_node.create_async().then(comm => {
+      comm.device.setNonBlocking(1)
+
+      const btc = new ledgerco.btc(comm)
+
+      btc.getWalletPublicKey_async("44'/0'/0'/0").then(res =>
+        this.setState({
+          publicKey: res.publicKey,
+        }),
+      )
+    })
+  }
+
   render() {
-    return <div>{'...'}</div>
+    const { publicKey } = this.state
+
+    return <div>publicKey: {publicKey}</div>
   }
 }
 
