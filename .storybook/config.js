7582545ca5a9da800e791b8b96d134d57bfd154b
meriadec <meriadec.pillet@gmail.com>
2018-01-16 15:12:50 +0100

    Wrap stories with ThemeProvider and inject global style in them

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,8 +1,19 @@
-import { configure } from '@storybook/react'
+import React from 'react'
+import { configure, addDecorator } from '@storybook/react'
+import { ThemeProvider } from 'styled-components'
+
+import 'styles/global'
+import theme from 'styles/theme'
 
 const req = require.context('../src', true, /.stories.js$/)
 function loadStories() {
   req.keys().forEach(filename => req(filename))
 }
+
+addDecorator(story => (
+  <ThemeProvider theme={theme}>
+    <div style={{ padding: 20 }}>{story()}</div>
+  </ThemeProvider>
+))
 
 configure(loadStories, module)
