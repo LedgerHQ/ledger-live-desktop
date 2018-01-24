3d5ff6784aa50eeb80b5df05dcc8c569d7f9847e
meriadec <meriadec.pillet@gmail.com>
2018-01-24 15:59:45 +0100

    Add QRCode component

---
+++ src/components/base/QRCode/index.js
@@ -0,0 +1,39 @@
+// @flow
+
+import React, { PureComponent } from 'react'
+import qrcode from 'qrcode'
+
+type Props = {
+  data: string,
+  size?: number,
+}
+
+class QRCode extends PureComponent<Props> {
+  static defaultProps = {
+    size: 200,
+  }
+
+  componentDidMount() {
+    this.drawQRCode()
+  }
+
+  componentDidUpdate() {
+    this.drawQRCode()
+  }
+
+  _canvas = null
+
+  drawQRCode() {
+    const { data, size } = this.props
+    qrcode.toCanvas(this._canvas, data, {
+      width: size,
+      margin: 0,
+    })
+  }
+
+  render() {
+    return <canvas ref={n => (this._canvas = n)} />
+  }
+}
+
+export default QRCode
