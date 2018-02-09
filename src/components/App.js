db9b4e3485eceb9eb1b2d36797c44dc61ea8a649
meriadec <meriadec.pillet@gmail.com>
2018-02-09 17:40:10 +0100

    Ability to print receive informations

--- src/components/App.js
+++ src/components/App.js
@@ -3,6 +3,7 @@
 import React from 'react'
 import { ThemeProvider } from 'styled-components'
 import { ConnectedRouter } from 'react-router-redux'
+import { Switch, Route } from 'react-router'
 import { Provider } from 'react-redux'
 import { I18nextProvider } from 'react-i18next'
 
@@ -11,6 +12,7 @@ import theme from 'styles/theme'
 import i18n from 'renderer/i18n'
 
 import Wrapper from 'components/Wrapper'
+import PrintWrapper from 'components/PrintWrapper'
 
 export default ({
   store,
@@ -25,7 +27,10 @@ export default ({
     <I18nextProvider i18n={i18n} initialLanguage={language}>
       <ThemeProvider theme={theme}>
         <ConnectedRouter history={history}>
-          <Wrapper />
+          <Switch>
+            <Route path="/print" component={PrintWrapper} />
+            <Route component={Wrapper} />
+          </Switch>
         </ConnectedRouter>
       </ThemeProvider>
     </I18nextProvider>
