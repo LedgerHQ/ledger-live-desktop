fd50660b15668f61daa51d3412d617a8eb5dab26
meriadec <meriadec.pillet@gmail.com>
2018-02-19 15:19:51 +0100

    Prevent throw of staticPath helper in storybook context

--- src/helpers/staticPath.js
+++ src/helpers/staticPath.js
@@ -1,5 +1,6 @@
-const isRunningInAsar = process.mainModule.filename.indexOf('app.asar') !== -1
+const isStorybook = !process.mainModule
+const isRunningInAsar = !isStorybook && process.mainModule.filename.indexOf('app.asar') !== -1
 
-export default (__DEV__
+export default (__DEV__ && !isStorybook
   ? __static
   : isRunningInAsar ? __dirname.replace(/app\.asar$/, 'static') : `${__dirname}/../static`)
