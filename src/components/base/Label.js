9c45b96fbf4f1a8fb1881b875646d17643156f3d
meriadec <meriadec.pillet@gmail.com>
2018-03-12 17:59:19 +0100

    Update send, receive and account settings modals

--- src/components/base/Label.js
+++ src/components/base/Label.js
@@ -1,11 +1,15 @@
 import styled from 'styled-components'
-import { fontSize } from 'styled-system'
+import { fontSize, color } from 'styled-system'
+
+import fontFamily from 'styles/styled/fontFamily'
 
 export default styled.label.attrs({
   fontSize: 3,
+  ff: 'Museo Sans|Regular',
+  color: 'grey',
 })`
+  ${color};
   ${fontSize};
+  ${fontFamily};
   display: block;
-  text-transform: uppercase;
-  letter-spacing: 1px;
 `
