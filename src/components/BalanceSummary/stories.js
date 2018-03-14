a2e756e9b7b48bada6fe84b2af34db7859b925a8
meriadec <meriadec.pillet@gmail.com>
2018-03-14 11:31:07 +0100

    Prevent NaN on BalanceInfos

---
+++ src/components/BalanceSummary/stories.js
@@ -0,0 +1,22 @@
+// @flow
+
+import React from 'react'
+import { storiesOf } from '@storybook/react'
+import { number } from '@storybook/addon-knobs'
+import { translate } from 'react-i18next'
+
+import BalanceInfos from './BalanceInfos'
+
+const stories = storiesOf('Components', module)
+
+const BalanceInfosComp = translate()(BalanceInfos)
+
+stories.add('BalanceInfos', () => (
+  <BalanceInfosComp
+    since="month"
+    counterValue="USD"
+    totalBalance={number('totalBalance', 1000, { min: 0 })}
+    sinceBalance={number('sinceBalance', 500, { min: 0 })}
+    refBalance={number('refBalance', 200, { min: 0 })}
+  />
+))
