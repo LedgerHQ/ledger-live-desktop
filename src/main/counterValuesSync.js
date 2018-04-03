94a722d51c68eaf7743f1d2dac1cc413ac5bc933
meriadec <meriadec.pillet@gmail.com>
2018-04-03 17:14:41 +0200

    Replace deprecated method

--- src/main/counterValuesSync.js
+++ src/main/counterValuesSync.js
@@ -1,12 +1,12 @@
 // @flow
 
-import { fetchCurrentCounterValues } from '@ledgerhq/wallet-common/lib/api/countervalue'
+import { fetchCurrentRates } from '@ledgerhq/wallet-common/lib/api/countervalue'
 
 type SendFunction = (type: string, data: *) => void
 
 export default async (send: SendFunction, { counterValue, currencies }: Object) => {
   try {
-    const data = await fetchCurrentCounterValues(currencies, counterValue)
+    const data = await fetchCurrentRates(currencies, counterValue)
     send('counterValues.update', data)
   } catch (err) {
     console.error(err) // eslint-disable-line no-console
