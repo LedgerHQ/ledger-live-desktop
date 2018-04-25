9be6fa82828b9b0b49080b7b37cab6ecdcde75ce
meriadec <meriadec.pillet@gmail.com>
2018-04-25 10:17:37 +0200

    Fix lint

--- src/components/ManagerPage/stories/PlugYourDevice.stories.js
+++ src/components/ManagerPage/stories/PlugYourDevice.stories.js
@@ -3,8 +3,6 @@
 import React from 'react'
 
 import { storiesOf } from '@storybook/react'
-import { text, select } from '@storybook/addon-knobs'
-import { action } from '@storybook/addon-actions'
 
 import Box from 'components/base/Box'
 import PlugYourDevice from '../PlugYourDevice'
