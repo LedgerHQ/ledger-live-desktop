b81553b1c5255440b81855fe0569c8fec2f37488
Thibaut Boustany <thibaut@ledger.fr>
2018-05-16 14:33:23 +0200

    Fix restart link not working - version bump to 0.1.2

--- src/components/DashboardPage/index.js
+++ src/components/DashboardPage/index.js
@@ -144,7 +144,7 @@ class DashboardPage extends PureComponent<Props, State> {
         <Box horizontal alignItems="flex-end">
           <Box grow>
             <Text color="dark" ff="Museo Sans" fontSize={7}>
-              {"NOW, YOU'RE PLAYING WITH v0.1.1 POWER!"}
+              {"NOW, YOU'RE PLAYING WITH v0.1.2 POWER!"}
             </Text>
             <Text color="grey" fontSize={5} ff="Museo Sans|Light">
               {totalAccounts > 0
