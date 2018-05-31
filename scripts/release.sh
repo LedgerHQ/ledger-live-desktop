0ede23ed1a3be44f1d1d97ee25c3ac6d687e21bf
Gaëtan Renaudeau <gaetan.renaudeau@ledger.fr>
2018-05-31 16:53:53 +0200

    todo

--- scripts/release.sh
+++ scripts/release.sh
@@ -12,6 +12,9 @@ if ! git diff-index --quiet HEAD --; then
   exit 1
 fi
 
+# TODO check if version is not already there
+# TODO check if local git HEAD is EXACTLY our remote master HEAD
+
 export SENTRY_URL=https://db8f5b9b021048d4a401f045371701cb@sentry.io/274561
 yarn compile
 build
