ef303c042f5873ee74af115904ccd712c6c5e7e5
Anastasia Poupeney <anastasia.poupeney@ledger.fr>
2018-06-13 18:34:20 +0200

    merge issues fix

--- src/commands/testCrash.js
+++ src/commands/testCrash.js
@@ -8,8 +8,10 @@ import { createCommand, Command } from 'helpers/ipc'
 type Input = void
 type Result = void
 
-const cmd: Command<Input, Result> = createCommand('testCrash', () => Observable.create(() => {
+const cmd: Command<Input, Result> = createCommand('testCrash', () =>
+  Observable.create(() => {
     process.exit(1)
-  }))
+  }),
+)
 
 export default cmd
