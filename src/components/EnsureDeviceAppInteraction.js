4f49626e8fdd39d0da27c2a1a8cb80a7e9626f50
meriadec <meriadec.pillet@gmail.com>
2018-06-18 16:20:53 +0200

    Move render-rendered functions to class instance

--- src/components/EnsureDeviceAppInteraction.js
+++ src/components/EnsureDeviceAppInteraction.js
@@ -26,6 +26,36 @@ class EnsureDeviceAppInteraction extends Component<{
   device: ?Device,
   account: Account,
 }> {
+  connectInteractionHandler = () =>
+    createCancelablePolling(500, () => {
+      if (!this.props.device) return Promise.reject()
+      return Promise.resolve(this.props.device)
+    })
+
+  openAppInteractionHandler = ({ device }) => {
+    const { account } = this.props
+    return createCancelablePolling(500, async () => {
+      const { address } = await getAddress
+        .send({
+          devicePath: device.path,
+          currencyId: account.currency.id,
+          path: account
+            ? account.freshAddressPath
+            : standardDerivation({ currency: account.currency, segwit: false, x: 0 }),
+          segwit: account ? isSegwitAccount(account) : false,
+        })
+        .toPromise()
+      return address
+    })
+  }
+
+  renderOpenAppTitle = ({ device }) => {
+    const { account } = this.props
+    return `Open the ${account.currency.name} app on your ${
+      device ? `${device.product} ` : 'device'
+    }`
+  }
+
   render() {
     const { account, ...props } = this.props
     const Icon = getCryptoCurrencyIcon(account.currency)
@@ -38,34 +68,14 @@ class EnsureDeviceAppInteraction extends Component<{
             icon: usbIcon,
             desc: 'Because it is required',
             minMs: 300,
-            run: () =>
-              createCancelablePolling(500, () => {
-                if (!this.props.device) return Promise.reject()
-                return Promise.resolve(this.props.device)
-              }),
+            run: this.connectInteractionHandler,
           },
           {
             id: 'address',
-            title: ({ device }) =>
-              `Open the ${account.currency.name} app on your ${
-                device ? `${device.product} ` : 'device'
-              }`,
+            title: this.renderOpenAppTitle,
             desc: 'To be able to retriev your Bitcoins',
             icon: Icon ? <Icon size={24} /> : null,
-            run: ({ device }) =>
-              createCancelablePolling(500, async () => {
-                const { address } = await getAddress
-                  .send({
-                    devicePath: device.path,
-                    currencyId: account.currency.id,
-                    path: account
-                      ? account.freshAddressPath
-                      : standardDerivation({ currency: account.currency, segwit: false, x: 0 }),
-                    segwit: account ? isSegwitAccount(account) : false,
-                  })
-                  .toPromise()
-                return address
-              }),
+            run: this.openAppInteractionHandler,
           },
         ]}
         {...props}
