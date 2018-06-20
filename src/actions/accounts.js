f788f98fff6083277dfc30258b5191f2a59bcf2c
meriadec <meriadec.pillet@gmail.com>
2018-06-20 09:50:19 +0200

    Make accounts save in db on ADD_ACCOUNT

--- src/actions/accounts.js
+++ src/actions/accounts.js
@@ -6,7 +6,7 @@ import db from 'helpers/db'
 
 export type AddAccount = Account => *
 export const addAccount: AddAccount = payload => ({
-  type: 'ADD_ACCOUNT',
+  type: 'DB:ADD_ACCOUNT',
   payload,
 })
 
