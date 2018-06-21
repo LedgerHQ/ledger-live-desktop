f7894c64a505caf8e6a60f749f83173714166be3
Gaëtan Renaudeau <gaetan.renaudeau@ledger.fr>
2018-06-21 08:54:40 +0200

    move release env to dist script to be sure we don't forget envs

--- scripts/dist.sh
+++ scripts/dist.sh
@@ -1,5 +1,11 @@
 #/bin/bash
 
-rm -rf dist &&
-NODE_ENV=production webpack-cli --mode production --config webpack/internals.config.js &&
+set -e
+
+export GIT_REVISION=`git rev-parse HEAD`
+export SENTRY_URL=https://db8f5b9b021048d4a401f045371701cb@sentry.io/274561
+
+rm -rf ./node_modules/.cache dist
+yarn
+NODE_ENV=production webpack-cli --mode production --config webpack/internals.config.js
 NODE_ENV=production electron-webpack
