7ec6c2e60f80dc53cd67bad41ba2a5582211241f
meriadec <meriadec.pillet@gmail.com>
2018-06-21 11:41:04 +0200

    Optimize CI builds by caching dependencies
    Now it takes ~1 minute instead of more than 5 minutes :tada:

---
+++ scripts/install-ci-deps.sh
@@ -0,0 +1,13 @@
+#/bin/bash
+
+source scripts/hash-utils.sh
+
+PACKAGE_JSON_HASH=`md5sum package.json | cut -d ' ' -f 1`
+CACHED_PACKAGE_JSON_HASH=`GET_HASH 'package.json'`
+
+if [ "$CACHED_PACKAGE_JSON_HASH" == "$PACKAGE_JSON_HASH" ]; then
+  echo "> Skipping yarn install"
+else
+  yarn install
+  SET_HASH 'package.json' $PACKAGE_JSON_HASH
+fi
--- scripts/postinstall.sh
+++ scripts/postinstall.sh
@@ -1,33 +1,12 @@
 #/bin/bash
 
+source scripts/hash-utils.sh
+
 function MAIN {
-  REBUILD_ELECTRON_NATIVE_DEPS
-  INSTALL_FLOW_TYPED
-}
-
-function GET_HASH_PATH {
-  HASH_NAME=$1
-  echo "./node_modules/.cache/LEDGER_HASH_$HASH_NAME.hash"
-}
-
-function GET_HASH {
-  HASH_NAME=$1
-  HASH_PATH=`GET_HASH_PATH $HASH_NAME`
-  if [ ! -e "$HASH_PATH" ]; then
-    echo ''
-  else
-    HASH_CONTENT=`cat "$HASH_PATH"`
-    echo $HASH_CONTENT
+  if ! $CI; then
+    REBUILD_ELECTRON_NATIVE_DEPS
   fi
-}
-
-function SET_HASH {
-  HASH_NAME=$1
-  HASH_CONTENT=$2
-  echo "setting hash $HASH_NAME to $HASH_CONTENT"
-  HASH_PATH=`GET_HASH_PATH $HASH_NAME`
-  mkdir -p ./node_modules/.cache
-  echo $HASH_CONTENT > $HASH_PATH
+  INSTALL_FLOW_TYPED
 }
 
 function INSTALL_FLOW_TYPED {
