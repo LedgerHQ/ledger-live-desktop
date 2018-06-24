423794294977680346946968729d3db48804f09d
meriadec <meriadec.pillet@gmail.com>
2018-06-24 20:19:37 +0200

    Use GenuineCheck in GenuineCheckModal

---
+++ src/components/GenuineCheckModal.js
@@ -0,0 +1,37 @@
+// @flow
+
+import React, { PureComponent } from 'react'
+import { translate } from 'react-i18next'
+
+import type { T } from 'types/common'
+
+import Modal, { ModalBody, ModalTitle, ModalContent } from 'components/base/Modal'
+import GenuineCheck from 'components/GenuineCheck'
+
+type Props = {
+  t: T,
+  onSuccess: void => void,
+  onFail: void => void,
+  onUnavailable: Error => void,
+}
+
+class GenuineCheckModal extends PureComponent<Props> {
+  renderBody = ({ onClose }) => {
+    const { t, onSuccess, onFail, onUnavailable } = this.props
+    return (
+      <ModalBody onClose={onClose}>
+        <ModalTitle>{t('app:genuinecheck.modal.title')}</ModalTitle>
+        <ModalContent>
+          <GenuineCheck onSuccess={onSuccess} onFail={onFail} onUnavailable={onUnavailable} />
+        </ModalContent>
+      </ModalBody>
+    )
+  }
+
+  render() {
+    const { t, ...props } = this.props
+    return <Modal {...props} render={this.renderBody} />
+  }
+}
+
+export default translate()(GenuineCheckModal)
