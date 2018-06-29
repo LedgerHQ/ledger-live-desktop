21e6bcea7667d151a720f8d2cd77e144936e9bb2
meriadec <meriadec.pillet@gmail.com>
2018-06-29 10:30:04 +0200

    Remove unused ChildSwitch component

--- src/components/base/ChildSwitch.js
+++
@@ -1,5 +0,0 @@
-// @flow
-import { Children } from 'react'
-
-export default ({ children, index }: { children: React$Node, index: number }): ?React$Node =>
-  Children.toArray(children)[index] || null
