6c5274fd3cc923f27195e8fdd8530da180123a49
meriadec <meriadec.pillet@gmail.com>
2018-07-03 18:22:37 +0200

    Fix modals gradient box hiding the modal footer border

--- src/components/GradientBox.js
+++ src/components/GradientBox.js
@@ -5,10 +5,10 @@ export default styled.div`
   width: 100%;
   height: 60px;
   position: absolute;
-  bottom: 68px;
+  bottom: 0;
   left: 0;
   right: 0;
-  background: linear-gradient(rgba(255, 255, 255, 0), #ffffff 70%);
+  background: linear-gradient(rgba(255, 255, 255, 0), #ffffff);
   z-index: 2;
   pointer-events: none;
 `
