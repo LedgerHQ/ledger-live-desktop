c993f97c5d29f530ee8cb73b72df3e57418dd6af
Gaëtan Renaudeau <gaetan.renaudeau@ledger.fr>
2018-07-03 07:51:19 +0200

    Capture the unhandled exceptions of the process

--- src/main/bridge.js
+++ src/main/bridge.js
@@ -7,8 +7,9 @@ import { ipcMain, app } from 'electron'
 import { ipcMainListenReceiveCommands } from 'helpers/ipc'
 import path from 'path'
 import logger from 'logger'
-import sentry from 'sentry/node'
+import sentry, { captureException } from 'sentry/node'
 import user from 'helpers/user'
+import { deserializeError } from 'helpers/errors'
 
 import setupAutoUpdater, { quitAndInstall } from './autoUpdate'
 import { setInternalProcessPID } from './terminator'
@@ -100,6 +101,11 @@ ipcMainListenReceiveCommands({
 
 function handleGlobalInternalMessage(payload) {
   switch (payload.type) {
+    case 'uncaughtException': {
+      const err = deserializeError(payload.error)
+      captureException(err)
+      break
+    }
     case 'setLibcoreBusy':
     case 'setDeviceBusy':
     case 'executeHttpQueryOnRenderer': {
