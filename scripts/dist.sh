5d6a90276d8924afcfef488c419a484ab656a31b
meriadec <meriadec.pillet@gmail.com>
2018-07-05 18:29:21 +0200

    Make electron-builder package icon for .AppImage

--- scripts/dist.sh
+++ scripts/dist.sh
@@ -1,3 +1,16 @@
 #!/bin/bash
 
+# hilarious fix: to make linux icon we have to remove icon.png from build folder
+# some context:
+#   - https://github.com/electron-userland/electron-builder/issues/2577
+#   - https://github.com/electron-userland/electron-builder/issues/2269
+if [[ `uname` == 'Linux' ]]; then
+  mv build/icon.png /tmp
+fi
+
 yarn compile && DEBUG=electron-builder electron-builder
+
+# hilarious fix continuation: put back the icon where it was
+if [[ `uname` == 'Linux' ]]; then
+  mv /tmp/icon.png build
+fi
