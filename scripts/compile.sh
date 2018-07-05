6b9799b77597956d5c4b27c81f8944be563c4f8f
meriadec <meriadec.pillet@gmail.com>
2018-07-05 18:07:38 +0200

    Disable useless .tar.gz and tweak AppImage and compile options

--- scripts/compile.sh
+++ scripts/compile.sh
@@ -4,8 +4,9 @@ set -e
 
 export GIT_REVISION=`git rev-parse HEAD`
 export SENTRY_URL=https://db8f5b9b021048d4a401f045371701cb@sentry.io/274561
+export JOBS=max
 
 rm -rf ./node_modules/.cache dist
-JOBS=max yarn
+yarn
 NODE_ENV=production yarn run webpack-cli --mode production --config webpack/internals.config.js
 NODE_ENV=production yarn run electron-webpack
