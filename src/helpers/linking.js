47f8474e08bd15c917363d0884399d7b9c1f2b26
meriadec <meriadec.pillet@gmail.com>
2018-07-27 18:18:14 +0200

    need to split that in two commits

--- src/helpers/linking.js
+++ src/helpers/linking.js
@@ -1,6 +1,13 @@
 // @flow
-import { shell } from 'electron'
 import { track } from 'analytics/segment'
+
+let shell
+if (process.env.STORYBOOK_ENV) {
+  shell = { openExternal: () => {} }
+} else {
+  const electron = require('electron')
+  shell = electron.shell
+}
 
 export const openURL = (
   url: string,
