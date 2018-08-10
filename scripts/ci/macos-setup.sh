e7c1c9734dcb7f27d415fbead15307a29cd83722
Arnaud97234 <arnaud.ulric@gmail.com>
2018-08-10 19:14:42 +0200

    update Xcode version from 9 to 9.4.1

--- scripts/ci/macos-setup.sh
+++ scripts/ci/macos-setup.sh
@@ -3,7 +3,6 @@
 sudo rm -rf /usr/local/lib/node_modules
 sudo rm /usr/local/bin/node
 cd  /usr/local/bin && ls -l | grep "../lib/node_modules/" | awk '{print $9}'| xargs rm
-
 curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.33.11/install.sh | bash
 echo "unset PREFIX" >> ~/.bashrc
 source ~/.bashrc
