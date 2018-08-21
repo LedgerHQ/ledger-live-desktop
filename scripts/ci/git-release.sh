c3d574dfa5b6197a96d5598149d92ea0b8a49192
Arnaud97234 <arnaud.ulric@gmail.com>
2018-08-21 15:49:53 +0200

    test create release on github

--- scripts/ci/git-release.sh
+++ scripts/ci/git-release.sh
@@ -4,6 +4,6 @@ curl -s POST /repos/Arnaud97234/ledger-live-desktop/releases \
 -d '{
   "tag_name": "v0.0.1",
   "name": "0.0.1",
-  "draft": "false",
-  "prerelease": "true"
+  "draft": false,
+  "prerelease": true
 }'
