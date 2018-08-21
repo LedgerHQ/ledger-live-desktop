2dd48e880381cf3ece3cc5f4545782c3580d44c4
Arnaud97234 <arnaud.ulric@gmail.com>
2018-08-21 16:15:01 +0200

    test create release on github

--- scripts/ci/git-release.sh
+++ scripts/ci/git-release.sh
@@ -1,3 +1,14 @@
 #!/bin/bash
 
-curl -s POST /repos/Arnaud97234/ledger-live-desktop/releases -d '{"tag_name": "v0.0.1", "name": "0.0.1", "draft": false, "prerelease": true }'
+curl -X POST \
+  https://github.com/Arnaud97234/ledger-live-desktop.git \
+  -H 'Cache-Control: no-cache' \
+  -H 'Content-Type: application/json' \
+  -H 'Postman-Token: 686d5307-32e3-4098-804c-e6cf88ac4589' \
+  -d '{
+	"tag_name": "v0.0.1",
+	"target_commitish": "circleci-QA",
+	"name": "0.0.1",
+	"draft": false,
+	"prerelease": true
+}'
