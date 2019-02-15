404f036e1598e24d29ccef58ce292abad2f60e5c
Dimitri Sabadie <dimitri.sabadie@ledger.fr>
2019-02-15 16:42:41 +0100

    Use a safer way to call the reset code in the libcoreReset cmd.

--- src/commands/libcoreReset.js
+++ src/commands/libcoreReset.js
@@ -1,15 +1,15 @@
 // @flow
 
 import { createCommand, Command } from 'helpers/ipc'
-import { of } from 'rxjs'
+import { from } from 'rxjs'
 import withLibcore from 'helpers/withLibcore'
 
 type Input = void
 type Result = boolean
 
-const cmd: Command<Input, Result> = createCommand('libcoreReset', () => {
-  withLibcore(core => core.getPoolInstance().freshResetAll())
-  return of(true)
-})
+const cmd: Command<Input, Result> = createCommand(
+  'libcoreReset',
+  () => from(withLibcore(core => core.getPoolInstance().freshResetAll()))
+)
 
 export default cmd
