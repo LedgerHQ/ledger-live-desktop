9730ec65f3293374f75cf185e4498a63c8d8d2ee
Valentin D. Pinkman <valentin.d.pinkman@icloud.com>
2019-03-27 14:48:09 +0100

    fix glitch message during firmware update

--- src/components/modals/UpdateFirmware/Installing.js
+++ src/components/modals/UpdateFirmware/Installing.js
@@ -1,20 +1,17 @@
 // @flow
 import React, { Fragment } from 'react'
-import { translate } from 'react-i18next'
+import { Trans } from 'react-i18next'
 
 import Box from 'components/base/Box'
 import Text from 'components/base/Text'
 import ProgressCircle from 'components/ProgressCircle'
 
-import type { T } from 'types/common'
-
 type Props = {
-  t: T,
   progress: number,
   installing: string,
 }
 
-function Installing({ t, progress, installing }: Props) {
+function Installing({ progress, installing }: Props) {
   return (
     <Fragment>
       <Box mx={7} align="center">
@@ -22,16 +19,16 @@ function Installing({ t, progress, installing }: Props) {
       </Box>
       <Box mx={7} mt={4} mb={2}>
         <Text ff="Museo Sans|Regular" align="center" color="dark" fontSize={6}>
-          {t(`manager.modal.steps.${installing}`)}
+          {installing ? <Trans i18nKey={`manager.modal.steps.${installing}`} /> : null}
         </Text>
       </Box>
       <Box mx={7} mt={4} mb={7}>
         <Text ff="Open Sans|Regular" align="center" color="graphite" fontSize={4}>
-          {t('manager.modal.mcuPin')}
+          <Trans i18nKey="manager.modal.mcuPin" />
         </Text>
       </Box>
     </Fragment>
   )
 }
 
-export default translate()(Installing)
+export default Installing
