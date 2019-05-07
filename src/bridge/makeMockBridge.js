19cc9e7d25ace16639ea69d73a7ae119e32b8b3a
Gaëtan Renaudeau <renaudeau.gaetan@gmail.com>
2019-05-07 12:37:02 +0200

    fixes mock bridge

--- src/bridge/makeMockBridge.js
+++ src/bridge/makeMockBridge.js
@@ -162,7 +162,7 @@ function makeMockBridge(opts?: Opts): WalletBridge<*> {
     signAndBroadcast: (account, t) =>
       Observable.create(o => {
         const rng = new Prando()
-        const op = genOperation(account, account, account.operations, account.currency, rng)
+        const op = genOperation(account, account, account.operations, rng)
         op.type = 'OUT'
         op.value = t.amount
         op.blockHash = null
