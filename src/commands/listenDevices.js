0c6c7b5d85a00982b8c2df458dfef52f81eefdae
Gaëtan Renaudeau <renaudeau.gaetan@gmail.com>
2019-05-23 09:19:15 +0200

    Update live-common and ledgerjs

--- src/commands/listenDevices.js
+++ src/commands/listenDevices.js
@@ -1,20 +1,11 @@
 // @flow
 
-import logger from 'logger'
 import { createCommand } from 'helpers/ipc'
 import { Observable } from 'rxjs'
 import CommNodeHid from '@ledgerhq/hw-transport-node-hid'
 import { LISTEN_DEVICES_DEBOUNCE } from 'config/constants'
 
 CommNodeHid.setListenDevicesDebounce(LISTEN_DEVICES_DEBOUNCE)
-
-CommNodeHid.setListenDevicesDebug((msg, ...args) =>
-  logger.debug(msg, {
-    type: 'listenDevices',
-    args,
-  }),
-)
-
 const cmd: any = createCommand('listenDevices', () => Observable.create(CommNodeHid.listen))
 
 export default cmd
