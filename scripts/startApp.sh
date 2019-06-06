63adb7464e75679d276c6ce19cdffa7ac75286cd
Arnaud97234 <arnaud.ulric@gmail.com>
2019-06-06 18:45:57 +0200

    downgrade electron for linux builds (temporary until we solve electron 2.0.17 issue on linux)

--- scripts/startApp.sh
+++ scripts/startApp.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
+set -e
+
 yarn start > test-e2e/logs.txt &
+pid=$!
 sleep 60 &
 ( tail -f -n0 test-e2e/logs.txt & ) | grep -q 'info: libcore {"intVersion":'
 if [ $? -eq 0 ] ; then
@@ -10,5 +13,5 @@ else
     exit 1
 fi
 sleep 5
-killall -9 Electron
+kill -9 $pid
 rm test-e2e/logs.txt
