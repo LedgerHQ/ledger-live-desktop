0312abddcb52b2f7fb8b10e719fb1d1a35e857ea
Juan Cortes Ross <juan@bohem.io>
2019-06-13 18:08:03 +0200

    LL-1487 Added empty state placeholder for token list

--- src/components/base/LabelWithExternalIcon.js
+++ src/components/base/LabelWithExternalIcon.js
@@ -7,19 +7,21 @@ import Label from 'components/base/Label'
 import Box from 'components/base/Box'
 import IconExternalLink from 'icons/ExternalLink'
 
-const LabelWrapper = styled(Label).attrs({})`
+const LabelWrapper = styled(Label).attrs({ ff: p => (p.ff ? p.ff : 'Museo Sans|Bold') })`
+  display: inline-flex;
+  color: ${p => p.theme.colors[p.color] || 'auto'};
   &:hover {
     color: ${p => p.theme.colors.wallet};
     cursor: pointer;
   }
 `
 
-type Props = { onClick: ?() => void, label: string }
+type Props = { onClick: ?() => void, label: string, color?: string, ff?: string }
 
 // can add more dynamic options if needed
-export function LabelWithExternalIcon({ onClick, label }: Props) {
+export function LabelWithExternalIcon({ onClick, label, color, ff }: Props) {
   return (
-    <LabelWrapper onClick={onClick}>
+    <LabelWrapper onClick={onClick} color={color} ff={ff}>
       <span>{label}</span>
       <Box ml={1}>
         <IconExternalLink size={12} />
