b0f97cc946081cb1dddaec899c014dd7aea7d679
Thibaut Boustany <thibaut@kastor.fr>
2019-06-20 12:18:36 +0200

    rimraf sqlite on libcore self reset fail

--- src/helpers/reset.js
+++ src/helpers/reset.js
@@ -1,12 +1,21 @@
 // @flow
 
 import { shell, remote } from 'electron'
+import path from 'path'
+import rimraf from 'rimraf'
 import resolveUserDataDirectory from 'helpers/resolveUserDataDirectory'
 import { disable as disableDBMiddleware } from 'middlewares/db'
 import db from 'helpers/db'
-import { delay } from 'helpers/promise'
+import { delay, promisify } from 'helpers/promise'
 import killInternalProcess from 'commands/killInternalProcess'
 import libcoreReset from 'commands/libcoreReset'
+
+async function deleteSqlite() {
+  const sqlitePath = path.resolve(resolveUserDataDirectory(), 'sqlite')
+  const rm = promisify(rimraf)
+
+  await rm(sqlitePath)
+}
 
 async function resetLibcore() {
   // we need to stop everything that is happening right now, like syncs
@@ -15,7 +24,12 @@ async function resetLibcore() {
     .toPromise()
     .catch(() => {}) // this is a normal error due to the crash of the process, we ignore it
   // we can now ask libcore to reset itself
-  await libcoreReset.send().toPromise()
+  await libcoreReset
+    .send()
+    .toPromise()
+    // if libcore db is encrypted and not unlocked with the correct pasword,
+    // libcore can't open it to self reset cleanly, so we remove the db files
+    .catch(deleteSqlite)
 }
 
 function reload() {
