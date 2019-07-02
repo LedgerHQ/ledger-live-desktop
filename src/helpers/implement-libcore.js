9dccb751ffb546fe437503571a01d1a2195eb8f4
Thibaut Boustany <thibaut@kastor.fr>
2019-07-02 16:29:37 +0200

    New libcore init process flow chart

--- src/helpers/implement-libcore.js
+++ src/helpers/implement-libcore.js
@@ -1,4 +1,39 @@
 // @flow
+//
+// Libcore implementation used to live here before libcore db encryption was added.
+// Now libcore needs to be init with the user password from the renderer without relying on env variables (for security/privacy reasons),
+// so we end up doing the following IPC dance when Internal process starts 💃
+//
+// Main Process------------------------------------+      Renderer Process-------------------------------------------+
+// |                                               |      |                                                          |
+// |            bridge-----------------------+     |      |   +----------+                          +-------------+  |
+// |            |                            |     |      |   |          |----getLibcorePassword--->|             |  |
+// |            |     +-------------------------------------->|  events  |                          | Redux Store |  |
+// |            |     |                      |     |      |   |          |<-------password----------|             |  |
+// |            |     |                      |     |      |   +----------+                          +-------------+  |
+// |            |     |                      |     |      |        |                                                 |
+// |            |     |                      |     |      |     password                                             |
+// |            |     |                      |     |      |        |                                                 |
+// |            |     |                      |     |      |        v                                                 |
+// |            |     |                      |     |      |   +------------------------+                             |
+// |            |     |                      |     |      |   |                        |                             |
+// |            |     |               +-----------------------|  commands/libcoreInit  |                             |
+// |            |     |               |      |     |      |   |                        |                             |
+// |            +-----|---------------|------+     |      |   +------------------------+                             |
+// |                  |               |            |      |                                                          |
+// +------------------|---------------|------------+      +----------------------------------------------------------+
+//                    |               |
+//           'initLibcore'          implementLibcore(password)
+//                    |               |
+// Internal Process---|---------------|------------+
+// |                  |               v            |
+// | implement-libcore---+  commandHandler-------+ |
+// | |                   |  |                    | |
+// | |    You are here   |  |  Executes Payload  | |
+// | |                   |  |                    | |
+// | +-------------------+  +--------------------+ |
+// +-----------------------------------------------+
+
 process.send({
   type: 'initLibcore',
 })
