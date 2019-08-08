1d8635e31b2d429f7c2ecfbdde068965599f7e77
Thibaut Boustany <thibaut@kastor.fr>
2019-08-08 11:57:07 +0200

    Remove star from context menu

--- src/components/ContextMenu/AccountContextMenu.js
+++ src/components/ContextMenu/AccountContextMenu.js
@@ -2,12 +2,10 @@
 
 import React, { PureComponent } from 'react'
 import { openModal } from 'reducers/modals'
-import { toggleStarAction } from 'actions/settings'
 import type { Account } from '@ledgerhq/live-common/lib/types/account'
 import { connect } from 'react-redux'
 import IconReceive from 'icons/Receive'
 import IconSend from 'icons/Send'
-import IconStar from 'icons/Star'
 import IconAccountSettings from 'icons/AccountSettings'
 import ContextMenuItem from './ContextMenuItem'
 import { MODAL_RECEIVE, MODAL_SEND, MODAL_SETTINGS_ACCOUNT } from '../../config/constants'
@@ -15,18 +13,16 @@ import { MODAL_RECEIVE, MODAL_SEND, MODAL_SETTINGS_ACCOUNT } from '../../config/
 type Props = {
   account: Account,
   children: any,
-  toggleStarAction: Function,
   openModal: Function,
 }
 
 const mapDispatchToProps = {
   openModal,
-  toggleStarAction,
 }
 
 class AccountContextMenu extends PureComponent<Props> {
   getContextMenuItems = () => {
-    const { openModal, toggleStarAction, account } = this.props
+    const { openModal, account } = this.props
     const items = [
       {
         label: 'accounts.contextMenu.send',
@@ -47,12 +43,6 @@ class AccountContextMenu extends PureComponent<Props> {
         callback: () => openModal(MODAL_SETTINGS_ACCOUNT, { account }),
       })
     }
-
-    items.push({
-      label: 'accounts.contextMenu.star',
-      Icon: IconStar,
-      callback: () => toggleStarAction(account.id),
-    })
 
     return items
   }
