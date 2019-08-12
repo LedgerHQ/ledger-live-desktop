9ca4c1e7c1c5b943518b3dc3d2de6aa864e7a911
Juan Cortes Ross <juan@bohem.io>
2019-08-12 19:49:53 +0200

    LL-1747 Persist starred accounts when migrating them

--- src/actions/settings.js
+++ src/actions/settings.js
@@ -78,3 +78,8 @@ export const dragDropStarAction = (payload: {
   type: 'SETTINGS_DRAG_DROP_STAR',
   payload,
 })
+
+export const replaceStarAccountId = (payload: { oldId: string, newId: string }) => ({
+  type: 'SETTINGS_REPLACE_STAR_ID',
+  payload,
+})
