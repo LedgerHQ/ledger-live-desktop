b772721f61522a32726c7e96d390b352c3149076
Pierre Bondoerffer <pierre.bondoerffer@ledger.fr>
2019-08-27 15:47:15 +0200

    LL-1810: Add ERC20 add account empty state

--- src/components/AccountPage/EmptyStateAccount.js
+++ src/components/AccountPage/EmptyStateAccount.js
@@ -9,6 +9,7 @@ import { translate, Trans } from 'react-i18next'
 import { openModal } from 'reducers/modals'
 import type { T } from 'types/common'
 import type { Account, TokenAccount } from '@ledgerhq/live-common/lib/types'
+import { listTokenTypesForCryptoCurrency } from '@ledgerhq/live-common/lib/currencies'
 
 import { MODAL_RECEIVE } from 'config/constants'
 
@@ -34,6 +35,9 @@ class EmptyStateAccount extends PureComponent<Props, *> {
     const { t, account, parentAccount, openModal } = this.props
     const mainAccount = account.type === 'Account' ? account : parentAccount
     if (!mainAccount) return null
+
+    const hasTokens = Array.isArray(mainAccount.tokenAccounts)
+
     return (
       <Box mt={7} alignItems="center" selectable>
         <img
@@ -45,13 +49,34 @@ class EmptyStateAccount extends PureComponent<Props, *> {
         <Box mt={5} alignItems="center">
           <Title>{t('account.emptyState.title')}</Title>
           <Description mt={3} style={{ display: 'block' }}>
-            <Trans i18nKey="account.emptyState.desc">
-              {'Make sure the'}
-              <Text ff="Open Sans|SemiBold" color="dark">
-                {mainAccount.currency.managerAppName}
-              </Text>
-              {'app is installed to receive funds.'}
-            </Trans>
+            {hasTokens ? (
+              <Trans i18nKey="account.emptyState.descToken">
+                {'Make sure the'}
+                <Text ff="Open Sans|SemiBold" color="dark">
+                  {mainAccount.currency.managerAppName}
+                </Text>
+                {'app is installed and start receiving'}
+                <Text ff="Open Sans|SemiBold" color="dark">
+                  {mainAccount.currency.ticker}
+                </Text>
+                {'and'}
+                <Text ff="Open Sans|SemiBold" color="dark">
+                  {account &&
+                    account.currency &&
+                    // $FlowFixMe
+                    listTokenTypesForCryptoCurrency(account.currency).join(', ')}
+                  {'tokens'}
+                </Text>
+              </Trans>
+            ) : (
+              <Trans i18nKey="account.emptyState.desc">
+                {'Make sure the'}
+                <Text ff="Open Sans|SemiBold" color="dark">
+                  {mainAccount.currency.managerAppName}
+                </Text>
+                {'app is installed and start receiving'}
+              </Trans>
+            )}
           </Description>
           <Button
             mt={5}
