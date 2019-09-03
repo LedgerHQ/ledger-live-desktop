7ce9d3aa9f9b4ca40b917daa8b9c2f762df18f66
Hugo Morosini <hugo.morosini@gmail.com>
2019-09-03 15:02:17 +0200

    export to mobile after account migration

---
+++ src/components/Exporter/ExportInstructions.js
@@ -0,0 +1,105 @@
+import React from 'react'
+import { Trans } from 'react-i18next'
+import styled from 'styled-components'
+import { BulletRow } from '../Onboarding/helperComponents'
+import Box from '../base/Box'
+import Text from '../base/Text'
+
+const BulletRowIcon = styled(Box).attrs({
+  ff: 'Rubik|Regular',
+  fontSize: 10,
+  textAlign: 'center',
+  color: 'wallet',
+  pl: 2,
+})`
+  background-color: rgba(100, 144, 241, 0.2);
+  border-radius: 12px;
+  display: inline-flex;
+  height: 18px;
+  width: 18px;
+  padding: 0px;
+  padding-top: 2px;
+`
+
+const Plus = styled(Box).attrs({
+  ff: 'Open Sans|SemiBold',
+  fontSize: 13,
+  textAlign: 'center',
+  color: 'wallet',
+  pl: 2,
+})`
+  display: inline-flex;
+  background-color: rgba(100, 144, 241, 0.2);
+  border-radius: 4px;
+  height: 14px;
+  line-height: 14px;
+  width: 14px;
+  padding: 0;
+`
+
+const stepsImportMobile = [
+  {
+    key: 'step1',
+    icon: <BulletRowIcon>{'1'}</BulletRowIcon>,
+    desc: (
+      <Box style={{ display: 'block' }}>
+        <Trans i18nKey="settings.export.modal.step1">
+          {'Tap the'}
+          <Text ff="Open Sans|SemiBold" color="dark">
+            <Plus>{'+'}</Plus>
+          </Text>
+          {'button in'}
+          <Text ff="Open Sans|SemiBold" color="dark">
+            {'Accounts'}
+          </Text>
+        </Trans>
+      </Box>
+    ),
+  },
+  {
+    key: 'step2',
+    icon: <BulletRowIcon>{'2'}</BulletRowIcon>,
+    desc: (
+      <Box style={{ display: 'block' }}>
+        <Trans i18nKey="settings.export.modal.step2">
+          {'Tap'}
+          <Text ff="Open Sans|SemiBold" color="dark">
+            {'Import desktop accounts'}
+          </Text>
+        </Trans>
+      </Box>
+    ),
+  },
+  {
+    key: 'step3',
+    icon: <BulletRowIcon>{'3'}</BulletRowIcon>,
+    desc: (
+      <Box style={{ display: 'block' }}>
+        <Trans i18nKey="settings.export.modal.step3">
+          {'Scan the'}
+          <Text ff="Open Sans|SemiBold" color="dark">
+            {'LiveQR Code'}
+          </Text>
+          {'until the loader hits 100%'}
+        </Trans>
+      </Box>
+    ),
+  },
+]
+
+const ExportInstructions = () => (
+  <>
+    <Box shrink style={{ width: 330, fontSize: 13, marginTop: 20 }}>
+      <Text ff="Open Sans|SemiBold" color="dark">
+        <Trans i18nKey="settings.export.modal.listTitle" />
+      </Text>
+    </Box>
+    <Box style={{ width: 330 }}>
+      {stepsImportMobile.map(step => (
+        <BulletRow key={step.key} step={step} />
+      ))}
+    </Box>
+  </>
+)
+
+export default ExportInstructions
