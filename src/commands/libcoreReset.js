f8c9948a648d67aaf807796964d65711646e396f
Gaëtan Renaudeau <renaudeau.gaetan@gmail.com>
2019-10-30 19:37:06 +0100

    update libcore

--- src/commands/libcoreReset.js
+++ src/commands/libcoreReset.js
@@ -2,13 +2,13 @@
 
 import { createCommand, Command } from 'helpers/ipc'
 import { from } from 'rxjs'
-import { withLibcore } from '@ledgerhq/live-common/lib/libcore/access'
+import { withLibcore, reset } from '@ledgerhq/live-common/lib/libcore/access'
 
 type Input = void
 type Result = void
 
 const cmd: Command<Input, Result> = createCommand('libcoreReset', () =>
-  from(withLibcore(core => core.getPoolInstance().freshResetAll())),
+  from(withLibcore(() => reset())),
 )
 
 export default cmd
