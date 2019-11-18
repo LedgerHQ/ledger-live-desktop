ef2244a142e9588450b67249b22a5480bf037261
Gaëtan Renaudeau <renaudeau.gaetan@gmail.com>
2019-11-18 09:39:27 +0100

    Improve TransactionConfirmFields

--- src/families/tezos/TransactionConfirmFields.js
+++ src/families/tezos/TransactionConfirmFields.js
@@ -1,22 +1,65 @@
 // @flow
-import React from 'react'
+import React, { useCallback } from 'react'
+import { useBaker } from '@ledgerhq/live-common/lib/families/tezos/bakers'
+import { shortAddressPreview, getMainAccount } from '@ledgerhq/live-common/lib/account'
+import { getDefaultExplorerView, getAddressExplorer } from '@ledgerhq/live-common/lib/explorers'
+import { openURL } from 'helpers/linking'
 import TransactionConfirmField from 'components/TransactionConfirm/TransactionConfirmField'
 import Text from 'components/base/Text'
 
-const Pre = ({ account, transaction }: *) => (
-  <>
-    <TransactionConfirmField label="Source">
-      <Text ff="Inter|Medium" color="palette.text.shade80" fontSize={2}>
-        {account.freshAddress}
-      </Text>
-    </TransactionConfirmField>
-    <TransactionConfirmField label="Delegate">
-      <Text ff="Inter|Medium" color="palette.text.shade80" fontSize={2}>
-        {transaction.recipient}
-      </Text>
-    </TransactionConfirmField>
-  </>
-)
+const addressStyle = {
+  wordBreak: 'break-all',
+  textAlign: 'right',
+  maxWidth: '50%',
+}
+
+const Pre = ({ account, parentAccount, transaction }: *) => {
+  const mainAccount = getMainAccount(account, parentAccount)
+  const baker = useBaker(transaction.recipient)
+  const explorerView = getDefaultExplorerView(mainAccount.currency)
+  const bakerURL = getAddressExplorer(explorerView, transaction.recipient)
+  const openBaker = useCallback(() => {
+    if (bakerURL) openURL(bakerURL)
+  }, [bakerURL])
+
+  return (
+    <>
+      <TransactionConfirmField label="Source">
+        <Text
+          style={addressStyle}
+          ml={1}
+          ff="Inter|Medium"
+          color="palette.text.shade80"
+          fontSize={3}
+        >
+          {account.freshAddress}
+        </Text>
+      </TransactionConfirmField>
+      <TransactionConfirmField label="Validator">
+        <Text
+          onClick={openBaker}
+          color="palette.primary.main"
+          ml={1}
+          ff="Inter|Medium"
+          fontSize={3}
+        >
+          {baker ? baker.name : shortAddressPreview(transaction.recipient)}
+        </Text>
+      </TransactionConfirmField>
+      <TransactionConfirmField label="Delegate">
+        <Text
+          style={addressStyle}
+          ml={1}
+          ff="Inter|Medium"
+          color="palette.text.shade80"
+          fontSize={3}
+        >
+          {transaction.recipient}
+        </Text>
+      </TransactionConfirmField>
+    </>
+  )
+}
 
 const Post = ({ transaction }: *) => (
   <>
