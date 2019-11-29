bf7a5876e9897f292148e1f40bfb4351740f26f3
Hugo Morosini <hugo.morosini@gmail.com>
2019-11-29 16:21:38 +0100

    fixed tooltip alignment problem

--- src/components/OperationsList/ConfirmationCell.js
+++ src/components/OperationsList/ConfirmationCell.js
@@ -24,11 +24,10 @@ const mapStateToProps = createStructuredSelector({
 })
 
 const Cell = styled(Box).attrs(() => ({
-  px: 4,
+  pl: 4,
   horizontal: true,
   alignItems: 'center',
 }))`
-  width: 44px;
 `
 
 type Props = {
