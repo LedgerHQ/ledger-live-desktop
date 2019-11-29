7a6f80370f050504473348e6f5a95895df6d2709
Hugo Morosini <hugo.morosini@gmail.com>
2019-11-29 13:47:30 +0100

    added blacklist mode

--- scripts/check-errors-wordings.js
+++ scripts/check-errors-wordings.js
@@ -3,16 +3,30 @@
 const WORDINGS = require('../static/i18n/en/app.json')
 const ERRORS = require('@ledgerhq/errors')
 
+const blacklist = [
+  "Error",
+  "CustomError",
+  "BluetoothRequired",
+  "TransportWebUSBGestureRequired",
+  "LedgerAPIError",
+  "LedgerAPIErrorMessage",
+  "generic",
+  "TimeoutError"
+]
+
 async function main() {
   console.log(`>> Checking for errors that lack wordings...`)
 
-  const errorNames = Object.keys(ERRORS).map(error => {
-  const Error = ERRORS[error]
+  const errorNames = Object.keys(ERRORS).reduce((acc, error) => {
+    const Error = ERRORS[error]
     if (typeof Error === 'function') {
       const t = new Error('test')
-      return t.name
+      if (t.name && !blacklist.includes(t.name)) {
+        acc.push(t.name)
+      }
     }
-  }).filter(error => error !== undefined)
+    return acc
+  }, [])
 
   const errorWordings = WORDINGS.errors
   let incomplete = false
@@ -20,13 +34,13 @@ async function main() {
   errorNames.forEach(errorName => {
     const errorWording = errorWordings[errorName]
     if (!errorWording) {
-      console.log(`missing wordings for error [${errorName}]`)
+      console.log(`-- No wordings found for error [${errorName}]`)
       incomplete = true
       return
     }
 
     if (!errorWording.title) {
-      console.log(`missing title for error [${errorName}]`)
+      console.log(`-- Missing title for error [${errorName}]`)
       incomplete = true
     }
   })
@@ -34,8 +48,8 @@ async function main() {
   console.log(`>> Checking for zombie error wordings...`)
 
   Object.keys(errorWordings).forEach(errorWordingName => {
-    if (!errorNames.includes(errorWordingName)) {
-      console.log(`No error called [${errorWordingName}] (found in wordings)`)
+    if (!errorNames.includes(errorWordingName) && !blacklist.includes(errorWordingName)) {
+      console.log(`-- No error [${errorWordingName}] (but found in wordings)`)
       incomplete = true
     }
   })
