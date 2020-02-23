a4d7dc3c42ac3670cd291b31fa76bac94ce6fb20
jsiguira <jun@dappface.com>
2020-02-23 13:58:56 +0100

    refactor AccountsList

--- src/renderer/components/AccountsList/AccountRow.js
+++ src/renderer/components/AccountsList/AccountRow.js
@@ -1,6 +1,6 @@
 // @flow
 
-import React, { PureComponent } from "react";
+import React from "react";
 import styled from "styled-components";
 import type { Account } from "@ledgerhq/live-common/lib/types";
 import { getEnv } from "@ledgerhq/live-common/lib/env";
@@ -12,123 +12,106 @@ import CryptoCurrencyIconWithCount from "~/renderer/components/CryptoCurrencyIco
 import FormattedVal from "~/renderer/components/FormattedVal";
 import Input from "~/renderer/components/Input";
 
-const InputWrapper = styled.div`
-  margin-left: 4px;
+interface Props {
+  account: Account;
+  isChecked?: boolean;
+  isDisabled?: boolean;
+  isReadonly?: boolean;
+  autoFocusInput?: boolean;
+  accountName: string;
+  onToggleAccount?: (Account, boolean) => void;
+  onEditName?: (Account, string) => void;
+  hideAmount?: boolean;
+}
 
-  & > div > div {
-    padding-left: 10px;
-    padding-right: 10px;
-  }
-`;
-
-type Props = {
-  account: Account,
-  isChecked?: boolean,
-  isDisabled?: boolean,
-  isReadonly?: boolean,
-  autoFocusInput?: boolean,
-  accountName: string,
-  onToggleAccount?: (Account, boolean) => void,
-  onEditName?: (Account, string) => void,
-  hideAmount?: boolean,
-};
-
-export default class AccountRow extends PureComponent<Props> {
-  handlePreventSubmit = (e: SyntheticEvent<*>) => {
+export default function AccountRow({
+  account,
+  isChecked,
+  onEditName,
+  accountName,
+  isDisabled,
+  isReadonly,
+  autoFocusInput,
+  hideAmount,
+}: Props) {
+  function handlePreventSubmit(e: SyntheticEvent<*>) {
     e.preventDefault();
     e.stopPropagation();
-  };
+  }
 
-  handleKeyPress = (e: SyntheticEvent<HTMLInputElement>) => {
+  function handleKeyPress(e: SyntheticEvent<HTMLInputElement>) {
     // this fixes a bug with the event propagating to the Tabbable
     e.stopPropagation();
-  };
+  }
 
-  onToggleAccount = () => {
+  function onToggleAccount() {
     const { onToggleAccount, account, isChecked } = this.props;
     if (onToggleAccount) onToggleAccount(account, !isChecked);
-  };
+  }
 
-  handleChangeName = (name: string) => {
+  function handleChangeName(name: string) {
     const { onEditName, account } = this.props;
     if (onEditName) onEditName(account, name);
-  };
+  }
 
-  onClickInput = (e: SyntheticEvent<*>) => {
+  function onClickInput(e: SyntheticEvent<*>) {
     e.preventDefault();
     e.stopPropagation();
-  };
+  }
 
-  onFocus = (e: *) => {
+  function onFocus(e: *) {
     e.target.select();
-  };
+  }
 
-  onBlur = (e: *) => {
+  function onBlur(e: *) {
     const { onEditName, account } = this.props;
     const { value } = e.target;
     if (!value && onEditName) {
       // don't leave an empty input on blur
       onEditName(account, account.name);
     }
-  };
-
-  _input = null;
-  overflowStyles = { textOverflow: "ellipsis", overflow: "hidden", whiteSpace: "nowrap" };
-  render() {
-    const {
-      account,
-      isChecked,
-      onEditName,
-      accountName,
-      isDisabled,
-      isReadonly,
-      autoFocusInput,
-      hideAmount,
-    } = this.props;
-
-    const tokenCount = (account.subAccounts && account.subAccounts.length) || 0;
-
-    return (
-      <AccountRowContainer
-        isDisabled={isDisabled}
-        onClick={isDisabled ? null : this.onToggleAccount}
-      >
-        <CryptoCurrencyIconWithCount currency={account.currency} count={tokenCount} withTooltip />
-        <Box shrink grow ff="Inter|SemiBold" color="palette.text.shade100" fontSize={4}>
-          {onEditName ? (
-            <InputWrapper>
-              <Input
-                style={this.overflowStyles}
-                value={accountName}
-                onChange={this.handleChangeName}
-                onClick={this.onClickInput}
-                onEnter={this.handlePreventSubmit}
-                onFocus={this.onFocus}
-                onBlur={this.onBlur}
-                onKeyPress={this.handleKeyPress}
-                maxLength={getEnv("MAX_ACCOUNT_NAME_SIZE")}
-                editInPlace
-                autoFocus={autoFocusInput}
-              />
-            </InputWrapper>
-          ) : (
-            <div style={{ ...this.overflowStyles, paddingLeft: 15 }}>{accountName}</div>
-          )}
-        </Box>
-        {!hideAmount ? (
-          <FormattedVal
-            val={account.balance}
-            unit={account.unit}
-            style={{ textAlign: "right", width: "auto" }}
-            showCode
-            fontSize={4}
-            color="palette.text.shade60"
-          />
-        ) : null}
-        {!isDisabled && !isReadonly && <CheckBox disabled isChecked={isChecked || !!isDisabled} />}
-      </AccountRowContainer>
-    );
   }
+
+  const overflowStyles = { textOverflow: "ellipsis", overflow: "hidden", whiteSpace: "nowrap" };
+  const tokenCount = (account.subAccounts && account.subAccounts.length) || 0;
+
+  return (
+    <AccountRowContainer isDisabled={isDisabled} onClick={isDisabled ? null : this.onToggleAccount}>
+      <CryptoCurrencyIconWithCount currency={account.currency} count={tokenCount} withTooltip />
+      <Box shrink grow ff="Inter|SemiBold" color="palette.text.shade100" fontSize={4}>
+        {onEditName ? (
+          <InputWrapper>
+            <Input
+              style={overflowStyles}
+              value={accountName}
+              onChange={handleChangeName}
+              onClick={onClickInput}
+              onEnter={handlePreventSubmit}
+              onFocus={onFocus}
+              onBlur={onBlur}
+              onKeyPress={handleKeyPress}
+              maxLength={getEnv("MAX_ACCOUNT_NAME_SIZE")}
+              editInPlace
+              autoFocus={autoFocusInput}
+            />
+          </InputWrapper>
+        ) : (
+          <div style={{ ...this.overflowStyles, paddingLeft: 15 }}>{accountName}</div>
+        )}
+      </Box>
+      {!hideAmount ? (
+        <FormattedVal
+          val={account.balance}
+          unit={account.unit}
+          style={{ textAlign: "right", width: "auto" }}
+          showCode
+          fontSize={4}
+          color="palette.text.shade60"
+        />
+      ) : null}
+      {!isDisabled && !isReadonly && <CheckBox disabled isChecked={isChecked || !!isDisabled} />}
+    </AccountRowContainer>
+  );
 }
 
 const AccountRowContainer: ThemedComponent<{
@@ -152,5 +135,14 @@ const AccountRowContainer: ThemedComponent<{
 
   &:active {
     background-color: ${p => darken(p.theme.colors.palette.background.default, 0.03)};
+  }
+`;
+
+const InputWrapper = styled.div`
+  margin-left: 4px;
+
+  & > div > div {
+    padding-left: 10px;
+    padding-right: 10px;
   }
 `;
