c7cd1268e31732dd73c28cc62a766755df3fe540
Valentin D. Pinkman <valentin.d.pinkman@icloud.com>
2020-03-16 15:08:43 +0100

    timeout to jest

--- tests/specs/onboarding.spec.js
+++ tests/specs/onboarding.spec.js
@@ -8,7 +8,7 @@ describe("When I launch the app for the first time", () => {
   let onboardingPage;
   let modalPage;
 
-  jest.timeout(10e3);
+  jest.setTimeout(10e3);
 
   beforeAll(async () => {
     app = applicationProxy();
