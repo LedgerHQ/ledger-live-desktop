020fb7c020513e12d2ce06b8e8a27dda1a865157
LFBarreto <lfb.access@gmail.com>
2020-03-17 16:10:59 +0100

    (TRX): fix merge conflicts

--- src/live-common-setup.js
+++ src/live-common-setup.js
@@ -1,8 +1,8 @@
 // @flow
 import "./env";
 import "@ledgerhq/live-common/lib/load/tokens/ethereum/erc20";
-// import "@ledgerhq/live-common/lib/load/tokens/tron/trc10";
-// import "@ledgerhq/live-common/lib/load/tokens/tron/trc20";
+import "@ledgerhq/live-common/lib/load/tokens/tron/trc10";
+import "@ledgerhq/live-common/lib/load/tokens/tron/trc20";
 
 import { listen as listenLogs } from "@ledgerhq/logs";
 import logger from "./logger";
