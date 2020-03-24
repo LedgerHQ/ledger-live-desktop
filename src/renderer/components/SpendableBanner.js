26cd9b690dc51a1b952f4f751b6f4735175051e8
Valentin D. Pinkman <valentin.d.pinkman@icloud.com>
2020-03-24 17:08:46 +0100

    add a max spendable banner to send flow

---
+++ src/renderer/components/SpendableBanner.js
@@ -0,0 +1,84 @@
+// @flow
+import React, { useEffect, useState } from "react";
+import styled from "styled-components";
+import { useTranslation } from "react-i18next";
+import type { Account, AccountLike, Transaction } from "@ledgerhq/live-common/lib/types";
+import { getAccountUnit, getMainAccount } from "@ledgerhq/live-common/lib/account";
+import { getAccountBridge } from "@ledgerhq/live-common/lib/bridge";
+import type { ThemedComponent } from "~/renderer/styles/StyleProvider";
+import InfoCircle from "~/renderer/icons/InfoCircle";
+import FormattedVal from "~/renderer/components/FormattedVal";
+import Box from "./Box";
+import Text from "./Text";
+
+const Container: ThemedComponent<{}> = styled(Box).attrs(() => ({
+  horizontal: true,
+  py: 1,
+  px: 2,
+  bg: "palette.text.shade10",
+}))`
+  border-radius: 4px;
+  align-items: center;
+`;
+
+type Props = {
+  account: AccountLike,
+  transaction: Transaction,
+  parentAccount: ?Account,
+};
+
+const SpendableBanner = ({ account, parentAccount, transaction }: Props) => {
+  const { t } = useTranslation();
+  const [maxSpendable, setMaxSpendable] = useState(null);
+
+  const bridge = getAccountBridge(account, parentAccount);
+  const mainAccount = getMainAccount(account, parentAccount);
+  const accountUnit = getAccountUnit(mainAccount);
+
+  useEffect(() => {
+    let cancel = false;
+    async function getMaxSpendable() {
+      if (bridge && account) {
+        const estimate = await bridge.estimateMaxSpendable({
+          account,
+          parentAccount,
+          transaction,
+        });
+        if (estimate && !cancel) {
+          setMaxSpendable(estimate);
+        }
+      }
+    }
+
+    getMaxSpendable();
+
+    return () => {
+      cancel = true;
+    };
+  }, [bridge, account, parentAccount, transaction]);
+
+  return (
+    <Container>
+      <InfoCircle size={12} />
+      <Text ff="Inter|Medium" fontSize={3} style={{ paddingLeft: 8 }}>
+        {t("send.steps.amount.banner")}
+      </Text>
+      <Text ff="Inter|Bold" fontSize={3} style={{ paddingLeft: 4 }}>
+        ~
+      </Text>
+      <Text ff="Inter|Bold" fontSize={3} style={{ paddingLeft: 2 }}>
+        {maxSpendable ? (
+          <FormattedVal
+            style={{ width: "auto" }}
+            color="palette.text.shade100"
+            val={maxSpendable}
+            unit={accountUnit}
+            showCode
+          />
+        ) : null}
+      </Text>
+    </Container>
+  );
+};
+
+export default SpendableBanner;
