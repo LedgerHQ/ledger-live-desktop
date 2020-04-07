89eb0d77d93b083ca39b050003f100e02ed7c5ab
Juan Cortes Ross <juan@bohem.io>
2020-04-07 15:46:16 +0200

    LL-2366 Introduce shallowAccountSelector to fix multiple selectors

--- src/renderer/reducers/accounts.js
+++ src/renderer/reducers/accounts.js
@@ -1,6 +1,6 @@
 // @flow
 
-import { createSelector } from "reselect";
+import { createSelector, createSelectorCreator, defaultMemoize } from "reselect";
 import type { OutputSelector } from "reselect";
 import { handleActions } from "redux-actions";
 import type { Account, AccountLike } from "@ledgerhq/live-common/lib/types";
@@ -16,6 +16,7 @@ import { getEnv } from "@ledgerhq/live-common/lib/env";
 import logger from "./../../logger/logger";
 import accountModel from "./../../helpers/accountModel";
 import type { State } from ".";
+import isEqual from "lodash/isEqual";
 
 export type AccountsState = Account[];
 const state: AccountsState = [];
@@ -70,6 +71,20 @@ const handlers: Object = {
 
 export const accountsSelector = (state: { accounts: AccountsState }): Account[] => state.accounts;
 
+// NB some components don't need to refresh every time an account is updated, usually it's only
+// when the balance/name/length/starred of accounts changes.
+const accountHash = (a: AccountLike) =>
+  `${a.type === "Account" ? a.name : ""}-${a.id}${a.starred ? "-*" : ""}-${a.balance.toString()}`;
+
+const shallowAccountsSelectorCreator = createSelectorCreator(defaultMemoize, (a, b) =>
+  isEqual(flattenAccounts(a).map(accountHash), flattenAccounts(b).map(accountHash)),
+);
+export const shallowAccountsSelector: OutputSelector<
+  State,
+  void,
+  Account[],
+> = shallowAccountsSelectorCreator(accountsSelector, a => a);
+
 // FIXME we might reboot this idea later!
 export const activeAccountsSelector = accountsSelector;
 
@@ -88,7 +103,7 @@ export const isUpToDateSelector: OutputSelector<State, void, boolean> = createSe
 );
 
 export const hasAccountsSelector: OutputSelector<State, void, boolean> = createSelector(
-  accountsSelector,
+  shallowAccountsSelector,
   accounts => accounts.length > 0,
 );
 
@@ -99,12 +114,14 @@ export const someAccountsNeedMigrationSelector: OutputSelector<
 > = createSelector(accountsSelector, accounts => accounts.some(canBeMigrated));
 
 // TODO: FIX RETURN TYPE
-export const currenciesSelector: OutputSelector<State, void, *> = createSelector(
-  accountsSelector,
-  accounts =>
-    [...new Set(flattenAccounts(accounts).map(a => getAccountCurrency(a)))].sort((a, b) =>
-      a.name.localeCompare(b.name),
-    ),
+export const currenciesSelector: OutputSelector<
+  State,
+  void,
+  *,
+> = createSelector(shallowAccountsSelector, accounts =>
+  [...new Set(flattenAccounts(accounts).map(a => getAccountCurrency(a)))].sort((a, b) =>
+    a.name.localeCompare(b.name),
+  ),
 );
 
 // TODO: FIX RETURN TYPE
@@ -112,7 +129,7 @@ export const cryptoCurrenciesSelector: OutputSelector<
   State,
   void,
   *,
-> = createSelector(accountsSelector, accounts =>
+> = createSelector(shallowAccountsSelector, accounts =>
   [...new Set(accounts.map(a => a.currency))].sort((a, b) => a.name.localeCompare(b.name)),
 );
 
@@ -132,7 +149,9 @@ export const starredAccountsSelector: OutputSelector<
   State,
   void,
   AccountLike[],
-> = createSelector(accountsSelector, accounts => flattenAccounts(accounts).filter(a => a.starred));
+> = createSelector(shallowAccountsSelector, accounts =>
+  flattenAccounts(accounts).filter(a => a.starred),
+);
 
 export const isStarredAccountSelector = (s: *, { accountId }: { accountId: string }): boolean =>
   flattenAccounts(s.accounts).some(a => a.id === accountId && a.starred);
