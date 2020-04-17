f6d62449c6b5aa97515f1349db7e9d4f3b92808d
Juan Cortes Ross <juan@bohem.io>
2020-04-17 13:41:18 +0200

    LL-2326 Remove unused wording and components

--- src/config/urls.js
+++ src/config/urls.js
@@ -43,22 +43,6 @@ export const urls = {
   stakingTezos: "https://www.ledger.com/staking-tezos",
   stakingTron: "http://www.ledger.com/staking-tron",
 
-  // Exchanges
-  coinhouse: "https://www.coinhouse.com/r/157530?utm_source=legderlive&utm_medium=referral",
-  changelly: "https://changelly.com/?ref_id=aac789605a01",
-  coinmama: "http://go.coinmama.com/visit/?bta=51801&nci=5343",
-  simplex: "https://partners.simplex.com/?partner=ledger",
-  paybis: "https://aff-tracking.paybis.com/click?pid=22&offer_id=1",
-  luno: "http://luno.go2cloud.org/aff_c?offer_id=4&aff_id=1001&source=ledger",
-  shapeshift: "https://shapeshift.io/#/coins?affiliate=ledger",
-  genesis: "https://genesistrading.com/ledger-live/",
-  kyberSwap: "http://kyber.network/swap?ref=0xE2D8481eeF31CDA994833974FFfEccd576f8D71E",
-  changeNow: "https://changenow.io?link_id=80ab1d8ad846e7",
-  thorSwap:
-    "https://www.thorswap.com/?utm_source=Wallet&utm_medium=ledger&utm_campaign=EmbedLink&utm_content=Link1",
-  coinberry: "https://www.coinberry.com/?utm_source=ledger",
-  btcDirect: "https://btcdirect.eu/en-gb?partnerId=261",
-
   // Errors
   errors: {
     CantOpenDevice: "https://support.ledger.com/hc/en-us/articles/115005165269",
