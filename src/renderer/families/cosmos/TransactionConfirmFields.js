355c51e8637877a4a16d7c06a73e7da4550382a6
LFBarreto <lfb.access@gmail.com>
2020-05-19 17:30:30 +0200

    (csms): fix memo issue + transaction confirm fields prepared

--- src/renderer/families/cosmos/TransactionConfirmFields.js
+++ src/renderer/families/cosmos/TransactionConfirmFields.js
@@ -6,11 +6,13 @@ import styled from "styled-components";
 import { Trans } from "react-i18next";
 import { BigNumber } from "bignumber.js";
 
-import type { AccountLike, Account, Transaction } from "@ledgerhq/live-common/lib/types";
+import type { Transaction } from "@ledgerhq/live-common/lib/types";
 import type { ThemedComponent } from "~/renderer/styles/StyleProvider";
+import type { FieldComponentProps } from "~/renderer/components/TransactionConfirm";
 
 import { getAccountUnit, getMainAccount } from "@ledgerhq/live-common/lib/account";
 import { formatCurrencyUnit } from "@ledgerhq/live-common/lib/currencies";
+import { useCosmosPreloadData } from "@ledgerhq/live-common/lib/families/cosmos/react";
 
 import TransactionConfirmField from "~/renderer/components/TransactionConfirm/TransactionConfirmField";
 import Text from "~/renderer/components/Text";
@@ -18,12 +20,12 @@ import WarnBox from "~/renderer/components/WarnBox";
 import Box from "~/renderer/components/Box";
 
 import { formatDelegationsInfo } from "./operationDetails";
-import { useCosmosPreloadData } from "@ledgerhq/live-common/lib/families/cosmos/react";
+
 import {
   OpDetailsData,
   OpDetailsVoteData,
-  Address,
 } from "~/renderer/modals/OperationDetails/styledComponents";
+import FormattedVal from "~/renderer/components/FormattedVal";
 
 const Info: ThemedComponent<{}> = styled(Box).attrs(() => ({
   ff: "Inter|SemiBold",
@@ -35,7 +37,7 @@ const Info: ThemedComponent<{}> = styled(Box).attrs(() => ({
   text-align: center;
 `;
 
-const AddressText = styled(Text).attrs(() => ({
+const FieldText = styled(Text).attrs(() => ({
   ml: 1,
   ff: "Inter|Medium",
   color: "palette.text.shade80",
@@ -46,15 +48,24 @@ const AddressText = styled(Text).attrs(() => ({
   max-width: 50%;
 `;
 
-const CosmosValidatorsField = ({
+const AddressText = styled(Text).attrs(() => ({
+  ml: 1,
+  ff: "Inter|Medium",
+  color: "palette.text.shade80",
+  fontSize: 3,
+}))`
+  word-break: break-all;
+  text-align: right;
+  max-width: 50%;
+  text-transform: capitalize;
+`;
+
+const CosmosDelegateValidatorsField = ({
   account,
   parentAccount,
   transaction,
-}: {
-  account: AccountLike,
-  parentAccount: ?Account,
-  transaction: Transaction,
-}) => {
+  field,
+}: FieldComponentProps) => {
   const mainAccount = getMainAccount(account, parentAccount);
 
   invariant(transaction.family === "cosmos", "cosmos transaction");
@@ -66,26 +77,11 @@ const CosmosValidatorsField = ({
 
   const formatedValidators = formatDelegationsInfo(validators || [], cosmosValidators);
 
-  return transaction.mode === "claimReward" || transaction.mode === "claimRewardCompound" ? (
-    <>
-      <TransactionConfirmField label="Validator">
-        <AddressText ff="Inter|SemiBold">{formatedValidators[0].address}</AddressText>
-      </TransactionConfirmField>
-      <TransactionConfirmField label="Reward amount">
-        <AddressText ff="Inter|SemiBold">
-          {formatCurrencyUnit(unit, formatedValidators[0].amount, {
-            disableRounding: false,
-            alwaysShowSign: false,
-            showCode: true,
-          })}
-        </AddressText>
-      </TransactionConfirmField>
-    </>
-  ) : (
-    formatedValidators && formatedValidators.length > 0 && (
+  return (
+    formatedValidators &&
+    formatedValidators.length > 0 && (
       <Box vertical justifyContent="space-between" mb={2}>
-        <TransactionConfirmField label={`Validators (${formatedValidators.length})`} />
-
+        <TransactionConfirmField label={field.label} />
         {formatedValidators
           .map(({ amount, ...delegation }) => ({
             ...delegation,
@@ -113,7 +109,7 @@ const CosmosValidatorsField = ({
                     </Trans>
                   </Text>
                 </Box>
-                <Address>{address}</Address>
+                <AddressText>{address}</AddressText>
               </OpDetailsVoteData>
             </OpDetailsData>
           ))}
@@ -122,15 +118,77 @@ const CosmosValidatorsField = ({
   );
 };
 
+const CosmosValidatorNameField = ({
+  account,
+  parentAccount,
+  transaction,
+  field,
+}: FieldComponentProps) => {
+  invariant(transaction.family === "cosmos", "cosmos transaction");
+
+  const { validators } = transaction;
+  const { validators: cosmosValidators } = useCosmosPreloadData();
+
+  const formattedValidator = useMemo(
+    () =>
+      validators && validators.length > 0
+        ? cosmosValidators.find(v => v.validatorAddress === validators[0].address)
+        : null,
+    [cosmosValidators, validators],
+  );
+
+  return (
+    formattedValidator && (
+      <TransactionConfirmField label={field.label}>
+        <FieldText>
+          <Text ff="Inter|Medium" fontSize={4}>
+            {formattedValidator.name}
+          </Text>
+          <br />
+          <AddressText ff="Inter|Regular" fontSize={2}>
+            {formattedValidator.validatorAddress}
+          </AddressText>
+        </FieldText>
+      </TransactionConfirmField>
+    )
+  );
+};
+
+const CosmosValidatorAmountField = ({
+  account,
+  parentAccount,
+  transaction,
+  field,
+}: FieldComponentProps) => {
+  const mainAccount = getMainAccount(account, parentAccount);
+
+  invariant(transaction.family === "cosmos", "cosmos transaction");
+
+  const unit = getAccountUnit(mainAccount);
+
+  const { validators } = transaction;
+
+  return validators && validators.length > 0 ? (
+    <TransactionConfirmField label={field.label}>
+      <FieldText>
+        <FormattedVal
+          color={"palette.text.shade80"}
+          unit={unit}
+          val={validators[0].amount}
+          fontSize={3}
+          showCode
+        />
+      </FieldText>
+    </TransactionConfirmField>
+  ) : null;
+};
+
 const CosmosSourceValidatorField = ({
   account,
   parentAccount,
   transaction,
-}: {
-  account: AccountLike,
-  parentAccount: ?Account,
-  transaction: Transaction,
-}) => {
+  field,
+}: FieldComponentProps) => {
   invariant(transaction.family === "cosmos", "cosmos transaction");
 
   const { cosmosSourceValidator } = transaction;
@@ -141,17 +199,17 @@ const CosmosSourceValidatorField = ({
   );
 
   return (
-    cosmosSourceValidator && (
-      <TransactionConfirmField label="From">
-        <Box alignContent="flex-end" vertical>
+    formattedValidator && (
+      <TransactionConfirmField label={field.label}>
+        <FieldText>
           <Text ff="Inter|Medium" fontSize={4}>
-            {formattedValidator && formattedValidator.name}
+            {formattedValidator.name}
           </Text>
-          <AddressText ff="Inter|Regular" fontSize={3}>
-            {cosmosSourceValidator.slice(0, 1).toUpperCase() +
-              cosmosSourceValidator.slice(1).toLowerCase()}
+          <br />
+          <AddressText ff="Inter|Regular" fontSize={2}>
+            {formattedValidator.validatorAddress}
           </AddressText>
-        </Box>
+        </FieldText>
       </TransactionConfirmField>
     )
   );
@@ -193,8 +251,10 @@ const Title = ({ transaction }: { transaction: Transaction }) => {
 };
 
 const fieldComponents = {
-  "cosmos.cosmosSourceValidator": CosmosSourceValidatorField,
-  "cosmos.validators": CosmosValidatorsField,
+  "cosmos.delegateValidators": CosmosDelegateValidatorsField,
+  "cosmos.validatorName": CosmosValidatorNameField,
+  "cosmos.validatorAmount": CosmosValidatorAmountField,
+  "cosmos.sourceValidatorName": CosmosSourceValidatorField,
 };
 
 export default {
