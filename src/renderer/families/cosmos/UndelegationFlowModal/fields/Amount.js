259e099919bad7d0620d0225e9e0c42379263f50
LFBarreto <lfb.access@gmail.com>
2020-05-21 20:53:24 +0200

    LL-2512 LL-2511 (#2926)
    * impl onChange manage drop down
    
    * setup steps
    
    * add footer
    
    * show options
    
    * filter by query
    
    * change validator
    
    * initially place selected validator
    
    * add useEffectOnce
    
    * LL-2515 (csms): add base redelegation flow stepper
    
    * LL-2514 (csms): redelegation flow information step added
    
    * move validator field
    
    * markup AmountField
    
    * add select options
    
    * show collect error messages
    
    * markup confirmation screen
    
    * LL-2516 LL-2511 (csms): add redelegatio + undelegation flow
    
    * fix
    
    * (csms): redelegation fix scrolling + amount field issues
    
    * (csms): fix memo issue + transaction confirm fields prepared
    
    Co-authored-by: JunichiSugiura <jun.sugiura.jp@gmail.com>

---
+++ src/renderer/families/cosmos/UndelegationFlowModal/fields/Amount.js
@@ -0,0 +1,144 @@
+// @flow
+import React, { useMemo, useState, useEffect } from "react";
+import { BigNumber } from "bignumber.js";
+import { useTranslation } from "react-i18next";
+import styled from "styled-components";
+import { getAccountUnit } from "@ledgerhq/live-common/lib/account";
+import type { Account, TransactionStatus } from "@ledgerhq/live-common/lib/types";
+import Box from "~/renderer/components/Box";
+import InputCurrency from "~/renderer/components/InputCurrency";
+import Label from "~/renderer/components/Label";
+import type { ThemedComponent } from "~/renderer/styles/StyleProvider";
+
+type Props = {
+  amount: BigNumber,
+  validator: *,
+  account: Account,
+  status: TransactionStatus,
+  onChange: (amount: BigNumber) => void,
+};
+
+export default function AmountField({
+  amount,
+  validator,
+  account,
+  onChange,
+  status: { errors, warnings },
+}: Props) {
+  const { t } = useTranslation();
+  const unit = getAccountUnit(account);
+
+  const [currentValidator, setCurrentValidator] = useState(validator);
+  const [initialAmount, setInitialAmount] = useState(validator ? validator.amount : BigNumber(0));
+
+  useEffect(() => {
+    if (validator && validator.address !== currentValidator.address) {
+      setCurrentValidator(validator);
+      setInitialAmount(validator.amount);
+    }
+  }, [validator, currentValidator]);
+
+  const options = useMemo(
+    () => [
+      {
+        label: "25%",
+        value: initialAmount.multipliedBy(0.25),
+      },
+      {
+        label: "50%",
+        value: initialAmount.multipliedBy(0.5),
+      },
+      {
+        label: "75%",
+        value: initialAmount.multipliedBy(0.75),
+      },
+      {
+        label: "100%",
+        value: initialAmount,
+      },
+    ],
+    [initialAmount],
+  );
+
+  const error = useMemo(() => Object.values(errors || {})[0], [errors]);
+  const warning = useMemo(() => Object.values(warnings || {})[0], [warnings]);
+
+  return (
+    <Box mt={5}>
+      <Label>{t("cosmos.undelegation.flow.steps.amount.fields.amount")}</Label>
+      <InputCurrency
+        autoFocus={false}
+        error={error}
+        warning={warning}
+        containerProps={{ grow: true }}
+        unit={unit}
+        value={amount}
+        onChange={onChange}
+        renderLeft={<InputLeft>{unit.code}</InputLeft>}
+        renderRight={
+          <InputRight>
+            {options.map(({ label, value }) => (
+              <AmountButton
+                active={value.eq(amount)}
+                key={label}
+                error={!!error}
+                onClick={() => onChange(value)}
+              >
+                {label}
+              </AmountButton>
+            ))}
+          </InputRight>
+        }
+      />
+    </Box>
+  );
+}
+
+const InputLeft = styled(Box).attrs(() => ({
+  ff: "Inter|Medium",
+  color: "palette.text.shade60",
+  fontSize: 4,
+  justifyContent: "center",
+  horizontal: true,
+  pl: 3,
+}))``;
+
+const InputRight = styled(Box).attrs(() => ({
+  ff: "Inter|Medium",
+  color: "palette.text.shade60",
+  fontSize: 4,
+  justifyContent: "center",
+  horizontal: true,
+}))`
+  padding: ${p => p.theme.space[2]}px;
+`;
+
+const AmountButton: ThemedComponent<{ error: boolean, active: boolean }> = styled.button.attrs(
+  () => ({
+    type: "button",
+  }),
+)`
+  background-color: ${p =>
+    p.error
+      ? p.theme.colors.lightRed
+      : p.active
+      ? p.theme.colors.palette.primary.main
+      : p.theme.colors.palette.action.hover};
+  color: ${p =>
+    p.active
+      ? p.theme.colors.palette.primary.contrastText
+      : p.theme.colors.palette.primary.main}!important;
+  border: none;
+  border-radius: 4px;
+  padding: 0px ${p => p.theme.space[2]}px;
+  margin: 0 2.5px;
+  font-size: 12px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  cursor: pointer;
+  transition: all 200ms ease-out;
+  &:hover {
+    filter: contrast(2);
+  }
+`;
---
+++ src/renderer/families/cosmos/UndelegationFlowModal/steps/Amount.js
@@ -0,0 +1,103 @@
+// @flow
+import { BigNumber } from "bignumber.js";
+import invariant from "invariant";
+import React, { useCallback, useMemo } from "react";
+import { useTranslation } from "react-i18next";
+import { getAccountBridge } from "@ledgerhq/live-common/lib/bridge";
+import TrackPage from "~/renderer/analytics/TrackPage";
+import Box from "~/renderer/components/Box";
+import Button from "~/renderer/components/Button";
+import type { FormattedDelegation } from "../../Delegation";
+import { ValidatorField, AmountField } from "../fields";
+import type { StepProps } from "../types";
+
+export default function StepAmount({
+  account,
+  transaction,
+  bridgePending,
+  onUpdateTransaction,
+  status,
+  validatorAddress,
+}: StepProps) {
+  invariant(account && transaction && transaction.validators, "account and transaction required");
+
+  const bridge = getAccountBridge(account);
+
+  const updateValidator = useCallback(
+    validatorFields => {
+      onUpdateTransaction(tx =>
+        bridge.updateTransaction(tx, {
+          ...tx,
+          validators:
+            tx.validators && tx.validators.length > 0
+              ? [{ ...tx.validators[0], ...validatorFields }]
+              : [validatorFields],
+        }),
+      );
+    },
+    [onUpdateTransaction, bridge],
+  );
+
+  const onChangeValidator = useCallback(
+    ({ address, amount }: FormattedDelegation) => {
+      updateValidator({ address, amount });
+    },
+    [updateValidator],
+  );
+
+  const onChangeAmount = useCallback(
+    (amount: BigNumber) => {
+      updateValidator({ amount });
+    },
+    [updateValidator],
+  );
+
+  const validator = useMemo(() => transaction.validators && transaction.validators[0], [
+    transaction,
+  ]);
+
+  const amount = useMemo(() => (validator ? validator.amount : BigNumber(0)), [validator]);
+
+  return (
+    <Box flow={1}>
+      <TrackPage category="Undelegation Flow" name="Step 1" />
+      <ValidatorField account={account} transaction={transaction} onChange={onChangeValidator} />
+      <AmountField
+        amount={amount}
+        validator={validator}
+        account={account}
+        status={status}
+        onChange={onChangeAmount}
+      />
+    </Box>
+  );
+}
+
+export function StepAmountFooter({
+  transitionTo,
+  account,
+  onClose,
+  status: { errors = {} },
+  bridgePending,
+  transaction,
+}: StepProps) {
+  const { t } = useTranslation();
+
+  invariant(account, "account required");
+
+  const hasErrors = !!Object.keys(errors).length;
+  const canNext = !bridgePending && !hasErrors;
+
+  return (
+    <>
+      <Box horizontal>
+        <Button mr={1} secondary onClick={onClose}>
+          {t("common.cancel")}
+        </Button>
+        <Button disabled={!canNext} primary onClick={() => transitionTo("device")}>
+          {t("common.continue")}
+        </Button>
+      </Box>
+    </>
+  );
+}
