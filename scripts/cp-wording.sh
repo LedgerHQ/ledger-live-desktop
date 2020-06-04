12fb54b9769d1bcc6d97d2dec55ab4f1e8d5c41a
Gaëtan Renaudeau <renaudeau.gaetan@gmail.com>
2020-06-04 09:14:22 +0200

    readd missing cp-wording.sh scripts

---
+++ scripts/cp-wording.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+# change path of a wording in i18n files
+# Usage: ./scripts/mv-wording.sh foo.bar foo.new.location
+
+set -e
+cd $(dirname $0)/..
+
+OLD=$1
+NEW=$2
+
+if [ -z "$OLD" ] || [ -z "$NEW" ] ; then
+  echo "Usage: ./scripts/mv-wording.sh foo.bar foo.new.location" >&2
+  exit 1
+fi
+
+for f in static/i18n/*/app.json; do
+  tmp=`mktemp`
+  cat $f | jq ".$NEW=.$OLD" > $tmp
+  mv $tmp $f
+done
