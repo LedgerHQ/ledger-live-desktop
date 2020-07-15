7aa51877f7473666706e24335453af055e20627e
Benihoud Khalil <khalil@ledger.fr>
2020-07-15 17:07:18 +0200

    fixed accessibility issues on the stepper

--- src/renderer/components/Stepper/Step.js
+++ src/renderer/components/Stepper/Step.js
@@ -31,18 +31,18 @@ const Wrapper: ThemedComponent<{ status: Status }> = styled(Box).attrs((p: { sta
 `;
 
 const StepNumber = styled(Box).attrs(p => ({
-  alignItems: "center",
-  justifyContent: "center",
   color: ["active", "valid", "error"].includes(p.status)
     ? "palette.primary.contrastText"
-    : "palette.text.shade30",
+    : "palette.text.shade40",
   bg: ["active", "valid"].includes(p.status)
     ? "palette.primary.main"
     : p.status === "error"
     ? "alertRed"
     : "palette.background.paper",
-  ff: "Inter|Bold",
+  ff: p.status === "active" ? "Inter|Bold" : "Inter|SemiBold",
 }))`
+  display: inline;
+  text-align: center;
   border-radius: 50%;
   border: 1px solid
     ${p =>
@@ -53,7 +53,8 @@ const StepNumber = styled(Box).attrs(p => ({
         : p.theme.colors.palette.text.shade30};
   font-size: 10px;
   height: ${RADIUS}px;
-  line-height: 10px;
+  line-height: ${RADIUS}px;
+  box-sizing: content-box;
   transition: all ease-in-out 0.1s ${p => (["active", "valid"].includes(p.status) ? 0.4 : 0)}s;
   width: ${RADIUS}px;
 `;
