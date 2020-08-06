c6434cbbf57ffbb9a726864d346d841d40968268
Thibaut Boustany <thibaut@kastor.fr>
2020-08-06 17:28:33 +0200

    Increase emphasis on "help" on connect banner

--- src/renderer/components/ConnectTroubleshooting.js
+++ src/renderer/components/ConnectTroubleshooting.js
@@ -4,8 +4,7 @@ import { Trans } from "react-i18next";
 import styled from "styled-components";
 import Box from "~/renderer/components/Box";
 import Text from "~/renderer/components/Text";
-import ConnectTroubleshootingHelpLink from "~/renderer/components/ConnectTroubleshootingHelpLink";
-import IconHelp from "~/renderer/icons/Help";
+import ConnectTroubleshootingHelpButton from "~/renderer/components/ConnectTroubleshootingHelpButton";
 import RepairDeviceButton from "~/renderer/components/RepairDeviceButton";
 import type { ThemedComponent } from "~/renderer/styles/StyleProvider";
 
@@ -34,16 +33,13 @@ const ConnectTroubleshooting = ({ appearsAfterDelay = 15000, onRepair }: Props) 
   return visible ? (
     <Wrapper p={2} horizontal alignItems="center">
       <Box p={2} horizontal justifyContent="center">
-        <Box marginRight="8px" justifyContent="center">
-          <IconHelp size={16} />
-        </Box>
         <Text ff="Inter|Regular" fontSize={4}>
           <Trans i18nKey="connectTroubleshooting.desc" />
         </Text>
       </Box>
-      <ConnectTroubleshootingHelpLink />
-      <Box flex="1" />
       {onRepair ? <RepairDeviceButton onRepair={onRepair} /> : null}
+      <Box flex="1" />
+      <ConnectTroubleshootingHelpButton buttonProps={{ primary: true }} />
     </Wrapper>
   ) : null;
 };
