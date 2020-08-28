7d6b039a38e92fcc58507e8fe35bd14e3cd569de
Valentin D. Pinkman <valentin.d.pinkman@icloud.com>
2020-08-28 15:34:27 +0200

    add new format to firmware hash

--- src/renderer/modals/UpdateFirmwareModal/steps/01-step-install-full-firmware.js
+++ src/renderer/modals/UpdateFirmwareModal/steps/01-step-install-full-firmware.js
@@ -90,7 +90,10 @@ const Body = ({
           {t("manager.modal.identifier")}
         </Text>
         <Identifier>
-          {firmware.osu && manager.formatHashName(firmware.osu.hash, deviceModelId, deviceInfo)}
+          {firmware.osu &&
+            manager
+              .formatHashName(firmware.osu.hash, deviceModelId, deviceInfo)
+              .map(hash => <span key={hash}>{hash}</span>)}
         </Identifier>
       </Box>
       <Box mt={isBlue ? 4 : null}>
