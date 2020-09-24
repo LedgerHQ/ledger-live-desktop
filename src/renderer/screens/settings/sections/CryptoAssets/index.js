e98d1ba0c949412bd954e75ad917c0b939035735
JunichiSugiura <jun.sugiura.jp@gmail.com>
2020-09-24 14:36:44 +0200

    remove Rates setting

--- src/renderer/screens/settings/sections/CryptoAssets/index.js
+++ src/renderer/screens/settings/sections/CryptoAssets/index.js
@@ -1,12 +1,19 @@
 // @flow
-
 import React from "react";
 import styled from "styled-components";
 import TrackPage from "~/renderer/analytics/TrackPage";
 import type { ThemedComponent } from "~/renderer/styles/StyleProvider";
 import { SettingsSection as Section } from "../../SettingsSection";
 import Currencies from "./Currencies";
-import Rates from "./Rates";
+
+export default function TabCryptoAssets() {
+  return (
+    <Wrapper>
+      <TrackPage category="Settings" name="Currencies" />
+      <Currencies />
+    </Wrapper>
+  );
+}
 
 const Wrapper: ThemedComponent<{}> = styled.div`
   > ${Section} {
@@ -15,13 +22,3 @@ const Wrapper: ThemedComponent<{}> = styled.div`
     }
   }
 `;
-
-const TabCryptoAssets = () => (
-  <Wrapper>
-    <TrackPage category="Settings" name="Currencies" />
-    <Currencies />
-    <Rates />
-  </Wrapper>
-);
-
-export default TabCryptoAssets;
