8e9e05f4553bb224c317200fb804e67b4dca711a
Benihoud Khalil <khalil@ledger.fr>
2020-09-29 13:44:15 +0100

    new fixes

--- src/renderer/screens/settings/sections/Accounts/index.js
+++ src/renderer/screens/settings/sections/Accounts/index.js
@@ -3,7 +3,7 @@
 import React from "react";
 import { useTranslation } from "react-i18next";
 import TrackPage from "~/renderer/analytics/TrackPage";
-import EyeSlash from "~/renderer/icons/EyeSlash";
+import EyeOff from "~/renderer/icons/EyeOff";
 import { SettingsSection as Section, SettingsSectionHeader as Header } from "../../SettingsSection";
 import HideEmptyTokenAccountsToggle from "./HideEmptyTokenAccountsToggle";
 import SectionExport from "./Export";
@@ -18,7 +18,7 @@ const SectionAccounts = () => {
         <TrackPage category="Settings" name="Accounts" />
 
         <Header
-          icon={<EyeSlash />}
+          icon={<EyeOff />}
           title={t("settings.accounts.hideEmptyTokens.title")}
           desc={t("settings.accounts.hideEmptyTokens.desc")}
           renderRight={<HideEmptyTokenAccountsToggle />}
