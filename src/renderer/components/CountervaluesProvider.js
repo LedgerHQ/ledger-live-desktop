15387193b32cd3fae646ce4aef968e4647a5dfc6
JunichiSugiura <jun.sugiura.jp@gmail.com>
2020-10-02 13:04:49 +0200

    fix hourly chart

--- src/renderer/components/CountervaluesProvider.js
+++ src/renderer/components/CountervaluesProvider.js
@@ -1,15 +1,17 @@
 // @flow
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useMemo } from "react";
 import { useSelector } from "react-redux";
 import {
   Countervalues,
   useCountervaluesExport,
 } from "@ledgerhq/live-common/lib/countervalues/react";
-import { trackingPairsSelector } from "~/renderer/countervalues";
+import { inferTrackingPairForAccounts } from "@ledgerhq/live-common/lib/countervalues/logic";
 import { setKey, getKey } from "~/renderer/storage";
+import { accountsSelector } from "~/renderer/reducers/accounts";
+import { counterValueCurrencySelector } from "~/renderer/reducers/settings";
 
 export default function CountervaluesProvider({ children }: { children: React$Node }) {
-  const trackingPairs = useSelector(trackingPairsSelector);
+  const trackingPairs = useTrackingPairs();
   const [initialCountervalues, setInitialCuntervalues] = useState();
 
   useEffect(() => {
@@ -38,6 +40,15 @@ function CountervaluesPersist({ children }: { children: React$Node }) {
   }, [rawState]);
 
   return children;
+}
+
+export function useTrackingPairs() {
+  const accounts = useSelector(accountsSelector);
+  const countervalue = useSelector(counterValueCurrencySelector);
+  return useMemo(() => inferTrackingPairForAccounts(accounts, countervalue), [
+    accounts,
+    countervalue,
+  ]);
 }
 
 // TODO countervalue
