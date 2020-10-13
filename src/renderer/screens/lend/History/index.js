f78376ee19b1b20bb7cc5087944d5afe12cccb6e
Valentin D. Pinkman <valentin.d.pinkman@icloud.com>
2020-10-13 14:55:30 +0200

    compound operations history

--- src/renderer/screens/lend/History/index.js
+++ src/renderer/screens/lend/History/index.js
@@ -1,24 +1,52 @@
 // @flow
-import React from "react";
+import React, { useMemo } from "react";
 import { useTranslation } from "react-i18next";
-// import type { CompoundAccountSummary } from "@ledgerhq/live-common/lib/compound/types";
+import type { AccountLikeArray, Operation } from "@ledgerhq/live-common/lib/types";
 import Box from "~/renderer/components/Box";
+import OperationsList from "~/renderer/components/OperationsList";
 import EmptyState from "../EmptyState";
 
 type Props = {
   navigateToCompoundDashboard: () => void,
+  accounts: AccountLikeArray,
 };
 
-const History = ({ navigateToCompoundDashboard }: Props) => {
+const useCompoundHistory = (accounts: AccountLikeArray): AccountLikeArray => {
+  const filterOps = (op: Operation): boolean => ["REDEEM", "SUPPLY"].includes(op.type);
+  const history = useMemo(
+    () =>
+      accounts.map(acc => {
+        const operations = acc.operations.filter(filterOps);
+        console.log(operations);
+        return {
+          ...acc,
+          operations,
+        };
+      }),
+    [accounts],
+  );
+
+  return history;
+};
+
+const History = ({ navigateToCompoundDashboard, accounts }: Props) => {
   const { t } = useTranslation();
+  const history = useCompoundHistory(accounts);
+
+  console.log({ history });
+
   return (
     <Box>
-      <EmptyState
-        title={t("lend.emptyState.history.title")}
-        description={t("lend.emptyState.history.description")}
-        buttonLabel={t("lend.emptyState.history.cta")}
-        onClick={navigateToCompoundDashboard}
-      />
+      {history.length ? (
+        <OperationsList accounts={history} />
+      ) : (
+        <EmptyState
+          title={t("lend.emptyState.history.title")}
+          description={t("lend.emptyState.history.description")}
+          buttonLabel={t("lend.emptyState.history.cta")}
+          onClick={navigateToCompoundDashboard}
+        />
+      )}
     </Box>
   );
 };
