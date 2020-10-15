110c38a7b3cc49796b170891dbf3aedd1fd2c5c6
LFBarreto <lfb.access@gmail.com>
2020-10-15 15:20:04 +0200

    LL-3691 fix ui select account enabled indicator

--- src/renderer/screens/lend/modals/SelectAccountStep.js
+++ src/renderer/screens/lend/modals/SelectAccountStep.js
@@ -1,9 +1,9 @@
 // @flow
 
-import React, { useCallback, useState } from "react";
+import React, { useCallback, useState, useMemo } from "react";
 import { useDispatch, connect } from "react-redux";
 import { createStructuredSelector } from "reselect";
-import { useTranslation } from "react-i18next";
+import { useTranslation, Trans } from "react-i18next";
 
 import {
   getAccountCurrency,
@@ -11,34 +11,57 @@ import {
   getAccountName,
 } from "@ledgerhq/live-common/lib/account";
 
-import type { AccountLike, CryptoCurrency, TokenCurrency } from "@ledgerhq/live-common/lib/types";
-import type { CompoundAccountSummary } from "@ledgerhq/live-common/lib/compound/types";
+import type {
+  AccountLike,
+  TokenAccount,
+  CryptoCurrency,
+  TokenCurrency,
+} from "@ledgerhq/live-common/lib/types";
+import type { CompoundAccountStatus } from "@ledgerhq/live-common/lib/compound/types";
+import { BigNumber } from "bignumber.js";
 
 import { openModal, closeModal } from "~/renderer/actions/modals";
 import Box from "~/renderer/components/Box";
 import Modal, { ModalBody } from "~/renderer/components/Modal";
-import { subAccountByCurrencyOrderedSelector } from "~/renderer/reducers/accounts";
+import { subAccountByCurrencyOrderedSelector, getAccountById } from "~/renderer/reducers/accounts";
 import Button from "~/renderer/components/Button";
 import Label from "~/renderer/components/Label";
 import Select from "~/renderer/components/Select";
 import CryptoCurrencyIcon from "~/renderer/components/CryptoCurrencyIcon";
 import Ellipsis from "~/renderer/components/Ellipsis";
 import FormattedVal from "~/renderer/components/FormattedVal";
+import { getAccountCapabilities } from "@ledgerhq/live-common/lib/compound/logic";
+import ToolTip from "~/renderer/components/Tooltip";
+import ExclamationCircle from "~/renderer/icons/ExclamationCircle";
+
+type Capabilities = {
+  enabledAmount: BigNumber,
+  enabledAmountIsUnlimited: boolean,
+  canSupply: boolean,
+  canSupplyMax: boolean,
+  canWithdraw: boolean,
+  status: CompoundAccountStatus,
+};
 
 export function AccountOption({
   account,
   isValue,
   disabled,
 }: {
-  account: AccountLike,
+  account: TokenAccount & {
+    capabilities: ?Capabilities,
+  },
   isValue?: boolean,
   disabled?: boolean,
 }) {
   const currency = getAccountCurrency(account);
   const unit = getAccountUnit(account);
   const name = getAccountName(account);
-
-  // @TODO show enable or not enabled ticker once info is available on account data
+  const { capabilities } = account;
+  const isEnabled =
+    capabilities &&
+    ((capabilities.enabledAmount && capabilities.enabledAmount.gt(0)) ||
+      capabilities.enabledAmountIsUnlimited);
 
   return (
     <Box grow horizontal alignItems="center" flow={2} style={{ opacity: disabled ? 0.2 : 1 }}>
@@ -51,17 +74,32 @@ export function AccountOption({
       <Box>
         <FormattedVal color="palette.text.shade60" val={account.balance} unit={unit} showCode />
       </Box>
+      <Box color="warning" style={{ width: 16 }}>
+        {!isEnabled && (
+          <ToolTip content={<Trans i18nKey="lend.enable.steps.selectAccount.alreadyEnabled" />}>
+            <ExclamationCircle size={16} />
+          </ToolTip>
+        )}
+      </Box>
     </Box>
   );
 }
 
-export const renderValue = ({ data }: { data: AccountLike }) =>
-  data ? <AccountOption account={data} isValue /> : null;
+export const renderValue = ({
+  data,
+}: {
+  data: TokenAccount & {
+    capabilities: ?Capabilities,
+  },
+}) => (data ? <AccountOption account={data} isValue /> : null);
 
-export const renderOption = ({ data }: { data: AccountLike }) => {
-  console.warn(data);
-  return data ? <AccountOption account={data} /> : null;
-};
+export const renderOption = ({
+  data,
+}: {
+  data: TokenAccount & {
+    capabilities: ?Capabilities,
+  },
+}) => (data ? <AccountOption account={data} /> : null);
 
 export const getOptionValue = (option?: { id: string }) => option && option.id;
 
@@ -71,30 +109,58 @@ type Props = {
   accounts: AccountLike[],
   nextStep: string,
   cta: React$Node,
+  getAccount: (id: string) => ?AccountLike,
   ...
-} & CompoundAccountSummary;
+};
 
-const SelectAccountStepModal = ({ name, currency, accounts, nextStep, cta, ...rest }: Props) => {
+const SelectAccountStepModal = ({
+  name,
+  currency,
+  accounts,
+  nextStep,
+  cta,
+  getAccount,
+  ...rest
+}: Props) => {
   const dispatch = useDispatch();
   const { t } = useTranslation();
 
-  const [account, setAccount] = useState(accounts[0]);
+  const formattedAccounts = useMemo(
+    () =>
+      accounts.map(a => ({
+        ...a,
+        capabilities: a.type === "TokenAccount" && getAccountCapabilities(a),
+      })),
+    [accounts],
+  );
+
+  const [account, setAccount] = useState(formattedAccounts[0]);
 
   const onClose = useCallback(() => {
     dispatch(closeModal(name));
   }, [name, dispatch]);
 
   const onNext = useCallback(() => {
+    const { capabilities } = account;
+    const isEnabled =
+      capabilities &&
+      ((capabilities.enabledAmount && capabilities.enabledAmount.gt(0)) ||
+        capabilities.enabledAmountIsUnlimited);
+    const parentAccount =
+      account.parentId && typeof account.parentId === "string"
+        ? getAccount(account.parentId)
+        : null;
     onClose();
     dispatch(
-      openModal(nextStep, {
+      openModal(isEnabled ? "MODAL_LEND_SUPPLY" : nextStep, {
         ...rest,
         currency,
         account,
         accountId: account.parentId ? account.parentId : null,
+        parentAccount,
       }),
     );
-  }, [onClose, dispatch, nextStep, rest, currency, account]);
+  }, [onClose, account, getAccount, dispatch, nextStep, rest, currency]);
 
   const onChangeAccount = useCallback(
     a => {
@@ -121,7 +187,7 @@ const SelectAccountStepModal = ({ name, currency, accounts, nextStep, cta, ...re
               <Label>{t("lend.enable.steps.selectAccount.selectLabel")}</Label>
               <Select
                 value={account}
-                options={accounts}
+                options={formattedAccounts}
                 getOptionValue={getOptionValue}
                 renderValue={renderValue}
                 renderOption={renderOption}
@@ -151,6 +217,7 @@ const SelectAccountStepModal = ({ name, currency, accounts, nextStep, cta, ...re
 
 const mapStateToProps = createStructuredSelector({
   accounts: subAccountByCurrencyOrderedSelector,
+  getAccount: getAccountById,
 });
 
 const m: React$ComponentType<Props> = connect(mapStateToProps)(SelectAccountStepModal);
