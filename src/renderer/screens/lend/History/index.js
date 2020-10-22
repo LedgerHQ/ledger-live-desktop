4a7dde2ef036dc08792c83908cae43bb7f093d2b
Valentin D. Pinkman <valentin.d.pinkman@icloud.com>
2020-10-22 18:03:42 +0200

    fix history list of compound ops

--- src/renderer/screens/lend/History/index.js
+++ src/renderer/screens/lend/History/index.js
@@ -1,7 +1,9 @@
 // @flow
-import React, { useCallback, useMemo } from "react";
+import React from "react";
 import { useTranslation } from "react-i18next";
 import type { AccountLikeArray, AccountLike, Operation } from "@ledgerhq/live-common/lib/types";
+import { findCompoundToken } from "@ledgerhq/live-common/lib/currencies";
+import { isCompoundTokenSupported } from "@ledgerhq/live-common/lib/families/ethereum/modules/compound";
 import Box from "~/renderer/components/Box";
 import OperationsList from "~/renderer/components/OperationsList";
 import EmptyState from "../EmptyState";
@@ -14,8 +16,12 @@ type Props = {
 const History = ({ navigateToCompoundDashboard, accounts }: Props) => {
   const { t } = useTranslation();
 
-  const filterOperation = (op: Operation, acc: AccountLike) =>
-    ["REDEEM", "SUPPLY"].includes(op.type);
+  const filterOperation = (op: Operation, acc: AccountLike) => {
+    if (acc.type !== "TokenAccount") return false;
+    const ctoken = findCompoundToken(acc.token);
+    if (!ctoken) return false;
+    return isCompoundTokenSupported(ctoken) && ["REDEEM", "SUPPLY"].includes(op.type);
+  };
 
   return (
     <Box>
