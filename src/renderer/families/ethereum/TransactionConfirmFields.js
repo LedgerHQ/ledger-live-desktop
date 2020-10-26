87920059e0c0a093ee7badfa3ca54d77d912d5c0
LFBarreto <lfb.access@gmail.com>
2020-10-26 15:08:30 +0100

    (Ethereum): add specific warning messages for tx modes

---
+++ src/renderer/families/ethereum/TransactionConfirmFields.js
@@ -0,0 +1,36 @@
+// @flow
+
+import invariant from "invariant";
+import React from "react";
+import { Trans } from "react-i18next";
+
+import type { Transaction } from "@ledgerhq/live-common/lib/types";
+
+import WarnBox from "~/renderer/components/WarnBox";
+
+const Warning = ({
+  transaction,
+  recipientWording,
+}: {
+  transaction: Transaction,
+  recipientWording: string,
+}) => {
+  invariant(transaction.family === "ethereum", "ethereum transaction");
+
+  switch (transaction.mode) {
+    case "compound.withdraw":
+    case "compound.supply":
+    case "erc20.approve":
+      return null;
+    default:
+      return (
+        <WarnBox>
+          <Trans i18nKey="TransactionConfirm.warning" values={{ recipientWording }} />
+        </WarnBox>
+      );
+  }
+};
+
+export default {
+  warning: Warning,
+};
