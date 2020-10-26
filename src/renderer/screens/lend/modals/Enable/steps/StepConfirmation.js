2679ba7762e51b080cb85734c4f8cabfd69f6617
LFBarreto <lfb.access@gmail.com>
2020-10-26 12:25:26 +0100

    LL-3766 (EnableFlow): remove view details cta on success screen

--- src/renderer/screens/lend/modals/Enable/steps/StepConfirmation.js
+++ src/renderer/screens/lend/modals/Enable/steps/StepConfirmation.js
@@ -110,46 +110,18 @@ export function StepConfirmationFooter({
   account,
   parentAccount,
   onRetry,
-  optimisticOperation,
   error,
   openModal,
   onClose,
   transitionTo,
   t,
 }: StepProps) {
-  const concernedOperation = optimisticOperation
-    ? optimisticOperation.subOperations && optimisticOperation.subOperations.length > 0
-      ? optimisticOperation.subOperations[0]
-      : optimisticOperation
-    : null;
   return (
     <Box horizontal justifyContent="flex-end">
       <Button ml={2} event="Lending Flow Step 4 Close Clicked" onClick={onClose} secondary>
         {t("lend.enable.steps.confirmation.success.done")}
       </Button>
-      {concernedOperation ? (
-        // FIXME make a standalone component!
-        <Button
-          ml={2}
-          id={"lend-confirmation-opc-button"}
-          event="Lending Flow Step 4 View OpD Clicked"
-          onClick={() => {
-            onClose();
-            if (account && concernedOperation) {
-              openModal("MODAL_OPERATION_DETAILS", {
-                operationId: concernedOperation.id,
-                accountId: account.id,
-                parentId: parentAccount && parentAccount.id,
-              });
-            }
-          }}
-          primary
-        >
-          {t("lend.enable.steps.confirmation.success.cta")}
-        </Button>
-      ) : error ? (
-        <RetryButton ml={2} primary onClick={onRetry} />
-      ) : null}
+      {error ? <RetryButton ml={2} primary onClick={onRetry} /> : null}
     </Box>
   );
 }
