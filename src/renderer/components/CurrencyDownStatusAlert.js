6c7bd224936748a2dc7e5dd2799af77ba16abcbc
Gaëtan Renaudeau <renaudeau.gaetan@gmail.com>
2020-11-16 13:39:25 +0100

    remove BCH warnings

--- src/renderer/components/CurrencyDownStatusAlert.js
+++ src/renderer/components/CurrencyDownStatusAlert.js
@@ -1,21 +1,11 @@
 // @flow
-import React from "react";
-import { createCustomErrorClass } from "@ledgerhq/errors";
 import type { TokenCurrency, CryptoCurrency } from "@ledgerhq/live-common/lib/types";
-import ErrorBanner from "./ErrorBanner";
 
 type Props = {
   currencies: Array<CryptoCurrency | TokenCurrency>,
 };
 
-const BitcoinCashHardforkOct2020Warning = createCustomErrorClass(
-  "BitcoinCashHardforkOct2020Warning",
-);
-
 const CurrencyDownStatusAlert = ({ currencies }: Props) => {
-  if (currencies.some(c => c.id === "bitcoin_cash")) {
-    return <ErrorBanner error={new BitcoinCashHardforkOct2020Warning()} warning />;
-  }
   return null;
 };
 
