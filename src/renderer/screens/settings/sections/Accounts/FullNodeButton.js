77432de4f2f37fe27249f6103f89f789e74efaa3
Juan Cortes Ross <juan@bohem.io>
2020-11-19 19:28:01 +0100

    Make the connect/edit button reactive

--- src/renderer/screens/settings/sections/Accounts/FullNodeButton.js
+++ src/renderer/screens/settings/sections/Accounts/FullNodeButton.js
@@ -3,12 +3,12 @@ import React, { useCallback } from "react";
 import { Trans } from "react-i18next";
 import { openModal } from "~/renderer/actions/modals";
 import { useDispatch } from "react-redux";
-import { getEnv } from "@ledgerhq/live-common/lib/env";
+import useEnv from "~/renderer/hooks/useEnv";
 import Track from "~/renderer/analytics/Track";
 import Button from "~/renderer/components/Button";
 
 const FullNodeButton = () => {
-  const fullNodeEnabled = getEnv("SATSTACK");
+  const fullNodeEnabled = useEnv("SATSTACK");
   const dispatch = useDispatch();
   const onOpenModal = useCallback(() => dispatch(openModal("MODAL_FULL_NODE")), [dispatch]);
 
