9da23d27f0828d1bbaa083be823ed1e63d629e23
Juan Cortes Ross <juan@bohem.io>
2020-11-19 18:50:44 +0100

    Missing wording for satStack status

--- src/renderer/screens/settings/sections/Accounts/FullNodeStatus.js
+++ src/renderer/screens/settings/sections/Accounts/FullNodeStatus.js
@@ -1,13 +1,17 @@
 // @flow
 import React from "react";
-import { useTranslation } from "react-i18next";
+import { useTranslation, Trans } from "react-i18next";
 import Box from "~/renderer/components/Box";
 import Text from "~/renderer/components/Text";
+import { Rotating } from "~/renderer/components/Spinner";
+import IconBlueSpinner from "~/renderer/icons/BlueSpinner";
+import Tooltip from "~/renderer/components/Tooltip";
 import type { ThemedComponent } from "~/renderer/styles/StyleProvider";
 import styled from "styled-components";
 import { colors } from "~/renderer/styles/theme";
 import useEnv from "~/renderer/hooks/useEnv";
 import useSatStackStatus from "~/renderer/hooks/useSatStackStatus";
+import type { SatsStackStatus } from "@ledgerhq/live-common/lib/families/bitcoin/satstack";
 
 const Dot: ThemedComponent<{
   color?: string,
@@ -24,37 +28,70 @@ const Wrapper = styled(Box)`
   display: flow;
 `;
 
-const naiveMapping = {
-  ko: ["satstack-disconnected", "node-disconnected", "invalid-chain"],
-  ok: ["ready"],
-  pending: ["syncing", "scanning", "initializing"],
+const getColorsForStatus = status => {
+  const naiveMapping = {
+    ko: ["satstack-disconnected", "node-disconnected", "invalid-chain"],
+    ok: ["ready"],
+    pending: ["syncing", "scanning", "initializing"],
+  };
+
+  return naiveMapping.ko.includes(status)
+    ? {
+        textColor: colors.alertRed,
+        dotColor: colors.alertRed,
+      }
+    : naiveMapping.ok.includes(status)
+    ? {
+        textColor: "palette.text.shade100",
+        dotColor: colors.positiveGreen,
+      }
+    : {
+        textColor: "wallet",
+      };
 };
 
 const FullNodeStatus = () => {
   const { t } = useTranslation();
-  const latestStatus = useSatStackStatus();
+  const latestStatus: SatsStackStatus = useSatStackStatus() || { type: "initializing" };
   const satStackAlreadyConfigured = useEnv("SATSTACK");
   if (!satStackAlreadyConfigured) return null;
 
-  const status = (latestStatus && latestStatus.type) || "";
-  const color = naiveMapping.ok.includes(status)
-    ? colors.positiveGreen
-    : naiveMapping.pending.includes(status)
-    ? colors.orange
-    : colors.alertRed;
+  // $FlowFixMe
+  const { progress, type } = latestStatus;
+  // $FlowFixMe
+  const { textColor, dotColor } = getColorsForStatus(type);
 
+  const formattedProgress = progress ? (parseFloat(progress) * 100).toFixed(0) : undefined;
   const statusLabel = `${t("fullNode.status")}:`;
   return (
     <Box horizontal>
-      <Wrapper horizontal alignItems="center">
-        <Text mr={2} ff="Inter|Regular" fontSize={3} color={"palette.text.shade50"}>
-          {statusLabel}
-        </Text>
-        <Dot color={color} />
-        <Text ml={1} ff="Inter|SemiBold" fontSize={3} color={"palette.text.shade100"}>
-          {status}
-        </Text>
-      </Wrapper>
+      <Tooltip
+        content={
+          <Trans i18nKey={`fullNode.modal.steps.satstack.connectionSteps.${type}.description`} />
+        }
+      >
+        <Wrapper horizontal alignItems="center">
+          <Text mr={2} ff="Inter|Regular" fontSize={3} color={"palette.text.shade50"}>
+            {statusLabel}
+          </Text>
+          {progress && formattedProgress ? (
+            <>
+              <Rotating size={16}>
+                <IconBlueSpinner size={16} />
+              </Rotating>
+              <Text ml={1} ff="Inter|SemiBold" fontSize={3} color={textColor}>
+                {`${formattedProgress} %`}
+              </Text>
+            </>
+          ) : (
+            <Dot color={dotColor} />
+          )}
+
+          <Text ml={1} ff="Inter|SemiBold" fontSize={3} color={textColor}>
+            <Trans i18nKey={`fullNode.modal.steps.satstack.connectionSteps.${type}.header`} />
+          </Text>
+        </Wrapper>
+      </Tooltip>
       <Box flex={1} />
     </Box>
   );
