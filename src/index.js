4ab23b71f31139a6e246bb2b1287d8d200073d05
machard <matthieu__@hotmail.fr>
2020-11-26 12:03:54 +0100

    timemachine ok

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
+if (process.env.SPECTRON_RUN) {
+  const timemachine = require("timemachine");
+  timemachine.config({
+    dateString: "March 14, 2018 13:34:42",
+  });
+}
+
 if (!process.env.IS_INTERNAL_PROCESS) {
   // Main electron thread
   require("./main");
