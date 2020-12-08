9ca5b562f814b8d9ba4898743c87d39f4ac38299
Valentin D. Pinkman <valentin.d.pinkman@hey.com>
2020-12-08 16:18:34 +0100

    add success to connect device

--- src/renderer/components/Onboarding/Screens/Tutorial/screens/GenuineCheck.js
+++ src/renderer/components/Onboarding/Screens/Tutorial/screens/GenuineCheck.js
@@ -1,16 +1,16 @@
 // @flow
 
-import React from "react";
+import React, { useCallback, useState } from "react";
 import styled from "styled-components";
-import { useTranslation } from "react-i18next";
+import { useTranslation, Trans } from "react-i18next";
 import { createAction } from "@ledgerhq/live-common/lib/hw/actions/manager";
 import { getEnv } from "@ledgerhq/live-common/lib/env";
-
+import { rgba } from "~/renderer/styles/helpers";
 import Text from "~/renderer/components/Text";
 import Button from "~/renderer/components/Button";
 import type { ThemedComponent } from "~/renderer/styles/StyleProvider";
 import ArrowLeft from "~/renderer/icons/ArrowLeft";
-// import ChevronRight from "~/renderer/icons/ChevronRight";
+import IconCheck from "~/renderer/icons/Check";
 import InfoCircle from "~/renderer/icons/InfoCircle";
 import { ContentContainer, HeaderContainer } from "../shared";
 import DeviceAction from "~/renderer/components/DeviceAction";
@@ -45,8 +45,63 @@ const Content = styled.div`
   justify-content: center;
 `;
 
-export function GenuineCheck({ sendEvent, context }) {
+const SuccessContainer = styled(Content)`
+  flex-direction: column;
+  max-width: 250px;
+`;
+
+const IconContainer = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  border-radius: 50%;
+  background-color: ${p => rgba(p.theme.colors.positiveGreen, 0.1)};
+  color: ${p => p.theme.colors.positiveGreen};
+  width: 48px;
+  height: 48px;
+`;
+
+const Success = ({ device }: *) => {
   const { t } = useTranslation();
+  return (
+    <SuccessContainer>
+      <IconContainer>
+        <IconCheck size={18} />
+      </IconContainer>
+      <Text mt={3} ff="Inter|SemiBold" fontSize={6}>
+        <Trans i18nKey="onboarding.screens.tutorial.screens.genuineCheck.success.title" />
+      </Text>
+      <Text mt={2} ff="Inter|Regular" fontSize={4} textAlign="center">
+        <Trans
+          i18nKey="onboarding.screens.tutorial.screens.genuineCheck.success.desc"
+          values={{ deviceName: t(`devices.${device.modelId}`) }}
+        />
+      </Text>
+    </SuccessContainer>
+  );
+};
+
+type Props = {
+  sendEvent: string => void,
+  context: {
+    deviceIsGenuine?: boolean,
+  },
+};
+
+export function GenuineCheck({ sendEvent, context }: Props) {
+  const { t } = useTranslation();
+  const { deviceIsGenuine } = context;
+  const [device, setDevice] = useState(null);
+
+  console.log(context);
+
+  const onResult = useCallback(
+    res => {
+      setDevice(res.device);
+      sendEvent("GENUINE_CHECK_SUCCESS");
+    },
+    [sendEvent, setDevice],
+  );
 
   return (
     <ScreenContainer>
@@ -60,11 +115,11 @@ export function GenuineCheck({ sendEvent, context }) {
           </Button>
         </HeaderContainer>
         <Content>
-          <DeviceAction
-            action={action}
-            onResult={res => sendEvent("GENUINE_CHECK_SUCCESS")}
-            request={null}
-          />
+          {deviceIsGenuine && device ? (
+            <Success device={device} />
+          ) : (
+            <DeviceAction action={action} onResult={onResult} request={null} />
+          )}
         </Content>
       </ContentContainer>
       <ContentFooter>
@@ -74,7 +129,7 @@ export function GenuineCheck({ sendEvent, context }) {
             {t("onboarding.screens.tutorial.screens.genuineCheck.buttons.prev")}
           </Text>
         </Button>
-        <Button primary onClick={() => sendEvent("NEXT")} disabled={!context.deviceIsGenuine}>
+        <Button primary onClick={() => sendEvent("NEXT")} disabled={!deviceIsGenuine}>
           <Text ff="Inter|Bold" fontSize={3} lineHeight="18px">
             {t("onboarding.screens.tutorial.screens.genuineCheck.buttons.next")}
           </Text>
