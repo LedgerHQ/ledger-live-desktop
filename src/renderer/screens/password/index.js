c77ad21830893dc4c8dce97b9b6df771ce9e0059
machard <matthieu__@hotmail.fr>
2020-12-16 11:12:58 +0100

    handle open app

--- src/renderer/screens/password/index.js
+++ src/renderer/screens/password/index.js
@@ -1,5 +1,5 @@
 // @flow
-import React, { useCallback } from "react";
+import React, { useCallback, useState } from "react";
 import { useTranslation } from "react-i18next";
 import type { RouterHistory, Match, Location } from "react-router-dom";
 import { useDispatch } from "react-redux";
@@ -14,12 +14,21 @@ import {
   SettingsSectionBody as Body,
   SettingsSectionRow as Row,
 } from "../settings/SettingsSection";
+import { createAction } from "@ledgerhq/live-common/lib/hw/actions/app";
+import { SyncSkipUnderPriority } from "@ledgerhq/live-common/lib/bridge/react";
+import DeviceAction from "~/renderer/components/DeviceAction";
+import { command } from "~/renderer/commands";
+import { mockedEventEmitter } from "~/renderer/components/DebugMock";
+import { getEnv } from "@ledgerhq/live-common/lib/env";
 
 type Props = {
   history: RouterHistory,
   location: Location,
   match: Match,
 };
+
+const connectApp = command("connectApp");
+const action = createAction(getEnv("MOCK") ? mockedEventEmitter : connectApp);
 
 // Props are passed from the <Route /> component in <Default />
 const Password = ({ history, location, match }: Props) => {
@@ -35,40 +44,55 @@ const Password = ({ history, location, match }: Props) => {
     [dispatch],
   );
 
+  const [result, setResult] = useState(null);
+
   return (
-    <Box pb={4} selectable>
-      <Box
-        ff="Inter|SemiBold"
-        color="palette.text.shade100"
-        fontSize={7}
-        mb={5}
-        data-e2e="password_title"
-        justifyContent="space-between"
-        horizontal
-      >
-        {t("llpassword.title")}
-        <Button event="Add" small primary onClick={onAddPassword}>
-          {t("llpassword.add")}
-        </Button>
-      </Box>
-      <Section>
-        <TrackPage category="Password" name="Index" />
-
-        <Header
-          icon={<IconLock size={16} />}
-          title={t("llpassword.title")}
-          desc={t("llpassword.desc")}
-        />
-
-        <Body>
-          <Row title="Password name" desc="password desc">
-            <Button event="Copy passwd" small primary onClick={() => {}}>
-              {t("llpassword.copy")}
+    <>
+      <SyncSkipUnderPriority priority={999} />
+      {result ? (
+        <Box pb={4} selectable>
+          <Box
+            ff="Inter|SemiBold"
+            color="palette.text.shade100"
+            fontSize={7}
+            mb={5}
+            data-e2e="password_title"
+            justifyContent="space-between"
+            horizontal
+          >
+            {t("llpassword.title")}
+            <Button event="Add" small primary onClick={onAddPassword}>
+              {t("llpassword.add")}
             </Button>
-          </Row>
-        </Body>
-      </Section>
-    </Box>
+          </Box>
+          <Section>
+            <TrackPage category="Password" name="Index" />
+
+            <Header
+              icon={<IconLock size={16} />}
+              title={t("llpassword.title")}
+              desc={t("llpassword.desc")}
+            />
+
+            <Body>
+              <Row title="Password name" desc="password desc">
+                <Button event="Copy passwd" small primary onClick={() => {}}>
+                  {t("llpassword.copy")}
+                </Button>
+              </Row>
+            </Body>
+          </Section>
+        </Box>
+      ) : (
+        <DeviceAction
+          onResult={setResult}
+          action={action}
+          request={{
+            appName: "Bitcoin",
+          }}
+        />
+      )}
+    </>
   );
 };
 export default Password;
