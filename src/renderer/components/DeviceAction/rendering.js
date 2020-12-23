3d30d28194dcc72bd79de663654edaed09e33a19
Arnaud97234 <arnaud.ulric@gmail.com>
2020-12-23 16:06:36 +0100

    fix font swap summary

--- src/renderer/components/DeviceAction/rendering.js
+++ src/renderer/components/DeviceAction/rendering.js
@@ -432,10 +432,10 @@ export const renderSwapDeviceConfirmation = ({
       },
       (value, key) => (
         <Box horizontal justifyContent="space-between" key={key} mb={2} ml="12px" mr="12px">
-          <Text fontWeight="500" color="palette.text.shade50" fontSize={3}>
+          <Text fontWeight="500" color="palette.text.shade40" fontSize={3}>
             <Trans i18nKey={`DeviceAction.swap.${key}`} />
           </Text>
-          <Text color="palette.text.shade100" fontWeight="500" fontSize={3}>
+          <Text color="palette.text.shade80" fontWeight="500" fontSize={3}>
             {value}
           </Text>
         </Box>
