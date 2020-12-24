2fdc4628d1a17020fbea461ab47b16c932deb771
LFBarreto <lfb.access@gmail.com>
2020-12-24 10:35:19 +0100

    (Carousel): remove algo banner

--- src/renderer/components/Carousel/helpers.js
+++ src/renderer/components/Carousel/helpers.js
@@ -2,11 +2,10 @@
 import LedgerAcademyBanner from "~/renderer/components/Carousel/banners/LedgerAcademy";
 import BackupPackBanner from "~/renderer/components/Carousel/banners/BackupPack";
 import BuyCryptoBanner from "~/renderer/components/Carousel/banners/BuyCrypto";
-import StakeAlgorandBanner from "~/renderer/components/Carousel/banners/StakeAlgorand";
 import SwapBanner from "~/renderer/components/Carousel/banners/Swap";
 import SellBanner from "~/renderer/components/Carousel/banners/Sell";
 import LendingBanner from "~/renderer/components/Carousel/banners/Lending";
-import BlackFridayBanner from "~/renderer/components/Carousel/banners/BlackFriday";
+// import BlackFridayBanner from "~/renderer/components/Carousel/banners/BlackFriday";
 
 export const getTransitions = (transition: "slide" | "flip", reverse: boolean = false) => {
   const mult = reverse ? -1 : 1;
@@ -47,18 +46,17 @@ export const getTransitions = (transition: "slide" | "flip", reverse: boolean = 
 
 export const getDefaultSlides = () => {
   return [
-    {
-      id: "blackfriday",
-      Component: BlackFridayBanner,
-      start: new Date("1 Nov 2020 00:01:00 PST"),
-      end: new Date("30 Nov 2020 23:59:00 PST"),
-    },
+    // {
+    //   id: "blackfriday",
+    //   Component: BlackFridayBanner,
+    //   start: new Date("1 Nov 2020 00:01:00 PST"),
+    //   end: new Date("30 Nov 2020 23:59:00 PST"),
+    // },
     { id: "swap", Component: SwapBanner },
     { id: "buy", Component: BuyCryptoBanner },
     { id: "sell", Component: SellBanner },
     { id: "backupPackBanner", Component: BackupPackBanner },
     { id: "ledgerAcademy", Component: LedgerAcademyBanner },
-    { id: "stakeAlgorand", Component: StakeAlgorandBanner },
     { id: "lending", Component: LendingBanner },
   ];
 };
