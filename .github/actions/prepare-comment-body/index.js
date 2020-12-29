4e16c9bc3c284adb0e9c82870a3c7fadde144ffe
machard <matthieu__@hotmail.fr>
2020-12-29 14:27:39 +0100

    try to add author slack dm

--- .github/actions/prepare-comment-body/index.js
+++ .github/actions/prepare-comment-body/index.js
@@ -3,6 +3,8 @@ const core = require("@actions/core");
 const main = async () => {
   const images = core.getInput("images");
   const runId = core.getInput("runId");
+  const pullId = core.getInput("pullId");
+  const author = core.getInput("author");
   const testoutput = core.getInput("testoutput");
   const lintoutput = core.getInput("lintoutput");
   const fullrepo = core.getInput("fullrepo").split("/");
@@ -61,8 +63,25 @@ Diff output ${imgDiffFailed ? "❌" : " ✅"}
 https://github.com/LedgerHQ/ledger-live-desktop/commits/develop
 `;
 
+  const githubSlackMap = {
+    machard: "U01DJR04M8Q",
+    "juan-cortes": "UE1D1FS77",
+    dasilvarosa: "UA14024H4",
+    valpinkman: "U98KPAN86",
+  };
+
+  const strSlackAuthor = `
+Lint outputs ${lintFailed ? "❌" : " ✅"}
+Tests outputs ${testsFailed ? "❌" : " ✅"}
+Diff output ${imgDiffFailed ? "❌" : " ✅"}
+
+https://github.com/LedgerHQ/ledger-live-desktop/pull/${pullId}
+`;
+
   core.setOutput("body", str);
   core.setOutput("bodySlack", strSlack);
+  core.setOutput("bodySlackAuthor", strSlackAuthor);
+  core.setOutput("slackAuthor", githubSlackMap[author]);
 };
 
 main().catch(err => core.setFailed(err));
