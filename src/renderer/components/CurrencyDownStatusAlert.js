b51a642a6a7e491b53375f2c6ef39aad0c8c8178
LFBarreto <lfb.access@gmail.com>
2021-01-20 11:45:47 +0100

    (Stratis): eol warning banner added

--- src/renderer/components/CurrencyDownStatusAlert.js
+++ src/renderer/components/CurrencyDownStatusAlert.js
@@ -1,11 +1,20 @@
 // @flow
+import React from "react";
+import { createCustomErrorClass } from "@ledgerhq/errors";
 import type { TokenCurrency, CryptoCurrency } from "@ledgerhq/live-common/lib/types";
 
+import ErrorBanner from "./ErrorBanner";
 type Props = {
   currencies: Array<CryptoCurrency | TokenCurrency>,
 };
 
+const StratisDown2021Warning = createCustomErrorClass("StratisDown2021Warning");
+
 const CurrencyDownStatusAlert = ({ currencies }: Props) => {
+  if (currencies.some(c => c.id === "stratis")) {
+    return <ErrorBanner error={new StratisDown2021Warning()} warning />;
+  }
+
   return null;
 };
 
