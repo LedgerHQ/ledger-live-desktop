51e825fe9792814c9d730e92f0b674d902e6cd7a
Valentin D. Pinkman <valentin.d.pinkman@hey.com>
2021-02-01 17:55:26 +0100

    fix import

--- tests/specs/accounts/tezos.spec.js
+++ tests/specs/accounts/tezos.spec.js
@@ -1,5 +1,6 @@
 import initialize from "../../common.js";
-import { accountsFlows, addAccount } from "./flows.js";
+import addAccount from "../../flows/accounts/addAccount";
+import { accountsFlows } from "./flows.js";
 
 describe("tezos family", () => {
   initialize("tezos-accounts", {
