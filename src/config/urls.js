c2a3dc2e24a3f32581f955b83644d3a989038a66
haammar-ledger <hakim.aammar-ext@ledger.fr>
2021-02-12 11:18:45 +0100

    Update one support link for Polkadot

--- src/config/urls.js
+++ src/config/urls.js
@@ -102,7 +102,7 @@ export const urls = {
   algorandStakingRewards:
     "https://support.ledger.com/hc/en-us/articles/360015897740?utm_source=ledger_live_desktop&utm_medium=self_referral&utm_content=algorand",
   polkadotFeesInfo:
-    "https://support.ledger.com/hc/en-us/articles/360018131220?utm_source=ledger_live_desktop&utm_medium=self_referral&utm_content=polkadot",
+    "https://support.ledger.com/hc/en-us/articles/360016289919?utm_source=ledger_live_desktop&utm_medium=self_referral&utm_content=polkadot",
   xpubLearnMore:
     "https://support.ledger.com/hc/en-us/articles/360011069619?utm_source=ledger_live_desktop&utm_medium=self_referral&utm_content=edit_account",
 
