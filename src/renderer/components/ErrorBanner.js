483886dd03e9ce23a4f6a7d4e7ab6e2d3a4dfc3e
Rémi Jarasson (Ext) <remi.jarasson-ext@ledger.fr>
2021-03-15 18:06:01 +0100

    refactor: ErrorBanner uses Alert

--- src/renderer/components/ErrorBanner.js
+++ src/renderer/components/ErrorBanner.js
@@ -1,50 +1,27 @@
 // @flow
 
-import React, { PureComponent } from "react";
-import styled from "styled-components";
-import type { ThemedComponent } from "~/renderer/styles/StyleProvider";
-import Box from "~/renderer/components/Box";
-import IconTriangleWarning from "~/renderer/icons/TriangleWarning";
+import React from "react";
+import { urls } from "~/config/urls";
 import TranslatedError from "./TranslatedError";
-import SupportLinkError from "./SupportLinkError";
+import Alert from "./Alert";
 
 type Props = {
   error: Error,
   warning?: boolean,
 };
 
-const ErrorBannerBox: ThemedComponent<{}> = styled(Box).attrs(() => ({
-  horizontal: true,
-  alignItems: "flex-start",
-  color: "palette.background.paper",
-  borderRadius: 1,
-  fontSize: 1,
-  px: 4,
-  py: 2,
-  mb: 4,
-}))`
-  background-color: ${p => (p.warning ? p.theme.colors.orange : p.theme.colors.alertRed)};
-`;
+const ErrorBanner = ({ error, warning }: Props) => {
+  const maybeUrl = error ? urls.errors[error.name] : null;
 
-class ErrorBanner extends PureComponent<Props> {
-  render() {
-    const { error, warning } = this.props;
-    return (
-      <ErrorBannerBox warning={warning}>
-        <Box mr={2} alignSelf="center">
-          <IconTriangleWarning height={16} width={16} />
-        </Box>
-        <Box ff="Inter|SemiBold" fontSize={3} vertical shrink>
-          <Box>
-            <TranslatedError error={error} />
-          </Box>
-          <Box>
-            <TranslatedError error={error} field="description" />
-          </Box>
-          <SupportLinkError error={error} />
-        </Box>
-      </ErrorBannerBox>
-    );
-  }
-}
+  return (
+    <Alert
+      type={warning ? "warning" : "error"}
+      title={<TranslatedError error={error} />}
+      learnMoreUrl={maybeUrl}
+      mb={4}
+    >
+      <TranslatedError error={error} field="description" />
+    </Alert>
+  );
+};
 export default ErrorBanner;
