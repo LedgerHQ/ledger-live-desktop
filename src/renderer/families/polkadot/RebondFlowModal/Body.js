5888f662802da41ac6705a99d1737c4620514f10
haammar-ledger <hakim.aammar-ext@ledger.fr>
2021-04-08 17:32:10 +0200

    LL-5049 Polkadot remove alert on retry

--- src/renderer/families/polkadot/RebondFlowModal/Body.js
+++ src/renderer/families/polkadot/RebondFlowModal/Body.js
@@ -126,6 +126,7 @@ const Body = ({
   const handleStepChange = useCallback(e => onChangeStepId(e.id), [onChangeStepId]);
 
   const handleRetry = useCallback(() => {
+    setTransactionError(null);
     onChangeStepId("amount");
   }, [onChangeStepId]);
 
