3b5eefee60bb07ac3fea0b430644ac49bd818f34
LFBarreto <lfb.access@gmail.com>
2021-06-22 18:11:19 +0200

    (Colors): fixes

--- src/renderer/styles/StyleProvider.js
+++ src/renderer/styles/StyleProvider.js
@@ -54,7 +54,6 @@ const StyleProvider = ({ children }: Props) => {
             left: 0,
             mixBlendMode: "hue",
             pointerEvents: "none",
-            opacity: 0.8,
           }}
           xmlns="http://www.w3.org/2000/svg"
           viewBox="0 0 380 100"
