4c791b4361ff7036098c35715a0ec17c5bb941c4
Valentin D. Pinkman <valentin.d.pinkman@hey.com>
2021-07-01 09:53:05 +0200

    fix waitForSync

--- tests/common.js
+++ tests/common.js
@@ -209,9 +209,9 @@ export default function initialize(name, { userData, env = {}, disableStartSnap 
       !illustrations.error && (await illustrations.waitForDisplayed());
     });
 
-    app.client.addCommand("waitForSync", async (timeout = 60000) => {
+    app.client.addCommand("waitForSync", async () => {
       const sync = await app.client.$("#topbar-synchronized");
-      return sync.waitForDisplayed({ timeout });
+      await sync.waitForDisplayed();
     });
 
     app.client.addCommand("screenshot", async function(countdown = 500) {
