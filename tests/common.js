801130f65798f34b63be9767563ba7c2fa555fc1
Valentin D. Pinkman <valentin.d.pinkman@hey.com>
2021-07-02 14:15:47 +0200

    inncrease pause ?

--- tests/common.js
+++ tests/common.js
@@ -214,7 +214,7 @@ export default function initialize(name, { userData, env = {}, disableStartSnap 
       await sync.waitForDisplayed({ timeout });
     });
 
-    app.client.addCommand("screenshot", async function(countdown = 1000) {
+    app.client.addCommand("screenshot", async function(countdown = 1500) {
       const unfocus = await app.client.$("#unfocus-please");
       await unfocus.click();
 
