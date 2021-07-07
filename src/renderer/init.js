cf670fabbdf12d7a34a11e5e33a57c39c0897670
Nabil Bourenane <bourenane.nabil@icloud.com>
2021-07-07 15:38:39 +0200

    Use Swap API v3 for alpha builds

--- src/renderer/init.js
+++ src/renderer/init.js
@@ -61,6 +61,9 @@ async function init() {
     connect,
   });
 
+  // Point to Swap API v3
+  setEnvOnAllThreads("SWAP_API_BASE", "https://swap-stg.ledger.com/v3");
+
   if (process.env.SPECTRON_RUN) {
     const spectronData = await getKey("app", "SPECTRON_RUN", {});
     _.each(spectronData.localStorage, (value, key) => {
