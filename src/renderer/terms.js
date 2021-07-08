e0bc2da017b7a4d73f39ee820046f60da370b41f
jonathangiamp <jonathan.giamporcaro@gmail.com>
2021-07-08 12:01:05 +0200

    chore(config): add support to dynamic terms urls
    Thanks to this new hook, url to terms webpage displayed is now based on
    user language. Terms supports `en`, `fr`, `es` and `de`. `en` is used as
    the fallback language.
    - Remove TERMS*.md files
    - Remove unused functions used until now to dynamically load TERMS*.md files

--- src/renderer/terms.js
+++ src/renderer/terms.js
@@ -1,16 +1,9 @@
 // @flow
 import { useEffect, useState } from "react";
 
-import network from "@ledgerhq/live-common/lib/network";
 import { useSelector } from "react-redux";
 import { languageSelector } from "~/renderer/reducers/settings";
 import { urls } from "~/config/urls";
-
-const getRawLanguageURL = (language: string) =>
-  `https://raw.githubusercontent.com/LedgerHQ/ledger-live-desktop/master/TERM.${language}.md`;
-const rawDefaultURL =
-  "https://raw.githubusercontent.com/LedgerHQ/ledger-live-desktop/master/TERMS.md";
-export const url = "https://github.com/LedgerHQ/ledger-live-desktop/blob/master/TERMS.md";
 
 const currentTermsRequired = "2019-12-04";
 const currentLendingTermsRequired = "2020-11-10";
@@ -31,31 +24,6 @@ export function acceptLendingTerms() {
   return global.localStorage.setItem("acceptedLendingTermsVersion", currentLendingTermsRequired);
 }
 
-export async function load(language: string) {
-  try {
-    const { data } = await network({ url: getRawLanguageURL(language) });
-    return data;
-  } catch (error) {
-    if (error.status === 404) {
-      const { data } = await network({ url: rawDefaultURL });
-      return data;
-    }
-    throw error;
-  }
-}
-
-export const useTerms = () => {
-  const [terms, setTerms] = useState(null);
-  const [error, setError] = useState(null);
-
-  const language = useSelector(languageSelector);
-  useEffect(() => {
-    load(language).then(setTerms, setError);
-  }, [language]);
-
-  return [terms, error];
-};
-
 export const usePrivacyUrl = () => {
   const [privacyUrl, setPrivacyUrl] = useState(urls.privacyPolicy.en);
 
@@ -65,4 +33,17 @@ export const usePrivacyUrl = () => {
   }, [language]);
 
   return privacyUrl;
+};
+
+// TODO: Merge useTermsUrl and usePrivacyUrl to create generic useDynamicUrl
+export const useTermsUrl = () => {
+  const fallbackMessage = urls.terms.en;
+  const [termsUrl, setTermsUrl] = useState(fallbackMessage);
+
+  const language = useSelector(languageSelector);
+  useEffect(() => {
+    setTermsUrl(urls.terms[language] || fallbackMessage);
+  }, [language]);
+
+  return termsUrl;
 };
