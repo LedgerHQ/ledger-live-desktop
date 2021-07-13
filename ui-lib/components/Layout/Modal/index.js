66a4cea3cf315f6d8da03e7534a5a992f42358f6
jgiamporcaro-ledger <jonathan.giamporcaro@gmail.com>
2021-07-13 18:13:06 +0200

    design(ui-lib): add modal transitions

--- ui-lib/components/Layout/Modal/index.js
+++ ui-lib/components/Layout/Modal/index.js
@@ -5,9 +5,16 @@ import styled from "styled-components";
 import type { ThemedComponent } from "@ui/styles/StyleProvider";
 import Button from "@ui/components/Button";
 import Cross from "@ui/icons/Cross";
+import TransitionInOut from "@ui/components/Transition/TransitionInOut";
+import TransitionScale from "@ui/components/Transition/TransitionScale";
 
-type ModalProps = { children: React$Node, onClose?: () => void, width?: number, height?: number };
-type ModalWrapperProps = ModalProps & { isOpen: boolean };
+type ModalProps = {
+  isOpen: boolean,
+  children: React$Node,
+  onClose?: () => void,
+  width?: number,
+  height?: number,
+};
 
 const Container: ThemedComponent<*> = styled.div`
   position: relative;
@@ -28,15 +35,14 @@ const Wrapper: ThemedComponent<{ width?: number, height?: number }> = styled.div
   flex-direction: column;
   align-items: stretch;
   justify-content: space-between;
-  position: absolute;
-  top: 50%;
-  left: 50%;
-  transform translate(-50%, -50%);
   z-index: ${p => p.theme.zIndexes[8]};
 `;
 
 const Overlay: ThemedComponent<*> = styled.div`
   position: fixed;
+  display: flex;
+  justify-content: center;
+  align-items: center;
   top: 0;
   left: 0;
   width: 100vw;
@@ -51,22 +57,24 @@ const CloseButton: ThemedComponent<*> = styled(Button)`
   right: ${p => p.theme.space[3]}px;
 `;
 
-const Modal = ({ children, width, height, onClose }: ModalProps) => (
-  <>
-    <Overlay />
-    <Wrapper width={width} height={height}>
-      <Container>
-        <CloseButton Icon={Cross} onClick={onClose} />
-        {children}
-      </Container>
-    </Wrapper>
-  </>
+const Modal = ({ isOpen, children, width, height, onClose }: ModalProps) => (
+  <TransitionInOut in={isOpen} appear mountOnEnter unmountOnExit>
+    <Overlay>
+      <TransitionScale in={isOpen} appear>
+        <Wrapper width={width} height={height}>
+          <Container>
+            <CloseButton Icon={Cross} onClick={onClose} />
+            {children}
+          </Container>
+        </Wrapper>
+      </TransitionScale>
+    </Overlay>
+  </TransitionInOut>
 );
 
-const ModalWrapper = ({ children, isOpen, ...modalProps }: ModalWrapperProps) => {
+const ModalWrapper = ({ children, ...modalProps }: ModalProps) => {
   const $root = React.useMemo(() => document.querySelector("#ll-modal-root"), []);
 
-  if (isOpen === false) return null;
   if ($root === null) throw new Error("modal root cannot be found");
 
   return ReactDOM.createPortal(<Modal {...modalProps}>{children}</Modal>, $root);
