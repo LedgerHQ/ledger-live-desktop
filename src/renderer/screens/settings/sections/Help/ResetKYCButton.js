81b1202d3d8348a5bdaca903a6b841d1eecb0e0c
Juan Cortes Ross <juan@bohem.io>
2021-07-19 12:40:37 +0200

    LL-6432 Reset KYC row from Settings/Help

---
+++ src/renderer/screens/settings/sections/Help/ResetKYCButton.js
@@ -0,0 +1,23 @@
+// @flow
+
+import React, { useCallback } from "react";
+import { useTranslation } from "react-i18next";
+import { useDispatch } from "react-redux";
+import { setSwapKYCStatus } from "~/renderer/actions/settings";
+import Button from "~/renderer/components/Button";
+
+export default function ResetKYCButton() {
+  // NB specific to wyre, should be on a per-provider basis on SWP-Form
+  const { t } = useTranslation();
+  const dispatch = useDispatch();
+
+  const onResetKYC = useCallback(() => {
+    dispatch(setSwapKYCStatus({ provider: "wyre" }));
+  }, [dispatch]);
+
+  return (
+    <Button small danger onClick={onResetKYC} event="KYCReset">
+      {t("common.reset")}
+    </Button>
+  );
+}
