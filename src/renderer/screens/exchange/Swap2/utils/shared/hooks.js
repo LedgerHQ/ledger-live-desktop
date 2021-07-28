0e6db8d0347176a66e43649ab0a7231f13b9fe19
jgiamporcaro-ledger <jonathan.giamporcaro@gmail.com>
2021-07-28 12:04:35 +0200

    feature(swap): create useSwapProviders hooks
    This hooks is in charge to fetch providers and filter them based on the
    blocklist passed as an env.

---
+++ src/renderer/screens/exchange/Swap2/utils/shared/hooks.js
@@ -0,0 +1,57 @@
+// @flow
+import { useReducer, useEffect } from "react";
+import type { AvailableProvider } from "@ledgerhq/live-common/lib/exchange/swap/types";
+import { getProviders } from "@ledgerhq/live-common/lib/exchange/swap";
+
+type State = {
+  isLoading: boolean,
+  error: ?Error,
+  providers: ?Array<AvailableProvider>,
+};
+
+type ActionType =
+  | { type: "SAVE_DATA", payload: $PropertyType<State, "providers"> }
+  | { type: "SAVE_ERROR", payload: $PropertyType<State, "error"> };
+
+const reducer = (state: State, action: ActionType) => {
+  switch (action.type) {
+    case "SAVE_DATA":
+      return { error: null, providers: action.payload, isLoading: false };
+    case "SAVE_ERROR":
+      return { error: action.payload, providers: null, isLoading: false };
+    default:
+      throw new Error("Uncorrect action type");
+  }
+};
+
+export const initialState = { isLoading: true, error: null, providers: null };
+
+const filterDisabledProviders = (provider: AvailableProvider) =>
+  !process.env.SWAP_DISABLED_PROVIDERS?.includes(provider.provider);
+
+export const useSwapProviders = () => {
+  const [state, dispatch] = useReducer(reducer, initialState);
+
+  useEffect(() => {
+    let isMounted = true;
+
+    const saveProviders = async () => {
+      try {
+        const allProviders = await getProviders();
+        const providers = allProviders.filter(filterDisabledProviders);
+
+        if (isMounted) dispatch({ type: "SAVE_DATA", payload: providers });
+      } catch (error) {
+        if (isMounted) dispatch({ type: "SAVE_ERROR", payload: error });
+      }
+    };
+
+    saveProviders();
+
+    return () => {
+      isMounted = false;
+    };
+  }, []);
+
+  return state;
+};
