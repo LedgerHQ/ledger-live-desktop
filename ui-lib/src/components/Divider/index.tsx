47c3c8d1ed15074c97d21109d37a65f40aef17e6
Juan Cortes Ross <juan@bohem.io>
2021-08-17 13:07:08 +0200

    Remove unused props

--- ui-lib/src/components/Divider/index.tsx
+++ ui-lib/src/components/Divider/index.tsx
@@ -1,18 +1,8 @@
 import styled from "styled-components";
 
-type DividerTypes = "light" | "dark";
-
-interface Props {
-  margin?: number;
-  type?: DividerTypes; // Nb default to light grey
-}
-const Divider = styled.div<Props>`
+const Divider = styled.div`
   display: block;
-  margin: ${p => (p.margin === undefined ? 20 : p.margin)}px 0;
-  background: ${p =>
-    p.type === "dark"
-      ? p.theme.colors.palette.v2.grey.borderDark
-      : p.theme.colors.palette.v2.grey.border};
+  background: ${p => p.theme.colors.palette.v2.grey.border};
   height: 1px;
 `;
 
