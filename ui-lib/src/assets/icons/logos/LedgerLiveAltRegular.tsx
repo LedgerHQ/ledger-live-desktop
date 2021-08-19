bff532552210b1569e7d57ad0040b67e60759026
jgiamporcaro-ledger <jonathan.giamporcaro@gmail.com>
2021-08-19 14:30:15 +0200

    review: make logos scalable

--- ui-lib/src/assets/icons/logos/LedgerLiveAltRegular.tsx
+++ ui-lib/src/assets/icons/logos/LedgerLiveAltRegular.tsx
@@ -11,7 +11,7 @@ function LedgerLiveAltRegular({
     <svg
       width={width}
       height={height}
-      viewBox={`0 0 ${width} ${height}`}
+      viewBox="0 0 38 32"
       fill="none"
       xmlns="http://www.w3.org/2000/svg"
     >
--- ui-lib/src/assets/icons/logos/LedgerLiveRegular.tsx
+++ ui-lib/src/assets/icons/logos/LedgerLiveRegular.tsx
@@ -11,7 +11,7 @@ function LedgerLiveRegular({
     <svg
       width={width}
       height={height}
-      viewBox={`0 0 ${width} ${height}`}
+      viewBox="0 0 155 32"
       fill="none"
       xmlns="http://www.w3.org/2000/svg"
     >
