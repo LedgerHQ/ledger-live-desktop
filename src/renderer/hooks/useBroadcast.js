143a91c64944178a1617135baf3e3619041ea260
machard <matthieu__@hotmail.fr>
2021-08-20 15:24:19 +0200

    bl

--- src/renderer/hooks/useBroadcast.js
+++ src/renderer/hooks/useBroadcast.js
@@ -11,16 +11,19 @@ import type {
 import { getAccountBridge } from "@ledgerhq/live-common/lib/bridge";
 import { execAndWaitAtLeast } from "@ledgerhq/live-common/lib/promise";
 import { getEnv } from "@ledgerhq/live-common/lib/env";
+
 type SignTransactionArgs = {
   account: ?AccountLike,
   parentAccount: ?Account,
 };
+
 export const useBroadcast = ({ account, parentAccount }: SignTransactionArgs) => {
   const broadcast = useCallback(
     async (signedOperation: SignedOperation): Promise<Operation> => {
       invariant(account, "account not present");
       const mainAccount = getMainAccount(account, parentAccount);
       const bridge = getAccountBridge(account, parentAccount);
+
       if (getEnv("DISABLE_TRANSACTION_BROADCAST")) {
         return Promise.resolve(signedOperation.operation);
       }
