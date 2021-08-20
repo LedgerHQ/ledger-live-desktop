62c8904bce29f693fe33908374902d39975bc914
machard <matthieu__@hotmail.fr>
2021-08-20 15:23:12 +0200

    revert file

--- src/renderer/hooks/useBroadcast.js
+++ src/renderer/hooks/useBroadcast.js
@@ -1,8 +1,7 @@
 // @flow
 import invariant from "invariant";
 import { useCallback } from "react";
-import { log } from "@ledgerhq/logs";
-import { getMainAccount, formatOperation } from "@ledgerhq/live-common/lib/account";
+import { getMainAccount } from "@ledgerhq/live-common/lib/account";
 import type {
   SignedOperation,
   Operation,
@@ -12,34 +11,26 @@ import type {
 import { getAccountBridge } from "@ledgerhq/live-common/lib/bridge";
 import { execAndWaitAtLeast } from "@ledgerhq/live-common/lib/promise";
 import { getEnv } from "@ledgerhq/live-common/lib/env";
-
 type SignTransactionArgs = {
   account: ?AccountLike,
   parentAccount: ?Account,
 };
-
 export const useBroadcast = ({ account, parentAccount }: SignTransactionArgs) => {
   const broadcast = useCallback(
     async (signedOperation: SignedOperation): Promise<Operation> => {
       invariant(account, "account not present");
       const mainAccount = getMainAccount(account, parentAccount);
       const bridge = getAccountBridge(account, parentAccount);
-
       if (getEnv("DISABLE_TRANSACTION_BROADCAST")) {
         return Promise.resolve(signedOperation.operation);
       }
 
-      return execAndWaitAtLeast(3000, async () => {
-        const operation = await bridge.broadcast({
+      return execAndWaitAtLeast(3000, () =>
+        bridge.broadcast({
           account: mainAccount,
           signedOperation,
-        });
-        log(
-          "transaction-summary",
-          `✔️ broadcasted! optimistic operation: ${formatOperation(mainAccount)(operation)}`,
-        );
-        return operation;
-      });
+        }),
+      );
     },
     [account, parentAccount],
   );
