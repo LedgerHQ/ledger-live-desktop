a69b873fb1fc04472de9259c79a7375c1fc71971
jelbaz-ledger <julien.el-baz@ledger.fr>
2021-08-26 10:10:00 +0200

    swap2: store the transaction in redux to share the state with drawers

--- src/renderer/screens/exchange/Swap2/Form/FeesDrawer/index.js
+++ src/renderer/screens/exchange/Swap2/Form/FeesDrawer/index.js
@@ -1,11 +1,12 @@
 // @flow
+import React, { useCallback } from "react";
 import { Trans } from "react-i18next";
 import styled from "styled-components";
-import React, { useCallback } from "react";
+import { useSelector } from "react-redux";
 import Box from "~/renderer/components/Box";
 import Text from "~/renderer/components/Text";
 import SendAmountFields from "~/renderer/modals/Send/SendAmountFields";
-import useBridgeTransaction from "@ledgerhq/live-common/lib/bridge/useBridgeTransaction";
+import { transactionSelector } from "~/renderer/actions/swap";
 
 const Separator = styled.div`
   border-top: 1px solid ${p => p.theme.colors.palette.divider};
@@ -14,39 +15,8 @@ const Separator = styled.div`
 `;
 
 export default function FeesDrawer({ swapTransaction, disableSlowStrategy = false }: *) {
-  // Drawers are getting initial props but not the updates since the state lives in a separate Component in the hierarchy tree.
-  // Hence, this hack - keeping a copy of the transaction and updating both - seems to be needed.
-  // TO DIG: Might be a good thing to store swapTransaction in redux.
-  const {
-    transaction,
-    setTransaction: _setTransaction,
-    updateTransaction: _updateTransaction,
-    account,
-    parentAccount,
-    status,
-  } = useBridgeTransaction(() => ({
-    account: swapTransaction.account,
-    parentAccount: swapTransaction.parentAccount,
-    transaction: swapTransaction.transaction,
-  }));
-
-  const updateTransaction = useCallback(
-    (...args) => {
-      _updateTransaction(...args);
-      swapTransaction.updateTransaction(...args);
-    },
-    // eslint-disable-next-line
-    [_updateTransaction, swapTransaction.updateTransaction],
-  );
-
-  const setTransaction = useCallback(
-    (...args) => {
-      _setTransaction(...args);
-      swapTransaction.setTransaction(...args);
-    },
-    // eslint-disable-next-line
-    [_setTransaction, swapTransaction.setTransaction],
-  );
+  const { setTransaction, updateTransaction, account, parentAccount, status } = swapTransaction;
+  const transaction = useSelector(transactionSelector);
 
   const mapStrategies = useCallback(
     strategy =>
