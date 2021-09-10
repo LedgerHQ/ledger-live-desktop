42d8465629c75966eb870a139e3d41576c09d9f8
Gregor Gilchrist <gregor.gilchrist@ledger.fr>
2021-09-10 16:06:21 +0200

    added eth test

--- playwright/accounts.spec.js
+++ playwright/accounts.spec.js
@@ -1,17 +1,34 @@
 const { test, expect } = require("@playwright/test");
 const initialize = require("./testInitialize");
 
-test("get the app running", async () => {
+test("navigate to btc", async () => {
   const electronApp = await initialize("1AccountBTC1AccountETH");
   const window = await electronApp.firstWindow();
 
-  const title = await window.title();
-  console.log("Page title is: " + title);
-
   await window.screenshot({ path: "start-of-test.png" });
+  await window.pause();
 
-  // await window.pause();
+  const title = await window.isVisible("#page-scroller >> text=Bitcoin");
+  expect(title).toBe(true);
+
   await window.screenshot({ path: "end-of-test.png" });
+
+  await electronApp.close();
+});
+
+test("navigate to eth", async () => {
+  const electronApp = await initialize("1AccountBTC1AccountETH");
+  const window = await electronApp.firstWindow();
+
+  await window.screenshot({ path: "start-of-eth-test.png" });
+
+  await window.click("text=Accounts");
+  await window.click("text=Ethereum Ethereum");
+  await window.click("#page-scroller >> text=Ethereum");
+  const title = await window.isVisible("#page-scroller >> text=Ethereum");
+  expect(title).toBe(true);
+
+  await window.screenshot({ path: "end-of-eth-test.png" });
 
   await electronApp.close();
 });
