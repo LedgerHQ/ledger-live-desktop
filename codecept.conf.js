3c4e8fb7c6ad887b7690e782fa166423ec622e01
Arnaud97234 <arnaud.ulric@gmail.com>
2021-09-10 15:56:45 +0200

    condeceptJS implem

---
+++ codecept.conf.js
@@ -0,0 +1,38 @@
+const { setHeadlessWhen } = require("@codeceptjs/configure");
+
+// turn on headless mode when running with HEADLESS=true environment variable
+// export HEADLESS=true && npx codeceptjs run
+setHeadlessWhen(process.env.HEADLESS);
+
+exports.config = {
+  tests: "playwright/*_test.js",
+  output: "./output",
+  helpers: {
+    Playwright: {
+      show: true,
+      browser: "electron",
+      electron: {
+        executablePath: require("electron"),
+        args: ["./.webpack/main.bundle.js"],
+      },
+    },
+  },
+  include: {
+    I: "./steps_file.js",
+  },
+  bootstrap: null,
+  mocha: {},
+  name: "ledger-live-desktop",
+  plugins: {
+    pauseOnFail: {},
+    retryFailedStep: {
+      enabled: true,
+    },
+    tryTo: {
+      enabled: true,
+    },
+    screenshotOnFail: {
+      enabled: true,
+    },
+  },
+};
