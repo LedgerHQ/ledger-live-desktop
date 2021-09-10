0db5cb29797b35e7465c605a80c6f71e7d63110b
Gregor Gilchrist <gregor.gilchrist@ledger.fr>
2021-09-10 11:28:25 +0200

    got test running until nano check page

--- playwright/hack.spec.js
+++ playwright/hack.spec.js
@@ -3,50 +3,39 @@ const { _electron: electron } = require("playwright");
 const { options } = require("yargs");
 
 test("get the app running", async () => {
-  // Launch Electron app.
   const electronApp = await electron.launch({
     args: ["./.webpack/main.bundle.js"],
   });
 
-  // Get the first window that the app opens, wait if necessary.
+  console.log("about to check for window")
+
   const window = await electronApp.firstWindow();
-  // Print the title.
 
   const title = await window.title();
   console.log("Page title is: " + title);
 
-  // Playwright has auto wait built in, so if we want to waitUntil something has
-  // loaded without performing any action(like a snapshot), we can call 'click'
-  // without performing the action, by passing the option {trial: true}
-  await window.screenshot({ path: "beforeClicking.png" });
+  await window.screenshot({ path: "start-of-test.png" });
 
-  // window.pause();
-
-  // // Getting the text from an element is messy, there's no built-in method...
-  // await console.log(
-  //   await window.evaluate(
-  //     el => el.innerTexelectronApp.trim(),
-  //     await window.$("#onboarding-get-started-button"),
-  //   ),
-  // );
   await window.waitForSelector('#__app__ready__', { state: 'attached'});
 
   await window.waitForLoadState('domcontentloaded');
 
-  // await window.pause();
-
   // await window.waitForSelector('button#onboarding-get-started-button', { state: 'visible'});
 
   const button = await window.$('#onboarding-get-started-button');
-  console.log("buttonText:" + await button.innerHTML());
+  console.log("buttonText: " + await button.innerHTML());
   await button.isEnabled();
 
-  // await window.click("button#onboarding-get-started-button");
-  await button.click();
+  await window.click("button#onboarding-get-started-button");
   await window.screenshot({ path: "afterClicking.png" });
+
   // await window.pause();
+  await window.click("#onboarding-terms-check");
+  await window.click('button:has-text("Enter Ledger app")');
+  await window.click('button:has-text("Nano S")');
+  await window.click('button:has-text("Connect deviceConnect your Nano SIs your device already set up? Connect it to th")');
+  await window.click('button:has-text("Check my Nano")');
 
-
-  // // Exit app.
+  await window.screenshot({ path: "end-of-test.png" });
   await electronApp.close();
 });
