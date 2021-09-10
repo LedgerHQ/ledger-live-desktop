7d88dc7ccb8a610a8ffc283167a702f6f672c736
Valentin D. Pinkman <valentin.d.pinkman@hey.com>
2021-09-10 14:47:26 +0200

    fix config and remove package-lock.json

--- playwright/hack.spec.js
+++ playwright/hack.spec.js
@@ -1,6 +1,5 @@
 const { test, expect } = require("@playwright/test");
 const { _electron: electron } = require("playwright");
-const { options } = require("yargs");
 
 test("get the app running", async () => {
   const electronApp = await electron.launch({
@@ -16,14 +15,14 @@ test("get the app running", async () => {
 
   await window.screenshot({ path: "start-of-test.png" });
 
-  await window.waitForSelector('#__app__ready__', { state: 'attached'});
+  await window.waitForSelector("#__app__ready__", { state: "attached" });
 
-  await window.waitForLoadState('domcontentloaded');
+  await window.waitForLoadState("domcontentloaded");
 
   // await window.waitForSelector('button#onboarding-get-started-button', { state: 'visible'});
 
-  const button = await window.$('#onboarding-get-started-button');
-  console.log("buttonText: " + await button.innerHTML());
+  const button = await window.$("#onboarding-get-started-button");
+  console.log("buttonText: " + (await button.innerHTML()));
   await button.isEnabled();
 
   await window.click("button#onboarding-get-started-button");
@@ -33,7 +32,9 @@ test("get the app running", async () => {
   await window.click("#onboarding-terms-check");
   await window.click('button:has-text("Enter Ledger app")');
   await window.click('button:has-text("Nano S")');
-  await window.click('button:has-text("Connect deviceConnect your Nano SIs your device already set up? Connect it to th")');
+  await window.click(
+    'button:has-text("Connect deviceConnect your Nano SIs your device already set up? Connect it to th")',
+  );
   await window.click('button:has-text("Check my Nano")');
 
   await window.screenshot({ path: "end-of-test.png" });
--- playwright/playwright.config.js
+++ playwright/playwright.config.js
@@ -3,7 +3,8 @@ const config = {
     headless: false,
     viewport: { width: 580, height: 300 },
     ignoreHTTPSErrors: true,
-    //timeout doesn't seem to have much effect. Needs `--timeout=60000` in the command line
+    deviceScaleFactor: 1,
+    // timeout doesn't seem to have much effect. Needs `--timeout=60000` in the command line
     timeout: 60000,
   },
 };
