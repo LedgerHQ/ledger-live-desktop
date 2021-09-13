5ac1b46a4890a3c89b00ebe08b4e3614793a4e72
jgiamporcaro-ledger <jonathan.giamporcaro@gmail.com>
2021-09-13 16:30:43 +0200

    design(swap): integrate not available screen

--- src/renderer/screens/exchange/Swap2/Form/FormNotAvailable/FormNotAvailable.js
+++ src/renderer/screens/exchange/Swap2/Form/FormNotAvailable/FormNotAvailable.js
@@ -1,12 +1,11 @@
 // @flow
 
 import React from "react";
-import { useTranslation } from "react-i18next";
+import { useTranslation, Trans } from "react-i18next";
 import styled from "styled-components";
 import Text from "~/renderer/components/Text";
 import Box from "~/renderer/components/Box";
-import WorldMap from "~/renderer/icons/WorldMap";
-import IconExclamationCircleThin from "~/renderer/icons/ExclamationCircleThin";
+import SwapCircle from "~/renderer/icons/SwapCircle";
 
 import Track from "~/renderer/analytics/Track";
 
@@ -14,8 +13,17 @@ const Body = styled(Box).attrs({
   alignItems: "center",
   color: "palette.text.shade50",
 })`
-  max-width: 16rem;
+  max-width: 25.5rem;
   position: absolute;
+  row-gap: 1.5rem;
+`;
+
+const TextSection = styled(Box)`
+  row-gap: 0.8125rem;
+`;
+
+const IconContainer = styled.div`
+  color: ${p => p.theme.colors.palette.primary.main};
 `;
 
 const FormNotAvailable = () => {
@@ -24,18 +32,23 @@ const FormNotAvailable = () => {
   return (
     <Box justifyContent="center" alignItems="center">
       <Track onMount event="NotAvailable" />
-      <WorldMap />
       <Body>
-        <IconExclamationCircleThin size={40} />
-        <Text
-          textAlign="center"
-          mt={3}
-          ff="Inter|SemiBold"
-          fontSize={6}
-          color="palette.text.shade100"
-        >
-          {t("swap2.form.notAvailable")}
-        </Text>
+        <IconContainer>
+          <SwapCircle size={70} />
+        </IconContainer>
+        <TextSection>
+          <Text textAlign="center" ff="Inter|SemiBold" fontSize={5} color="palette.text.shade100">
+            {t("swap2.form.notAvailable.title")}
+          </Text>
+          <Text
+            textAlign="center"
+            ff="Inter|Regular"
+            fontSize="0.875rem"
+            color="palette.text.shade50"
+          >
+            <Trans i18nKey="swap2.form.notAvailable.content" components={{ br: <br /> }} />
+          </Text>
+        </TextSection>
       </Body>
     </Box>
   );
