c1ca6fdfcf646527377657a60585a13f235cd461
Arnaud97234 <arnaud.ulric@gmail.com>
2021-09-13 10:26:12 +0200

    codeceptjs poc

--- codecept.conf.js
+++ codecept.conf.js
@@ -5,7 +5,7 @@ const { setHeadlessWhen } = require("@codeceptjs/configure");
 setHeadlessWhen(process.env.HEADLESS);
 
 exports.config = {
-  tests: "playwright/*.spec.js",
+  tests: "playwright/*_test.js",
   output: "./output",
   helpers: {
     Playwright: {
