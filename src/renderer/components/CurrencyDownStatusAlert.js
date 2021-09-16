8cc4f4507b778a88aac72d1c9994137983c827c1
machard <matthieu__@hotmail.fr>
2021-09-16 10:15:23 +0200

    feedback

--- src/renderer/components/CurrencyDownStatusAlert.js
+++ src/renderer/components/CurrencyDownStatusAlert.js
@@ -1,17 +1,13 @@
 // @flow
 import React from "react";
-import { createCustomErrorClass } from "@ledgerhq/errors";
 import type { TokenCurrency, CryptoCurrency } from "@ledgerhq/live-common/lib/types";
 import ErrorBanner from "./ErrorBanner";
 type Props = {
   currencies: Array<CryptoCurrency | TokenCurrency>,
 };
 
-const StratisDown2021Warning = createCustomErrorClass("StratisDown2021Warning");
-
 const CurrencyDownStatusAlert = ({ currencies }: Props) => {
   const errors = [];
-  if (currencies.some(c => c.id === "stratis")) errors.push(new StratisDown2021Warning());
 
   return errors.length > 0 ? (
     <div>
