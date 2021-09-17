8178f6d5970ad0778f60d74a0bb53f0db0f1cc5d
Jonathan Giamporcaro <jonathan.giamporcaro@gmail.com>
2021-09-17 14:28:36 +0200

    LL-6359 - SWAP - Display fees strategy icon (#4209)
    * design(swap): add fees strategy icon
    
    * review: fix useMemo call

--- src/renderer/screens/exchange/Swap2/Form/FormSummary/SectionFees.js
+++ src/renderer/screens/exchange/Swap2/Form/FormSummary/SectionFees.js
@@ -12,7 +12,37 @@ import sendAmountByFamily from "~/renderer/generated/SendAmountFields";
 import type { SwapTransactionType } from "~/renderer/screens/exchange/Swap2/utils/shared/useSwapTransaction";
 import { rateSelector } from "~/renderer/actions/swap";
 import FormattedVal from "~/renderer/components/FormattedVal";
+import Box from "~/renderer/components/Box";
 import Text from "~/renderer/components/Text";
+import TachometerHigh from "~/renderer/icons/TachometerHigh";
+import TachometerLow from "~/renderer/icons/TachometerLow";
+import TachometerMedium from "~/renderer/icons/TachometerMedium";
+import styled from "styled-components";
+
+type Strategies = "slow" | "medium" | "fast" | "advanced";
+const FEES_STRATEGY_ICONS: {
+  [Strategies]: ({ color?: string, size?: number }) => React$Element<"svg">,
+} = {
+  slow: TachometerLow,
+  medium: TachometerMedium,
+  fast: TachometerHigh,
+  advanced: TachometerHigh,
+};
+
+const IconSection = styled(Box)`
+  flex-direction: row;
+  column-gap: 0.25rem;
+  color: ${props => props.theme.colors.palette.text.shade40};
+`;
+
+const Separator = styled.div`
+  width: 3px;
+  height: 3px;
+  background-color: ${props => props.theme.colors.palette.text.shade40};
+  border-radius: 9999px;
+  align-self: center;
+  margin-left: 2px;
+`;
 
 const SectionFees = ({ swapTransaction }: { swapTransaction: SwapTransactionType }) => {
   const { t } = useTranslation();
@@ -25,6 +55,10 @@ const SectionFees = ({ swapTransaction }: { swapTransaction: SwapTransactionType
   const showSummaryValue = fromAccountUnit && estimatedFees && estimatedFees.gt(0);
   const canEdit =
     showSummaryValue && transaction?.networkInfo && sendAmountByFamily[account?.currency?.family];
+  const StrategyIcon = useMemo(
+    () => FEES_STRATEGY_ICONS[swapTransaction.transaction?.feesStrategy],
+    [swapTransaction.transaction?.feesStrategy],
+  );
 
   // Deselect slow strategy if the exchange rate is changed to fixed.
   useEffect(
@@ -55,15 +89,24 @@ const SectionFees = ({ swapTransaction }: { swapTransaction: SwapTransactionType
   );
 
   const summaryValue = canEdit ? (
-    <FormattedVal
-      color="palette.text.shade100"
-      val={estimatedFees}
-      unit={fromAccountUnit}
-      fontSize={3}
-      ff="Inter|SemiBold"
-      showCode
-      alwaysShowValue
-    />
+    <>
+      <IconSection>
+        {StrategyIcon ? <StrategyIcon /> : null}
+        <Text fontSize={4} fontWeight="600">
+          {t(`fees.${swapTransaction.transaction?.feesStrategy}`)}
+        </Text>
+        <Separator />
+      </IconSection>
+      <FormattedVal
+        color="palette.text.shade100"
+        val={estimatedFees}
+        unit={fromAccountUnit}
+        fontSize={3}
+        ff="Inter|SemiBold"
+        showCode
+        alwaysShowValue
+      />
+    </>
   ) : (
     <Text color="palette.text.shade100" fontSize={4}>
       {"-"}
