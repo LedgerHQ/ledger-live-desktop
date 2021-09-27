6830c3bf3d6abb7da8916a0471b928408f623330
LFBarreto <lfb.access@gmail.com>
2021-09-27 16:22:51 +0200

    (Rebrand): removal of all design system resources from main repo -> moving to ledherHQ/ui (#4266)

--- ui-lib/src/styles/index.ts
+++
@@ -1,6 +0,0 @@
-export { default as defaultTheme } from "./theme";
-export { default as palettes } from "./palettes";
-export * from "./helpers";
-export * from "./global";
-export * from "./reset";
-export * from "./StyleProvider";
