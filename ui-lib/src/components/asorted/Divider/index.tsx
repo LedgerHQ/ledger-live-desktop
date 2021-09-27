0606e32530b718bedd390346b6642905b553ebbf
LFBarreto <lfb.access@gmail.com>
2021-09-27 14:17:55 +0200

    (Rebrand): removal of all design system resources from main repo -> moving to ledherHQ/ui

--- ui-lib/src/components/asorted/Divider/index.tsx
+++
@@ -1,9 +0,0 @@
-import styled from "styled-components";
-
-const Divider = styled.div`
-  display: block;
-  background: ${(p) => p.theme.colors.palette.neutral.c90};
-  height: 1px;
-`;
-
-export default Divider;
