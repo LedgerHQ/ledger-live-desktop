2f23f6130c4b6502e61ce05382c0c0e0471f729f
Valentin D. Pinkman <valentin.d.pinkman@hey.com>
2021-09-28 12:56:23 +0200

    fix args in build

--- tools/dist/index.js
+++ tools/dist/index.js
@@ -79,8 +79,6 @@ const buildTasks = args => [
       if (args.nightly) {
         commands.push("--config");
         commands.push("electron-builder-nightly.yml");
-        commands.push("--publish");
-        commands.push("always");
       }
       if (args.ci) {
         commands.push("--config");
