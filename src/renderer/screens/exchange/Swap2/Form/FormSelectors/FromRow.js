0ac5c186f3120d2c360e797f08f22108ccc6350e
LFBarreto <lfb.access@gmail.com>
2021-10-08 14:03:21 +0200

    LL-7233 (Swap): source selector ordering logic updated

--- src/renderer/screens/exchange/Swap2/Form/FormSelectors/FromRow.js
+++ src/renderer/screens/exchange/Swap2/Form/FormSelectors/FromRow.js
@@ -1,5 +1,5 @@
 // @flow
-import React from "react";
+import React, { useEffect } from "react";
 import { useSelector } from "react-redux";
 import { useTranslation } from "react-i18next";
 import styled from "styled-components";
@@ -18,9 +18,38 @@ import type {
   SwapSelectorStateType,
   SwapTransactionType,
 } from "@ledgerhq/live-common/lib/exchange/swap/hooks";
-import { usePickDefaultAccount } from "@ledgerhq/live-common/lib/exchange/swap/hooks";
 import { track } from "~/renderer/analytics/segment";
 import { SWAP_VERSION } from "../../utils/index";
+
+import { useCurrenciesByMarketcap } from "@ledgerhq/live-common/lib/currencies/sortByMarketcap";
+import { listCryptoCurrencies, listTokens } from "@ledgerhq/live-common/lib/currencies";
+
+// Pick a default source account if none are selected.
+// TODO use live-common once its ready
+const usePickDefaultAccount = (accounts, fromAccount, setFromAccount): void => {
+  const list = [...listCryptoCurrencies(), ...listTokens()];
+  const allCurrencies = useCurrenciesByMarketcap(list);
+
+  useEffect(() => {
+    if (!fromAccount && allCurrencies.length > 0) {
+      allCurrencies.some(({ id }) => {
+        const filteredAcc = accounts.filter(
+          acc => getAccountCurrency(acc)?.id === id && acc.balance.gt(0) && !acc.disabled,
+        );
+
+        if (filteredAcc.length > 0) {
+          const defaultAccount = filteredAcc
+            .sort((a, b) => b.balance.minus(a.balance).toNumber())
+            .find(Boolean);
+
+          setFromAccount(defaultAccount);
+          return true;
+        }
+        return false;
+      });
+    }
+  }, [accounts, allCurrencies, fromAccount, setFromAccount]);
+};
 
 type Props = {
   fromAccount: $PropertyType<SwapSelectorStateType, "account">,
