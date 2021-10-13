a8bef29990084dc95a5efbf774e662ad390e71f6
Valentin D. Pinkman <valentin.d.pinkman@icloud.com>
2021-10-13 17:07:42 +0200

    new icons + fixes for autoupdate and overlay in nightly mode (#4317)

--- src/renderer/Default.js
+++ src/renderer/Default.js
@@ -48,7 +48,6 @@ import { ToastOverlay } from "~/renderer/components/ToastOverlay";
 import Drawer from "~/renderer/drawers/Drawer";
 import UpdateBanner from "~/renderer/components/Updater/Banner";
 import FirmwareUpdateBanner from "~/renderer/components/FirmwareUpdateBanner";
-import pkg from "../../package.json";
 
 export const TopBannerContainer: ThemedComponent<{}> = styled.div`
   position: sticky;
@@ -207,7 +206,7 @@ export default function Default() {
                     <ToastOverlay />
                   </Box>
 
-                  {pkg.name === "ledger-live-desktop-nightly" ? <NightlyLayer /> : null}
+                  {__NIGHTLY__ ? <NightlyLayer /> : null}
 
                   <LibcoreBusyIndicator />
                   <DeviceBusyIndicator />
