cdaf77481dc587b88cff2fef94f68f3713dc15fd
Olivier Freyssinet <olivier.freyssinet@ledger.fr>
2021-10-25 09:58:08 +0200

    Use getLanguages for LangSwitcher instead of hardcoded value

--- src/renderer/components/Onboarding/LangSwitcher.v3.tsx
+++ src/renderer/components/Onboarding/LangSwitcher.v3.tsx
@@ -5,16 +5,11 @@ import { langAndRegionSelector } from "~/renderer/reducers/settings";
 import { useDispatch, useSelector } from "react-redux";
 import { languageLabels } from "~/renderer/screens/settings/sections/General/LanguageSelect";
 import { Dropdown } from "@ledgerhq/react-ui";
+import { getLanguages } from "~/config/languages";
 
 import moment from "moment";
 
-const options = [
-  { value: "en", support: "full", label: languageLabels.en },
-  { value: "fr", support: "full", label: languageLabels.fr },
-  { value: "ru", support: "partial", label: languageLabels.ru },
-  { value: "zh", support: "partial", label: languageLabels.zh },
-  { value: "es", support: "partial", label: languageLabels.es },
-];
+const options = getLanguages().map(value => ({ value, label: languageLabels[value] }));
 
 const styles = {
   // TODO: implement this behavior in the @ledger/ui lib, here we are just overriding the style from the design system lib to have the MENU right aligned
