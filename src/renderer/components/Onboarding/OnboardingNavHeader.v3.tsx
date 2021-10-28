54199332751c8397d2820b6781e75bb87fb789c5
Olivier Freyssinet <olivier.freyssinet@ledger.fr>
2021-10-28 18:19:20 +0200

    Use Navigator component from ui lib

--- src/renderer/components/Onboarding/OnboardingNavHeader.v3.tsx
+++ src/renderer/components/Onboarding/OnboardingNavHeader.v3.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import styled from "styled-components";
-import { Button } from "@ledgerhq/react-ui";
+import { Button, Header } from "@ledgerhq/react-ui";
 import LangSwitcher from "./LangSwitcher";
 import ledgerLogo from "./assets/ledgerLogo.svg";
 import { registerAssets } from "~/renderer/components/Onboarding/preloadAssets";
@@ -9,15 +9,12 @@ import { useTranslation } from "react-i18next";
 
 registerAssets([ledgerLogo]);
 
-const OnboardingContainer = styled.div`
+const OnboardingContainer = styled(Header)`
   position: absolute;
   top: 40px;
   left: 16px;
   right: 30px;
   z-index: 1;
-  display: flex;
-  flex-direction: row;
-  justify-content: space-between;
   align-items: center;
 `;
 
@@ -35,13 +32,15 @@ interface Props {
 
 export default function OnboardingNavHeader({ onClickPrevious }: Props) {
   const { t } = useTranslation();
+  const left = (
+    <Button iconPosition="left" Icon={ArrowLeftRegular} onClick={onClickPrevious}>
+      {t("common.previous")}
+    </Button>
+  );
+  const right = <LangSwitcher />;
   return (
-    <OnboardingContainer>
-      <Button iconPosition="left" Icon={ArrowLeftRegular} onClick={onClickPrevious}>
-        {t("common.previous")}
-      </Button>
+    <OnboardingContainer {...{ left, right }}>
       <Logo />
-      <LangSwitcher />
     </OnboardingContainer>
   );
 }
