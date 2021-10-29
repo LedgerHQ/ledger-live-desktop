6f46e24fff3f61900e2913855ed9ee74d82cd6f5
Olivier Freyssinet <olivier.freyssinet@ledger.fr>
2021-10-29 16:47:55 +0200

    Add env var for testing ModalStepper

--- src/renderer/ModalsLayer.js
+++ src/renderer/ModalsLayer.js
@@ -10,7 +10,7 @@ import type { ThemedComponent } from "~/renderer/styles/StyleProvider";
 import { modalsStateSelector } from "~/renderer/reducers/modals";
 import modals from "~/renderer/modals";
 import ModalStepper from "./components/ModalStepper";
-import {stepperSteps} from "./components/ModalStepper/testModal";
+import { stepperSteps } from "./components/ModalStepper/testModal";
 
 // TODO: SNOOOOOOOOWW
 // import Snow, { isSnowTime } from '~/renderer/components/extra/Snow'
@@ -39,9 +39,11 @@ const ModalsLayer = ({ visibleModals }: *) => {
       MODAL_SHOW_ONCE && global.sessionStorage.setItem(name, Date.now()),
   );
 
+  const showTestModalStepper = process.env.TEST_MODAL_STEPPER;
+
   return (
     <Transition
-      in={true || filteredModals.length > 0}
+      in={showTestModalStepper || filteredModals.length > 0}
       appear
       mountOnEnter
       unmountOnExit
@@ -53,12 +55,14 @@ const ModalsLayer = ({ visibleModals }: *) => {
     >
       {state => (
         <BackDrop state={state}>
-          <ModalStepper
-            title="Basics"
-            onClose={() => console.log('onClose')}
-            onFinish={() => console.log('onFinish')}
-            steps={stepperSteps}
-          />
+          {showTestModalStepper ? (
+            <ModalStepper
+              title="Basics"
+              onClose={() => console.log("onClose")}
+              onFinish={() => console.log("onFinish")}
+              steps={stepperSteps}
+            />
+          ) : null}
           {/* {// Will only render at the end of december
           isSnowTime() ? <Snow numFlakes={200} /> : null} */}
           {filteredModals.map(({ name, ...data }, i) => {
