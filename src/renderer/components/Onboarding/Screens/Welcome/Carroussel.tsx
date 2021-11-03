b550a568d0bf64f515f1aec31085c9513d1babe0
Thomas Laforge <thomas.laforge@ledger.fr>
2021-11-03 09:46:23 +0100

    carousel fit welcome needs

--- src/renderer/components/Onboarding/Screens/Welcome/Carroussel.tsx
+++ src/renderer/components/Onboarding/Screens/Welcome/Carroussel.tsx
@@ -1,14 +1,10 @@
 import React, { useState, useCallback, useEffect, useRef } from "react";
 
-import { Button, Flex, Logos, Text } from "@ledgerhq/react-ui";
+import { Button, Flex, Icons, Logos, Text } from "@ledgerhq/react-ui";
 import { useTranslation } from "react-i18next";
 import styled from "styled-components";
 import { TransitionGroup } from "react-transition-group";
 import TransitionSlide from "@ledgerhq/react-ui/components/transitions/TransitionSlide";
-
-import IconLeft from "@ledgerhq/react-ui/assets/icons/ArrowLeftMedium";
-import IconRight from "@ledgerhq/react-ui/assets/icons/ArrowRightMedium";
-import IconClose from "@ledgerhq/react-ui/assets/icons/CloseMedium";
 
 export interface CarrousselStep {
     logo: string,
@@ -16,50 +12,41 @@ export interface CarrousselStep {
     descKey: string,
 }
 
-interface CarrousselProps {
-    onChange: (newIndex: number) => void,
-    currentIndex: number,
-    steps: CarrousselStep[]
-} 
-
-// export default function Carroussel(props: CarrousselProps){
-//     const {steps, currentIndex, onChange} = props;
-//     const {t} = useTranslation();
-//     const currentStep = steps[currentIndex];
-
-//     return <Flex flexDirection="column">
-//         <Text type="h3">{t(currentStep.titleKey)}</Text>
-//         <Text type="body">{t(currentStep.descKey)}</Text>
-//     </Flex>
-// }
-
-const Wrapper = styled(Flex)<{ image?: string }>`
-  width: 100%;
-  height: 100%;
-  background: gray;
+const Wrapper = styled(Flex)`
   flex-direction: column;
-  padding: ${(p) => p.theme.space[8]}px ${(p) => p.theme.space[10]}px;
-  padding-top: 140px; // Nb gives air to not overlap the illustration
-  z-index: ${(p) => p.theme.zIndexes[8]};
-  background: url(${(p) => p.image}) no-repeat ${(p) => p.theme.colors.palette.neutral.c100};
+  align-items: center;
+  position: relative;
+  flex: 1;
+  text-align: center;
+  width: 350px;
+  margin: auto;
+  z-index: 999;
+`;
+
+const SlideLogo = styled(Flex)<{ image?: string }>`
+  min-height: 350px;
+  min-width: 350px;
+  margin-bottom: 40px;
+  background: url(${(p) => p.image}) no-repeat;
+  background-position: center center; 
   background-size: contain;
-  background-position: right 60px bottom;
+  z-index: ${(p) => p.theme.zIndexes[8]};
 `;
 
 export type SlideProps = {
-  onClick: () => void;
   title: string;
   description: string;
   image?: string;
 };
 
-const Slide = ({ title, description, image, onClick }: SlideProps): React.ReactElement => {
+const Slide = ({ title, description, image }: SlideProps): React.ReactElement => {
   return (
-    <Wrapper key={"key"} image={image} onClick={onClick}>
-      <Text color="palette.neutral.c00" ff="Inter|Regular" fontSize={10}>
+    <Wrapper>
+      <SlideLogo key={"key"} image={image} />
+      <Text mb={12} ff="Alpha|Medium" textTransform="uppercase" type="h3" fontWeight="400" fontSize={28}>
         {title}
       </Text>
-      <Text color="palette.neutral.c00" ff="Alpha|Medium" textTransform="uppercase" fontSize={20}>
+      <Text mb={76} type="body" ff="Alpha|Medium" fontWeight="500" fontSize={14}>
         {description}
       </Text>
     </Wrapper>
@@ -67,14 +54,14 @@ const Slide = ({ title, description, image, onClick }: SlideProps): React.ReactE
 };
 
 const CarouselWrapper = styled.div`
-  width: 100%;
-  height: 180px;
-  border-radius: 8px;
-  overflow: hidden;
-  cursor: pointer;
-  position: relative;
   flex: 1;
-  background: ${(p) => p.theme.colors.palette.neutral.c100};
+  height: 700px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  cursor: pointer;
+  margin: 44px;
+  position: relative;
 `;
 
 const Controllers = styled(Flex)`
@@ -127,45 +114,20 @@ const Bullets = styled.div<{ active?: number }>`
   }
 `;
 
-const Close = styled.div`
-  position: absolute;
-  top: ${(p) => p.theme.space[7]}px;
-  right: ${(p) => p.theme.space[5]}px;
-  color: ${(p) => p.theme.colors.palette.neutral.c00};
-  &:hover {
-    opacity: 0.5;
-  }
-`;
-
-const DismissWrapper = styled.div`
-  color: white;
-  align-items: center;
-  justify-content: center;
-  display: flex;
-  flex-direction: column;
-  padding: ${(p) => p.theme.space[13]}px;
-  row-gap: ${(p) => p.theme.space[7]}px;
-`;
-
 export type Props = {
   timeout?: number;
   queue: SlideProps[];
-  isDismissed: boolean;
-  onDismiss: () => void;
 };
 
-const DEFAULT_TIMEOUT = 7000;
+const DEFAULT_TIMEOUT = 20000;
 const Carousel = ({
   timeout = DEFAULT_TIMEOUT,
   queue,
-  isDismissed,
-  onDismiss,
 }: Props): React.ReactElement | null => {
   const intervalRef = useRef<ReturnType<typeof setInterval>>();
 
   const [direction, setDirection] = useState("right");
   const [index, setIndex] = useState(0);
-  const [wantToDismiss, setWantToDismiss] = useState(false);
   const [paused, setPaused] = useState(false);
   const childFactory = useCallback(
     (child) => React.cloneElement(child, { direction }),
@@ -191,9 +153,6 @@ const Carousel = ({
   const onPrevious = useCallback(() => onSlide("right"), [onSlide]);
   const onNext = useCallback(() => onSlide("left"), [onSlide]);
 
-  const onWantToDismiss = () => setWantToDismiss(true);
-  const onCancelDismiss = () => setWantToDismiss(false);
-
   const onMouseEnter = () => setPaused(true);
   const onMouseLeave = () => setPaused(false);
 
@@ -205,56 +164,30 @@ const Carousel = ({
     if (!paused) intervalRef.current = setInterval(onSlide, _timeout);
   }, [onSlide, paused, timeout]);
 
-  useEffect(() => {
-    if (isDismissed) setWantToDismiss(false);
-  }, [isDismissed]);
-
-  if (!queue?.length || isDismissed) return null;
+  if (!queue?.length) return null;
 
   return (
-    <CarouselWrapper id={"carousel"} onMouseEnter={onMouseEnter} onMouseLeave={onMouseLeave}>
-      {wantToDismiss ? (
-        <DismissWrapper>
-          <Text color="palette.neutral.c00" ff="Inter|Medium" fontSize={13}>
-            {"This banner will not show up again until there is a new announcement"}
-          </Text>
-          <Flex>
-            <Button color="palette.neutral.c00" type="secondary" onClick={onDismiss}>
-              {"Confirm"}
-            </Button>
-            <Button color="palette.neutral.c00" type="primary" onClick={onCancelDismiss}>
-              {"Show again"}
-            </Button>
-          </Flex>
-        </DismissWrapper>
-      ) : (
-        <div>
-          <TransitionGroup component={null} childFactory={childFactory}>
-            <TransitionSlide key={index} direction={direction}>
-              <Slide {...queue[index]} />
-            </TransitionSlide>
-          </TransitionGroup>
+    <CarouselWrapper onMouseEnter={onMouseEnter} onMouseLeave={onMouseLeave}>
+      <TransitionGroup component={null} childFactory={childFactory}>
+        <TransitionSlide key={index} direction={direction}>
+          <Slide {...queue[index]} />
+        </TransitionSlide>
+      </TransitionGroup>
 
-          <Close id={"carousel-dismiss"} onClick={onWantToDismiss}>
-            <IconClose size={18} />
-          </Close>
+      <Bullets active={index + 1}>
+        {queue.map((_, i) => (
+          <div key={`bullet_${i}`} onClick={() => wrappedSetIndex(i)} />
+        ))}
+      </Bullets>
 
-          <Bullets active={index + 1}>
-            {queue.map((_, i) => (
-              <div key={`bullet_${i}`} onClick={() => wrappedSetIndex(i)} />
-            ))}
-          </Bullets>
-
-          <Controllers>
-            <div onClick={onPrevious}>
-              <IconLeft size={20} />
-            </div>
-            <div onClick={onNext}>
-              <IconRight size={20} />
-            </div>
-          </Controllers>
+      <Controllers>
+        <div onClick={onPrevious}>
+          <Icons.ArrowLeftMedium size={20} />
         </div>
-      )}
+        <div onClick={onNext}>
+          <Icons.ArrowRightMedium size={20} />
+        </div>
+      </Controllers>
     </CarouselWrapper>
   );
 };
