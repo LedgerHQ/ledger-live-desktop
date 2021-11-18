5f48006de89cafb0f14ee2783ffe7e4619f5bb31
Olivier Freyssinet <olivier.freyssinet@ledger.fr>
2021-11-18 15:59:34 +0100

    Add v3 styling for the "Add account" button

--- src/renderer/screens/accounts/AccountRowItem/Placeholder.tsx
+++ src/renderer/screens/accounts/AccountRowItem/Placeholder.tsx
@@ -1,32 +1,9 @@
 import React, { useCallback } from "react";
 import { useTranslation } from "react-i18next";
 import { useDispatch } from "react-redux";
-import styled from "styled-components";
-
+import { Icons } from "@ledgerhq/react-ui";
 import { openModal } from "~/renderer/actions/modals";
-import Box from "~/renderer/components/Box";
-import IconPlus from "~/renderer/icons/Plus";
-import { ThemedComponent } from "~/renderer/styles/StyleProvider";
-
-const AddAccountButton: ThemedComponent<{}> = styled.div`
-  border: 1px dashed rgba(153, 153, 153, 0.3);
-  cursor: pointer;
-  border-radius: 4px;
-  padding: 20px;
-  color: #abadb6;
-  font-weight: 600;
-  align-items: center;
-  justify-content: center;
-  display: flex;
-  flex-direction: row;
-  height: auto;
-
-  &:hover {
-    cursor: pointer;
-    border-color: ${p => p.theme.colors.palette.text.shade100};
-    color: ${p => p.theme.colors.palette.text.shade100};
-  }
-`;
+import Button from "~/renderer/components/Button";
 
 const Placeholder = () => {
   const dispatch = useDispatch();
@@ -37,14 +14,18 @@ const Placeholder = () => {
   }, [dispatch]);
 
   return (
-    <Box mb={5}>
-      <AddAccountButton isAccountRow onClick={openAddAccounts} pb={6}>
-        <IconPlus size={16} mr={20} />
-        <Box ml={2} ff="Inter|Regular" fontSize={4}>
-          {t("addAccounts.cta.add")}
-        </Box>
-      </AddAccountButton>
-    </Box>
+    <Button
+      outline
+      variant="shade"
+      Icon={Icons.PlusMedium}
+      onClick={openAddAccounts}
+      iconPosition="left"
+      mt={8}
+      ml={12}
+      mr={12}
+    >
+      {t("addAccounts.cta.add")}
+    </Button>
   );
 };
 
