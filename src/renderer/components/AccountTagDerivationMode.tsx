e7d11b678d358be32b6b788f3e909d66a9ff99fd
Olivier Freyssinet <olivier.freyssinet@ledger.fr>
2021-11-22 17:13:11 +0100

    Add TagProps to AccountTagDerivationMode

--- src/renderer/components/AccountTagDerivationMode.tsx
+++ src/renderer/components/AccountTagDerivationMode.tsx
@@ -1,14 +1,21 @@
 import React from "react";
 import { Tag } from "@ledgerhq/react-ui";
+import { TagProps } from "@ledgerhq/react-ui/components/Tag";
 import { AccountLike } from "@ledgerhq/live-common/lib/types";
 import { getTagDerivationMode } from "@ledgerhq/live-common/lib/derivation";
 
 type Props = {
   account: AccountLike;
   margin?: string;
-};
+} & TagProps;
 
-export default function AccountTagDerivationMode({ account, margin }: Props) {
+export default function AccountTagDerivationMode({
+  account,
+  margin,
+  disabled = true,
+  type,
+  ...otherProps
+}: Props) {
   if (account.type !== "Account") return null;
 
   const tag =
@@ -17,7 +24,14 @@ export default function AccountTagDerivationMode({ account, margin }: Props) {
     getTagDerivationMode(account.currency, account.derivationMode);
 
   return tag ? (
-    <Tag type="outlinedOpacity" size="small" disabled active margin={margin ?? "0 6px"}>
+    <Tag
+      type={"outlinedOpacity" || type}
+      size="small"
+      disabled={disabled}
+      active
+      margin={margin ?? "0 6px"}
+      {...otherProps}
+    >
       {tag}
     </Tag>
   ) : null;
