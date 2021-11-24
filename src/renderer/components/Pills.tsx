a1afaf2c358ad83f6eeeabcc2e325cb7f65c2a59
Olivier Freyssinet <olivier.freyssinet@ledger.fr>
2021-11-24 14:43:09 +0100

    Add v3 styling for Pills component

---
+++ src/renderer/components/Pills.tsx
@@ -0,0 +1,35 @@
+import React from "react";
+import { Bar, Text } from "@ledgerhq/react-ui";
+
+export type Item = {
+  label: string;
+  key: string;
+  value?: any;
+};
+
+type Props = {
+  items: Item[];
+  activeKey: string;
+  onChange: (arg0: Item) => void;
+};
+
+function Pills(props: Props) {
+  const { items, activeKey, onChange } = props;
+
+  return (
+    <Bar
+      initialActiveIndex={items.findIndex(it => it.key === activeKey)}
+      onTabChange={index => {
+        onChange(items[index]);
+      }}
+    >
+      {items.map(({ label }, index) => (
+        <Text key={index} variant="small" color="inherit">
+          {label}
+        </Text>
+      ))}
+    </Bar>
+  );
+}
+
+export default Pills;
