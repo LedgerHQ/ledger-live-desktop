ef7f37b47b7966184cf55602b1319323801355c4
Gregor Gilchrist <gregor.gilchrist@ledger.fr>
2021-11-29 15:48:30 +0100

    fixed account test

--- playwright/specs/account.spec.ts
+++ playwright/specs/account.spec.ts
@@ -4,9 +4,7 @@ import { AccountModal } from "../models/AccountModal";
 import { DeviceAction } from "../models/DeviceAction";
 
 // specific environment
-test.use({ userdata: "skip-onboarding", env: { DEV_TOOLS: true } });
-
-process.env.PWDEBUG = "1";
+test.use({ userdata: "skip-onboarding" });
 
 const currencies = ["BTC", "LTC", "ETH", "ATOM", "XTZ", "XRP"];
 
@@ -26,7 +24,6 @@ test.describe.parallel("Accounts", () => {
 
       await test.step(`[${currency}] Open device app`, async () => {
         await deviceAction.openApp();
-        await page.pause();
         expect(await page.screenshot()).toMatchSnapshot(`${currency}-device-open-app.png`);
       });
 
