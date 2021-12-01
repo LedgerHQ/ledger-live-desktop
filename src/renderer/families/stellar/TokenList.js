3514f8be4c25a10a5df54f7595875236d87240f3
Iveta <quietbits@gmail.com>
2021-12-01 10:19:14 -0500

    Add Stellar asset to account

---
+++ src/renderer/families/stellar/TokenList.js
@@ -0,0 +1,33 @@
+// @flow
+import React from "react";
+import { Trans } from "react-i18next";
+import { useDispatch } from "react-redux";
+import type { Account } from "@ledgerhq/live-common/lib/types/account";
+
+import { openModal } from "~/renderer/actions/modals";
+
+import Box from "~/renderer/components/Box";
+import IconPlus from "~/renderer/icons/Plus";
+import Button from "~/renderer/components/Button";
+
+const ReceiveButton = ({ account }: { account: Account }) => {
+  const dispatch = useDispatch();
+  const onReceiveClick = () => {
+    dispatch(openModal("MODAL_STELLAR_ADD_ASSET", { account }));
+  };
+  return (
+    <Button small color="palette.primary.main" onClick={onReceiveClick}>
+      <Box horizontal flow={1} alignItems="center">
+        <IconPlus size={12} />
+        <Box>
+          <Trans i18nKey="tokensList.stellar.cta" />
+        </Box>
+      </Box>
+    </Button>
+  );
+};
+
+export default {
+  ReceiveButton,
+  hasSpecificTokenWording: true,
+};
