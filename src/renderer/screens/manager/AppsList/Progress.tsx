fcf3c140b4909861ea965930d78600050c696c9e
Olivier Freyssinet <olivier.freyssinet@ledger.fr>
2021-12-10 13:04:49 +0100

    Add v3 styling to manager Progress loader

--- src/renderer/screens/manager/AppsList/Progress.tsx
+++ src/renderer/screens/manager/AppsList/Progress.tsx
@@ -3,20 +3,9 @@ import styled from "styled-components";
 import { Trans } from "react-i18next";
 
 import { State } from "@ledgerhq/live-common/lib/apps/types";
+import { Flex, InfiniteLoader, ProgressLoader, Text } from "@ledgerhq/react-ui";
 
 import { useAppInstallProgress } from "@ledgerhq/live-common/lib/apps/react";
-
-import Box from "~/renderer/components/Box";
-import Text from "~/renderer/components/Text";
-import ProgressBar from "~/renderer/components/Progress";
-
-const Holder = styled.div`
-  width: 100px;
-  height: 5px;
-  position: relative;
-  border-radius: 5px;
-  overflow: hidden;
-`;
 
 type Props = {
   state: State;
@@ -27,46 +16,50 @@ type Props = {
   isCurrent: boolean;
 };
 
+const LOADER_SIZE = 18;
+
+const Loader = styled(ProgressLoader).attrs(p => ({
+  frontStrokeColor: p.theme.colors.primary.c80,
+  radius: LOADER_SIZE / 2,
+  stroke: 3.5,
+  frontStrokeLinecap: "round",
+  showPercentage: false,
+}))``;
+
+const LoaderContainer = styled.div`
+  height: ${LOADER_SIZE}px;
+  width: ${LOADER_SIZE}px;
+`;
+
 // we can forward appOp from state.currentAppOp if it matches the contextual app
 const Progress = ({ state, name, updating, installing, uninstalling, isCurrent }: Props) => {
   const progress = useAppInstallProgress(state, name);
 
   return (
-    <Box flex="1" horizontal justifyContent="flex-end" overflow="hidden">
-      <Box flex="0 0 auto" vertical alignItems="flex-end" justifyContent="center">
-        <Box
-          flex="0 0 auto"
-          horizontal
-          alignItems="center"
-          justifyContent="center"
-          py={1}
-          maxWidth="100%"
-        >
-          <Text ff="Inter|SemiBold" fontSize={3} color="palette.primary.main">
-            <Trans
-              i18nKey={
-                updating
-                  ? "manager.applist.item.updating"
-                  : uninstalling
-                  ? "manager.applist.item.uninstalling"
-                  : installing && isCurrent && progress !== 1
-                  ? "manager.applist.item.installing"
-                  : "manager.applist.item.scheduled"
-              }
-            />
-          </Text>
-        </Box>
-        <Holder>
-          {uninstalling ? (
-            <ProgressBar infinite timing={1200} />
-          ) : installing && isCurrent && progress !== 1 ? (
-            <ProgressBar infinite timing={1200} progress={progress || 0} />
-          ) : (
-            <ProgressBar color="palette.text.shade20" progress={-1} />
-          )}
-        </Holder>
-      </Box>
-    </Box>
+    <Flex flex="1" flexDirection="row" justifyContent="flex-end" alignItems="center">
+      <Text variant="paragraph" fontWeight="semibold" color="primary.c80" mr={2}>
+        <Trans
+          i18nKey={
+            updating
+              ? "manager.applist.item.updating"
+              : uninstalling
+              ? "manager.applist.item.uninstalling"
+              : installing && isCurrent && progress !== 1
+              ? "manager.applist.item.installing"
+              : "manager.applist.item.scheduled"
+          }
+        />
+      </Text>
+      <LoaderContainer>
+        {uninstalling ? (
+          <InfiniteLoader size={LOADER_SIZE} />
+        ) : installing && isCurrent && progress !== 1 ? (
+          <Loader progress={(progress || 0) * 100} />
+        ) : (
+          <Loader progress={0} />
+        )}
+      </LoaderContainer>
+    </Flex>
   );
 };
 
