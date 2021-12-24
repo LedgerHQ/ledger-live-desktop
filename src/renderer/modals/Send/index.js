30f554855c6094eb79dc73c18f0576fb250a60d2
Gaëtan Renaudeau <renaudeau.gaetan@gmail.com>
2021-12-24 13:30:12 +0100

    live-common bump

--- src/renderer/modals/Send/index.js
+++ src/renderer/modals/Send/index.js
@@ -1,6 +1,6 @@
 // @flow
 
-import React, { useState } from "react";
+import React, { useCallback, useState } from "react";
 import Modal from "~/renderer/components/Modal";
 import Body from "./Body";
 import type { StepId } from "./types";
@@ -10,45 +10,35 @@ type Props = {
   onClose: Function,
 };
 
-const SendModal = ({ stepId, onClose }: Props) => {
-  const [state, setState] = useState({
-    stepId: stepId || "recipient",
-  });
+const MODAL_LOCKED: { [_: StepId]: boolean } = {
+  recipient: true,
+  amount: true,
+  summary: true,
+  device: true,
+  confirmation: true,
+};
 
-  const handleReset = () => {
-    setState({
-      ...state,
-      stepId: "recipient",
-    });
-  };
-
-  const handleStepChange = (stepId: StepId) => setState({ ...state, stepId });
-
-  const isModalLocked = ["recipient", "amount", "summary", "device", "confirmation"].includes(
-    state.stepId,
-  );
-
-  const rest = {};
-  if (onClose) {
-    rest.onClose = onClose;
-  }
-
+const SendModal = ({ stepId: initialStepId, onClose }: Props) => {
+  const [stepId, setStep] = useState(() => initialStepId || "recipient");
+  const handleReset = useCallback(() => setStep("recipient"), []);
+  const handleStepChange = useCallback(stepId => setStep(stepId), []);
+  const isModalLocked = MODAL_LOCKED[stepId];
   return (
     <Modal
       name="MODAL_SEND"
       centered
-      refocusWhenChange={state.stepId}
+      refocusWhenChange={stepId}
       onHide={handleReset}
+      onClose={onClose}
       preventBackdropClick={isModalLocked}
       render={({ onClose, data }) => (
         <Body
-          stepId={state.stepId}
+          stepId={stepId}
           onClose={onClose}
           onChangeStepId={handleStepChange}
           params={data || {}}
         />
       )}
-      {...rest}
     />
   );
 };
