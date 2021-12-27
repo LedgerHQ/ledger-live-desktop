9681155b2be1b9738e0b15dcdf446434425c8a3d
ofreyssinet-ledger <91890529+ofreyssinet-ledger@users.noreply.github.com>
2021-12-27 10:31:40 +0100

    Small fixes on vitry rebranding (#4564)
    * Fix TagsTabBar style (cursor, fontSize, gaps)
    
    * Refactor ConfirmationCheck component -> use BoxedIcon from UI lib

--- src/renderer/components/TagsTabBar.tsx
+++ src/renderer/components/TagsTabBar.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import styled from "styled-components";
 import { Tag, Text } from "@ledgerhq/react-ui";
 import { Flex } from "@ledgerhq/react-ui/components/layout";
 
@@ -8,25 +9,25 @@ type Props = {
   tabs: string[];
 };
 
+const Tab = styled(Tag).attrs({
+  type: "opacity",
+})`
+  cursor: pointer;
+`;
+
 const TagsTabBar = (props: Props) => {
   const { onSelectedChanged, selectedIndex, tabs } = props;
   return (
-    <Flex flexDirection="row">
+    <Flex flexDirection="row" columnGap={3}>
       {tabs.map((tab, index) => {
         const isSelected = index === selectedIndex;
         const color = isSelected ? "palette.neutral.c100" : "palette.neutral.c80";
         return (
-          <Tag
-            type="opacity"
-            key={index}
-            onClick={() => onSelectedChanged(index)}
-            active={isSelected}
-            style={{ marginLeft: index === 0 ? 0 : "8px" }}
-          >
-            <Text type="small" color={color}>
+          <Tab key={index} onClick={() => onSelectedChanged(index)} active={isSelected}>
+            <Text variant="small" color={color}>
               {tab}
             </Text>
-          </Tag>
+          </Tab>
         );
       })}
     </Flex>
