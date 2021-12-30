81299f36c3e952654d7f255733d7ca38e799e8e9
alalmi-ledger <allane.lalmi@ledger.fr>
2021-12-30 14:15:45 +0100

    Added test for onboarding new device flow

--- playwright/models/OnboardingPage.ts
+++ playwright/models/OnboardingPage.ts
@@ -1,5 +1,6 @@
 import { Page, Locator } from '@playwright/test';
 
+
 export class OnboardingPage {
   readonly page: Page;
   readonly getStartedButton: Locator;
@@ -17,11 +18,32 @@ export class OnboardingPage {
     this.termsCheckbox = page.locator("#onboarding-terms-check");
     this.termsSubmitButton = page.locator('#onboarding-terms-submit');
     this.selectDeviceButton = (device: string): Locator => page.locator(`button:has-text("${device}")`);
-    this.connectDeviceButton = page.locator('button:has-text("Connect deviceConnect your Nano XIs your device already set up? Connect it to th")');
+    this.connectDeviceButton = page.locator('#initialized-device');
     this.modalContainer = page.locator('data-test-id=modal-container');
     this.checkMyNanoButton = page.locator('button:has-text("Check my Nano")');
-    this.continueButton = page.locator('button:has-text("Continue")')
+    this.continueButton = page.locator('button:has-text("Continue")');
+    this.firstUseButton = page.locator('#first-use');
+    this.leftArrowBasicsButton = page.locator('#pedagogy-left');
+    this.rightArrowBasicsButton = page.locator('#pedagogy-right');
+    this.setupWalletButton = page.locator('#setup-nano-wallet-cta');
+    this.getStartedCtaButton = page.locator('#get-started-cta');
+    this.beCarefulButton = page.locator('#be-careful-cta');
+    this.startSetupButton = page.locator('#device-howto-cta');
+    this.pincodeCheckbox = page.locator('#pincode-private-cb');
+    this.setupPincodeButton = page.locator('#device-pincode-cta');
+    this.confirmPincodeButton = page.locator('#pincode-howto-cta');
+    this.recoveryPhraseCheckbox = page.locator('#recoveryphrase-private-cb');
+    this.recoverySetupButton = page.locator('#device-recoveryphrase-cta');
+    this.writeRecoveryPhraseButton = page.locator('#use-recovery-sheet');
+    this.confirmRecoveryPhraseButton = page.locator('#recovery-howto-3');
+    this.hideRecoveryPhraseButton = page.locator('#hide-recovery-cta');
+    this.quizStartButton = page.locator('#quizz-start-cta');
+    this.quizAnswerTopButton = page.locator('#answer-0');
+    this.quizAnswerBottomButton = page.locator('#answer-1');
+    this.quizNextButton = page.locator('#quizz-next-cta');
+    this.quizSuccessButton = page.locator('#quizz-success-cta');
   }
+  
 
   async getStarted() {
     await this.getStartedButton.click();
@@ -44,6 +66,52 @@ export class OnboardingPage {
     await this.modalContainer.waitFor({ state: 'visible' });
   }
 
+  async newDevice() {
+    await this.firstUseButton.click();
+  }
+
+  async basicsCarrousel() {
+    await this.rightArrowBasicsButton.click();
+  }
+
+  async newSetup() {
+    await this.getStartedCtaButton.click();
+    await this.beCarefulButton.click();
+    await this.startSetupButton.click();
+    await this.pincodeCheckbox.click();
+    await this.setupPincodeButton.click();
+    await this.confirmPincodeButton.click();
+    await this.recoveryPhraseCheckbox.click();
+    await this.recoverySetupButton.click();
+    await this.writeRecoveryPhraseButton.click();
+    await this.confirmRecoveryPhraseButton.click();
+    await this.hideRecoveryPhraseButton.click();
+  }
+  
+  async setupWallet() {
+    await this.setupWalletButton.click();
+  }
+
+  async startQuiz() {
+    await this.quizStartButton.click();
+  }
+
+  async answerQuizTop() {
+    await this.quizAnswerTopButton.click();
+  }
+
+  async answerQuizBottom() {
+    await this.quizAnswerBottomButton.click();
+  }
+
+  async quizNextQuestion() {
+    await this.quizNextButton.click();
+  }
+
+  async quizEnd() {
+    await this.quizSuccessButton.click();
+  }
+
   async checkDevice() {
     await this.checkMyNanoButton.click();
   }
@@ -52,3 +120,4 @@ export class OnboardingPage {
     await this.continueButton.click()
   }
 };
+
