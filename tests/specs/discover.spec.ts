73202a4f2e1fe6286c428c0773d35f8f8065b387
Gregor Gilchrist <gregor.gilchrist@ledger.fr>
2022-01-20 22:55:19 +0100

    fixed test with awaits

--- tests/specs/discover.spec.ts
+++ tests/specs/discover.spec.ts
@@ -14,55 +14,66 @@ test.beforeAll(async () => {
 });
 
 // TODO: tidy up test and add more features to test app before making test run
-test.skip("Navigate to live app", async ({ page }) => {
+test("Live SDK", async ({ page }) => {
   const discoverPage = new DiscoverPage(page);
 
-  test.step("Navigate to catalog", async () => {
+  await test.step("Navigate to catalog", async () => {
+    // await page.pause();
     await discoverPage.navigateToCatalog();
+    expect(await page.screenshot()).toMatchSnapshot({
+      name: "catalog.png",
+    });
   });
 
-  test.step("Open Test App", async () => {
+  await test.step("Open Test App", async () => {
     await discoverPage.openTestApp();
+    expect(await page.screenshot()).toMatchSnapshot({
+      name: "open-test-app.png",
+    });
   });
 
-  test.step("Accept Live App Disclaimer", async () => {
+  await test.step("Accept Live App Disclaimer", async () => {
     await discoverPage.acceptLiveAppDisclaimer();
+    expect(await page.screenshot()).toMatchSnapshot({
+      name: "accept-live-disclaimer.png",
+    });
   });
 
-  test.step("List all accounts", async () => {
-    await page.pause();
+  await test.step("List all accounts", async () => {
+    // await page.pause();
+    // FIXME: wrong screenshot
     await discoverPage.getAccountsList();
     expect(await page.screenshot()).toMatchSnapshot({
       name: "live-app-list-all-accounts.png",
     });
   });
 
-  test.step("Request Account modal - open", async () => {
-    await page.pause();
+  await test.step("Request Account modal - open", async () => {
+    // await page.pause();
     await discoverPage.requestAccount();
     expect(await page.screenshot()).toMatchSnapshot({
       name: "live-app-request-account-modal-1.png",
     });
   });
 
-  test.step("Request Account - account dropdown", async () => {
-    await page.pause();
+  await test.step("Request Account - account dropdown", async () => {
+    // await page.pause();
     await discoverPage.openAccountDropdown();
     expect(await page.screenshot()).toMatchSnapshot({
       name: "live-app-request-account-modal-2.png",
     });
   });
 
-  test.step("Request Account - select BTC", async () => {
-    await page.pause();
+  await test.step("Request Account - select BTC", async () => {
+    // await page.pause();
     await discoverPage.selectAccount();
     expect(await page.screenshot()).toMatchSnapshot({
       name: "live-app-request-account-modal-3.png",
     });
   });
 
-  test.step("Request Account - single account output", async () => {
-    await page.pause();
+  await test.step("Request Account - single account output", async () => {
+    // await page.pause();
     await discoverPage.exitModal();
     expect(await page.screenshot()).toMatchSnapshot({
       name: "live-app-request-single-account-output.png",
