a7171396ffc6af935fe8c8941359b16909f32c9a
Gregor Gilchrist <gregor.gilchrist@ledger.fr>
2022-01-20 12:44:27 +0100

    adding sdk util

--- tests/specs/discover.spec.ts
+++ tests/specs/discover.spec.ts
@@ -3,9 +3,9 @@ import { expect } from "@playwright/test";
 import { DiscoverPage } from "../models/DiscoverPage";
 
 // Comment out to disable recorder
-// process.env.PWDEBUG = "1";
+process.env.PWDEBUG = "1";
 
-test.use({ userdata: "1AccountBTC1AccountETH" });
+test.use({ userdata: "1AccountBTC1AccountETH", env: { DEV_TOOLS: true } });
 
 test.beforeAll(async () => {
   // Can start dummy app here?
@@ -13,11 +13,12 @@ test.beforeAll(async () => {
 });
 
 // TODO: tidy up test and add more features to test app before making test run
-test.skip("Navigate to live app", async ({ page }) => {
+test("Navigate to live app", async ({ page }) => {
   const discoverPage = new DiscoverPage(page);
   await discoverPage.navigateToCatalog();
   await discoverPage.openTestApp();
   await discoverPage.acceptLiveAppDisclaimer();
+  await page.pause();
   await discoverPage.requestAccount();
   expect(await page.screenshot()).toMatchSnapshot({
     name: "live-app-request-account-modal.png",
