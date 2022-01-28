beb0458f594713c6e8c703e09101ef4d1fe8c2d4
Gregor Gilchrist <gregor.gilchrist@ledger.fr>
2022-01-28 13:41:11 +0100

    trying with focus to fix issue

--- tests/models/DiscoverPage.ts
+++ tests/models/DiscoverPage.ts
@@ -54,6 +54,7 @@ export class DiscoverPage {
     });
 
     await this.disclaimerText.waitFor({ state: "visible" });
+    await this.disclaimerText.focus();
   }
 
   async acceptLiveAppDisclaimer() {
