ac6a067ffc06cb2e3f3288113c404b30332eae41
Thomas Laforge <thomas.laforge@ledger.fr>
2022-02-08 01:40:10 +0100

    handle dark theme

--- src/renderer/components/Onboarding/Screens/SelectDevice/DeviceSelector.js
+++ src/renderer/components/Onboarding/Screens/SelectDevice/DeviceSelector.js
@@ -2,16 +2,20 @@
 
 import React from "react";
 import styled from "styled-components";
+import useTheme from "~/renderer/hooks/useTheme";
 import type { DeviceModelId } from "@ledgerhq/devices";
 import type { ThemedComponent } from "~/renderer/styles/StyleProvider";
-import nanoX from "./assets/nanoX.svg";
-import nanoS from "./assets/nanoS.svg";
-import nanoSP from "./assets/NanoSP.svg";
+import nanoX from "~/renderer/images/devices/nanoX.png";
+import nanoXDark from "~/renderer/images/devices/nanoX_dark.png";
+import nanoS from "~/renderer/images/devices/nanoS.png";
+import nanoSDark from "~/renderer/images/devices/nanoS_dark.png";
+import nanoSP from "~/renderer/images/devices/nanoSP.png";
+import nanoSPDark from "~/renderer/images/devices/nanoSP_dark.png";
 
 import { registerAssets } from "~/renderer/components/Onboarding/preloadAssets";
 import { DeviceSelectorOption } from "./DeviceSelectorOption";
 
-registerAssets([nanoX, nanoS, nanoSP]);
+registerAssets([nanoX, nanoS, nanoSP, nanoXDark, nanoSDark, nanoSPDark]);
 
 const DeviceSelectContainer: ThemedComponent<*> = styled.div`
   display: flex;
@@ -30,41 +34,38 @@ const DeviceSelectContainer: ThemedComponent<*> = styled.div`
   }
 `;
 
-const NanoX = styled.div`
-  background: url(${nanoX}) no-repeat top right;
+const DeviceIllustration = styled.div`
+  background: url(${p=> p.url}) no-repeat top right;
   background-position: center;
   width: 36px;
   height: 180px;
 `;
 
-const NanoS = styled.div`
-  background: url(${nanoS}) no-repeat top right;
-  width: 36px;
-  height: 180px;
-`;
-
-const NanoSP = styled.div`
-  width: 36px;
-  height: 180px;
-  background: url(${nanoSP}) no-repeat top right;
-`;
-
 const deviceNanoSP = {
   id: "nanoSP",
   label: "Nano S Plus",
-  Illu: NanoSP,
+  images: {
+    dark: nanoSPDark,
+    light: nanoSP
+  },
 };
 
 const devices = [
   {
     id: "nanoS",
     label: "Nano S",
-    Illu: NanoS,
+    images: {
+      dark: nanoSDark,
+      light: nanoS
+    },
   },
   {
     id: "nanoX",
     label: "Nano X",
-    Illu: NanoX,
+    images: {
+      dark: nanoXDark,
+      light: nanoX
+    },
   },
 ];
 
@@ -78,14 +79,15 @@ type DeviceSelectorProps = {
 };
 
 export function DeviceSelector({ onClick }: DeviceSelectorProps) {
+  const type = useTheme("colors.palette.type") || "light";
   return (
     <DeviceSelectContainer>
-      {devices.map(({ id, label, Illu }) => (
+      {devices.map(({ id, label, images }) => (
         <DeviceSelectorOption
           id={`device-${id}`}
           key={id}
           label={label}
-          Illu={<Illu />}
+          Illu={<DeviceIllustration url={ images[type] } />}
           onClick={() => onClick(id)}
         />
       ))}
