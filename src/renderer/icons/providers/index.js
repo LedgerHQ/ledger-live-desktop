cc4f460c7919d292826c46419914053947cb6502
Alexandre Chabrolin <9203826+chabroA@users.noreply.github.com>
2022-02-22 15:02:11 +0100

    Add ftx logos

--- src/renderer/icons/providers/index.js
+++ src/renderer/icons/providers/index.js
@@ -4,5 +4,7 @@ import CoinifySquare from "./CoinifySquare";
 import MoonPay from "./MoonPay";
 import Paraswap from "./Paraswap";
 import Wyre from "./Wyre";
+import FTX from "./FTX";
+import FTXUS from "./FTXUS";
 
-export { Changelly, Coinify, CoinifySquare, MoonPay, Paraswap, Wyre };
+export { Changelly, Coinify, CoinifySquare, MoonPay, Paraswap, Wyre, FTX, FTXUS };
