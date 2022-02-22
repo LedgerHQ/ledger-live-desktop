ecb3d00f65d2227368923d89b785a449de2c0adf
Olivier Freyssinet <olivier.freyssinet@ledger.fr>
2022-02-22 16:23:00 +0100

    Fix learn page padding, remove title, fix iframe icons & fonts loading

--- src/renderer/screens/learn/index.tsx
+++ src/renderer/screens/learn/index.tsx
@@ -1,30 +1,31 @@
 import React from "react";
 import { useTranslation } from "react-i18next";
-import { Flex, Text } from "@ledgerhq/react-ui";
+import styled from "styled-components";
+import { Flex } from "@ledgerhq/react-ui";
 import useTheme from "~/renderer/hooks/useTheme";
+
+const Container = styled(Flex).attrs({
+  flex: 1,
+  flexDirection: "column",
+  alignItems: "stretch",
+  justifyContent: "flex-start",
+})`
+  margin: -${p => p.theme.space[6]}px;
+  margin-right: -${p => p.theme.space[6] - p.theme.overflow.trackSize}px;
+`;
 
 const learnURL = "https://www.ledger.com/ledger-live-learn";
 
 export default function LearnScreen() {
-  const { t, i18n } = useTranslation();
+  const { i18n } = useTranslation();
   const themeType: string = useTheme("colors.palette.type");
 
   return (
-    <Flex
-      flexDirection="column"
-      flex={1}
-      alignItems="stretch"
-      justifyContent="flex-start"
-      px={1}
-      mx={-1}
-    >
-      <Text variant="h3" fontSize="28px" lineHeight="33px">
-        {t("learn.title")}
-      </Text>
+    <Container>
       <Flex flexGrow={1}>
         <iframe
           loading="eager"
-          sandbox="allow-scripts"
+          sandbox="allow-scripts allow-same-origin"
           frameBorder="0"
           allowFullScreen={false}
           width="100%"
@@ -32,6 +33,6 @@ export default function LearnScreen() {
           src={`${learnURL}?theme=${themeType}&lang=${i18n.languages[0]}`}
         />
       </Flex>
-    </Flex>
+    </Container>
   );
 }
