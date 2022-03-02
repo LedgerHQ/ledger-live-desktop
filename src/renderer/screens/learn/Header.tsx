e2f54218393bddfb9c243833b1dfb859edaf68e6
Olivier Freyssinet <olivier.freyssinet@ledger.fr>
2022-03-02 11:30:03 +0100

    Fix header lineheight

--- src/renderer/screens/learn/Header.tsx
+++ src/renderer/screens/learn/Header.tsx
@@ -1,21 +1,12 @@
 import React from "react";
-import { Flex, Text } from "@ledgerhq/react-ui";
-import styled from "@ledgerhq/react-ui/components/styled";
+import { Text } from "@ledgerhq/react-ui";
 import { useTranslation } from "react-i18next";
-
-const TitleContainer = styled(Flex).attrs({
-  height: "48px",
-  alignItems: "center",
-  mb: "40px",
-})``;
 
 export default function Header() {
   const { t } = useTranslation();
   return (
-    <TitleContainer>
-      <Text variant="h3" fontSize="28px">
-        {t("learn.title")}
-      </Text>
-    </TitleContainer>
+    <Text variant="h3" fontSize="28px" lineHeight="34px" mb="40px">
+      {t("learn.title")}
+    </Text>
   );
 }
