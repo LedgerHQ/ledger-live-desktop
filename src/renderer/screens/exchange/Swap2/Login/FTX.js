682ff331047171e7d772613fdcbcd658a50dd392
Alexandre Chabrolin <9203826+chabroA@users.noreply.github.com>
2022-03-04 16:39:21 +0100

    move code to LLC

--- src/renderer/screens/exchange/Swap2/Login/FTX.js
+++ src/renderer/screens/exchange/Swap2/Login/FTX.js
@@ -1,10 +1,11 @@
 // @flow
 
-import React, { useRef, useMemo, useCallback, useEffect } from "react";
-import SwapConnectWidget from "../SwapConnectWidget";
+import React, { useRef, useMemo } from "react";
 
-import type { FTXProviders } from "../utils";
-import { getFTXURL } from "../utils";
+import type { FTXProviders } from "@ledgerhq/live-common/lib/exchange/swap/utils";
+import { getFTXURL } from "@ledgerhq/live-common/lib/exchange/swap/utils";
+
+import SwapConnectWidget from "../SwapConnectWidget";
 
 type Props = { onClose: Function, provider: FTXProviders };
 
@@ -12,37 +13,6 @@ const FTXLogin = ({ onClose, provider }: Props) => {
   const url = useMemo(() => getFTXURL({ type: "login", provider }), [provider]);
 
   const webviewRef = useRef(null);
-
-  /**
-   * Force removal of authToken item from webview local storage when user is
-   * asked to login. Done to allow for FTX login reset (when cleaning app.json)
-   */
-  const handleExecuteJavaScript = useCallback(() => {
-    const webview = webviewRef.current;
-    if (webview) {
-      /**
-       * FIXME:
-       * should remove auth token and ask the user to log in again (by provider
-       * his login / password infos)
-       */
-
-      webview.executeJavaScript(`window.localStorage.removeItem("authToken");`);
-      webview.executeJavaScript(`window.localStorage.clear();`);
-    }
-  }, []);
-
-  useEffect(() => {
-    const webview = webviewRef.current;
-    if (webview) {
-      webview.addEventListener("dom-ready", handleExecuteJavaScript);
-    }
-
-    return () => {
-      if (webview) {
-        webview.removeEventListener("dom-ready", handleExecuteJavaScript);
-      }
-    };
-  }, [handleExecuteJavaScript]);
 
   return <SwapConnectWidget provider={provider} onClose={onClose} url={url} ref={webviewRef} />;
 };
