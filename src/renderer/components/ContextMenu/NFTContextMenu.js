b9f9ed8edd005df8b2fced8c7b03796338f2479b
lambertkevin <kevin.lambert@ledger.fr>
2022-03-25 16:44:19 +0100

    Update DropDowns for NFTs

--- src/renderer/components/ContextMenu/NFTContextMenu.js
+++ src/renderer/components/ContextMenu/NFTContextMenu.js
@@ -1,62 +1,34 @@
 // @flow
-import React from "react";
+import React, { useMemo, memo } from "react";
 import { useTranslation } from "react-i18next";
 import { useNftMetadata } from "@ledgerhq/live-common/lib/nft/NftMetadataProvider";
-import IconOpensea from "~/renderer/icons/Opensea";
-import IconRarible from "~/renderer/icons/Rarible";
-import IconGlobe from "~/renderer/icons/Globe";
-import { openURL } from "~/renderer/linking";
 import ContextMenuItem from "./ContextMenuItem";
+import nftLinksFactory from "~/helpers/nftLinksFactory";
 
 type Props = {
   contract: string,
   tokenId: string,
+  currencyId: string,
   leftClick?: boolean,
   children: any,
 };
 
-export default function NFTContextMenu({ leftClick, children, contract, tokenId }: Props) {
+const NFTContextMenu = ({ leftClick, children, contract, tokenId, currencyId }: Props) => {
   const { t } = useTranslation();
-  const { metadata } = useNftMetadata(contract, tokenId);
-
-  const defaultLinks = {
-    openSea: `https://opensea.io/assets/${contract}/${tokenId}`,
-    rarible: `https://rarible.com/token/${contract}:${tokenId}`,
-    etherscan: `https://etherscan.io/token/${contract}?a=${tokenId}`,
-  };
-
-  const menuItems = [
-    {
-      key: "opensea",
-      label: t("NFT.viewer.actions.open", { viewer: "Opensea.io" }),
-      Icon: IconOpensea,
-      type: "external",
-      callback: () => openURL(metadata?.links?.opensea || defaultLinks.openSea),
-    },
-    {
-      key: "rarible",
-      label: t("NFT.viewer.actions.open", { viewer: "Rarible" }),
-      Icon: IconRarible,
-      type: "external",
-      callback: () => openURL(metadata?.links?.rarible || defaultLinks.rarible),
-    },
-    {
-      key: "sep2",
-      type: "separator",
-      label: "",
-    },
-    {
-      key: "etherscan",
-      label: t("NFT.viewer.actions.open", { viewer: "Explorer" }),
-      Icon: IconGlobe,
-      type: "external",
-      callback: () => openURL(metadata?.links?.etherscan || defaultLinks.etherscan),
-    },
-  ];
+  const { status, metadata } = useNftMetadata(contract, tokenId, currencyId);
+  const links = useMemo(() => nftLinksFactory(currencyId, t, metadata?.links), [
+    currencyId,
+    metadata?.links,
+    t,
+  ]);
+  const menuItems = useMemo(() => (status === "loaded" ? links : []), [links, status]);
 
   return (
     <ContextMenuItem leftClick={leftClick} items={menuItems}>
       {children}
     </ContextMenuItem>
   );
-}
+};
+
+// $FlowFixMe
+export default memo(NFTContextMenu);
--- src/renderer/drawers/NFTViewerDrawer/ExternalViewerButton.js
+++ src/renderer/drawers/NFTViewerDrawer/ExternalViewerButton.js
@@ -1,6 +1,6 @@
 // @flow
 
-import React from "react";
+import React, { useMemo, memo } from "react";
 import { useTranslation } from "react-i18next";
 import styled from "styled-components";
 
@@ -9,15 +9,11 @@ import Button from "~/renderer/components/Button";
 import DropDownSelector, { DropDownItem } from "~/renderer/components/DropDownSelector";
 import IconDots from "~/renderer/icons/Dots";
 import IconExternal from "~/renderer/icons/ExternalLink";
-import IconOpensea from "~/renderer/icons/Opensea";
-import IconRarible from "~/renderer/icons/Rarible";
-import IconGlobe from "~/renderer/icons/Globe";
-import { openURL } from "~/renderer/linking";
-
-import type { DropDownItemType } from "~/renderer/components/DropDownSelector";
-import type { ThemedComponent } from "~/renderer/styles/StyleProvider";
+import nftLinksFactory from "~/helpers/nftLinksFactory";
 
 import type { NFTMetadataResponse } from "@ledgerhq/live-common/lib/types";
+import type { DropDownItemType } from "~/renderer/components/DropDownSelector";
+import type { ThemedComponent } from "~/renderer/styles/StyleProvider";
 
 const Separator: ThemedComponent<{}> = styled.div`
   background-color: ${p => p.theme.colors.palette.divider};
@@ -37,68 +33,44 @@ const Item: ThemedComponent<{
   display: flex;
 `;
 
-type ItemType = DropDownItemType & {
-  icon?: React$Element<*>,
-  onClick?: Function,
-  type?: "separator",
-};
-
 type ExternalViewerButtonProps = {
   links: $PropertyType<$PropertyType<NFTMetadataResponse, "result">, "links">,
   contract: string,
   tokenId: string,
+  currencyId: string,
 };
 
-export const ExternalViewerButton = ({ links, contract, tokenId }: ExternalViewerButtonProps) => {
+const ExternalViewerButton = ({
+  links,
+  contract,
+  tokenId,
+  currencyId,
+}: ExternalViewerButtonProps) => {
   const { t } = useTranslation();
 
-  const defaultLinks = {
-    openSea: `https://opensea.io/assets/${contract}/${tokenId}`,
-    rarible: `https://rarible.com/token/${contract}:${tokenId}`,
-    etherscan: `https://etherscan.io/token/${contract}?a=${tokenId}`,
-  };
+  const items: DropDownItemType[] = useMemo(() => nftLinksFactory(currencyId, t, links), [
+    currencyId,
+    links,
+    t,
+  ]);
 
-  const items: DropDownItemType[] = [
-    {
-      key: "opensea",
-      label: t("NFT.viewer.actions.open", { viewer: "Opensea.io" }),
-      icon: <IconOpensea size={16} />,
-      onClick: () => openURL(links?.opensea || defaultLinks.openSea),
-    },
-    {
-      key: "rarible",
-      label: t("NFT.viewer.actions.open", { viewer: "Rarible" }),
-      icon: <IconRarible size={16} />,
-      onClick: () => openURL(links?.rarible || defaultLinks.rarible),
-    },
-    {
-      key: "sep2",
-      type: "separator",
-      label: "",
-    },
-    {
-      key: "etherscan",
-      label: t("NFT.viewer.actions.open", { viewer: "Explorer" }),
-      icon: <IconGlobe size={16} />,
-      onClick: () => openURL(links?.etherscan || defaultLinks.etherscan),
-    },
-  ];
-
-  const renderItem = ({ item }: { item: ItemType }) => {
+  const renderItem = ({ item }) => {
     if (item.type === "separator") {
       return <Separator />;
     }
 
+    const Icon = item.Icon ? React.createElement(item.Icon, { size: 16 }) : <></>;
+
     return (
       <Item
-        id={`external-popout-${item.key}`}
+        id={`external-popout-${item.id}`}
         horizontal
         flow={2}
         onClick={item.onClick}
-        disableHover={item.key === "hideEmpty"}
+        disableHover={item.id === "hideEmpty"}
       >
         <Box horizontal>
-          {item.icon ? <Box mr={2}>{item.icon}</Box> : null}
+          {item.Icon ? <Box mr={2}>{Icon}</Box> : null}
           {item.label}
         </Box>
         <Box ml={4}>
@@ -125,3 +97,6 @@ export const ExternalViewerButton = ({ links, contract, tokenId }: ExternalViewe
     </DropDownSelector>
   );
 };
+
+// $FlowFixMe
+export default memo(ExternalViewerButton);
