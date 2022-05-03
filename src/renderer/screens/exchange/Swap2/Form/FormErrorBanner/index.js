4c72dce7c8f5f0c7926c3a542b09354d78f7b53a
Alexandre Chabrolin <9203826+chabroA@users.noreply.github.com>
2022-05-03 11:05:48 +0200

    Add transaltion to error banner

--- src/renderer/screens/exchange/Swap2/Form/FormErrorBanner/index.js
+++ src/renderer/screens/exchange/Swap2/Form/FormErrorBanner/index.js
@@ -2,23 +2,19 @@
 import React from "react";
 import { openURL } from "~/renderer/linking";
 import { urls } from "~/config/urls";
-// import { useTranslation } from "react-i18next";
+import { useTranslation } from "react-i18next";
 
 import SectionInformative from "~/renderer/screens/exchange/Swap2/Form/FormSummary/SectionInformative";
-import { getProviderName } from "@ledgerhq/live-common/lib/exchange/swap/utils";
 
 const FormErrorBanner = ({ provider, error }: { provider?: string, error: string }) => {
-  // const { t } = useTranslation();
+  const { t } = useTranslation();
 
   if (!provider) return null;
 
   const openProviderSupport = () => openURL(urls.swap.providers[provider]?.support);
 
-  // FIXME: need proper wording / transaltion
-
-  const ctaLabel = `Contact ${getProviderName(provider)} support`;
-
-  const message = `Error: ${error}`;
+  const ctaLabel = t("common.getSupport");
+  const message = `${t("crash.title")} - ${error}`;
 
   return <SectionInformative message={message} ctaLabel={ctaLabel} onClick={openProviderSupport} />;
 };
