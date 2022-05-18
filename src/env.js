ba775a58c8274438a0133ee081155b91e5af6528
Alexandre Chabrolin <9203826+chabroA@users.noreply.github.com>
2022-05-18 11:32:58 +0200

    update staging URL

--- src/env.js
+++ src/env.js
@@ -3,6 +3,6 @@ import "dotenv/config";
 process.env = {
   ...process.env,
   // FIXME: needed to build a workable test bundle of the app, to be removed one backend API is in prod
-  SWAP_API_BASE: "https://swap.staging.aws.ledger.fr/v3",
+  SWAP_API_BASE: "https://swap.aws.stg.ldg-tech.com/v3",
   NODE_ENV: (process.env || {}).NODE_ENV || "production",
 };
